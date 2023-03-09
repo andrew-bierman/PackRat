ea521d30c7e7de9ba6396a10c3f5b68e842ecdde
andrew-bierman <abbierman101@gmail.com>
2023-03-09 18:28:28 -0500

    boilerplate code added

---
+++ server/config.js
@@ -0,0 +1,10 @@
+// require('dotenv').config();
+import { config } from 'dotenv';
+
+config();
+
+export const MONGODB_URI = process.env.MONGODB_URI
+// module.exports = {
+//     MONGODB_URI: process.env.MONGODB_URI
+
+// }
