4c75e6e2c6aa09a126691ca89d6ff09ee088b456
andrew-bierman <abbierman101@gmail.com>
2023-04-03 16:41:23 -0400

    quick fix on search input component

--- client/components/SearchInput.js
+++ client/components/SearchInput.js
@@ -20,7 +20,7 @@ import { SafeAreaView } from "react-native";
 import { Platform } from "react-native";
 
 import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
-// import { Google_Api_Key } from "../constants/api";
+import { GOOGLE_API_KEY } from "@env";
 
 // redux
 import { useDispatch } from "react-redux";
@@ -79,7 +79,7 @@ export const SearchInput = () => {
       <GooglePlacesAutocomplete
         placeholder="Search"
         query={{
-          key: Google_Api_Key,
+          key: GOOGLE_API_KEY,
           language: "en", // language of the results
         }}
         minLength={2}
