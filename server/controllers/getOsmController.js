4d7fe75221762daac857e01a3cba6f1f82f45159
andrew-bierman <abbierman101@gmail.com>
2023-04-05 11:15:34 -0400

    changing route to node fetch and adding some helper functions for query building

--- server/controllers/getOsmController.js
+++ server/controllers/getOsmController.js
@@ -1,19 +1,71 @@
-import axios from 'axios'
+import fetch from 'node-fetch';
 import osmtogeojson from 'osmtogeojson';
 
 export const getOsm = async (req, res) => {
-    let { query } = req.body
-    const overpassUrl = process.env.OSM_URI
-    try {
-        const response = await axios.post(overpassUrl, query);
+    console.log('req', req); // log the request body to see what it looks like
 
-        if (response.status === 200) {
-            const geojsonData = osmtogeojson(response.data);
+    const overpassUrl = process.env.OSM_URI;
+
+    const activityTypeTags = {
+        hiking: '["highway"~"path|footway"]',
+        skiing: '["piste:type"~"downhill|nordic"]',
+        climbing: '["sport"="climbing"]',
+        cycling: '["highway"~"cycleway(:left|:right)?"]',
+        canoeing: '["waterway"~"riverbank|canal|stream"]',
+        horseback_riding: '["highway"="bridleway"]',
+        kayaking: '["waterway"~"riverbank|canal|stream|rapids|waterfall"]',
+        rock_climbing: '["natural"="cliff"]',
+        sailing: '["waterway"~"riverbank|canal|harbour|basin"]',
+    };
+
+    async function formatOverpassQuery(activityType, startPoint, endPoint) {
+        const tagString = activityTypeTags[activityType];
+        const overpassQuery = `[out:json][timeout:25];
+          (
+            way${tagString}(${startPoint.latitude},${startPoint.longitude},${endPoint.latitude},${endPoint.longitude});
+          );
+          (._;>;);
+          out skel qt;`;
+
+        return overpassQuery;
+    }
+
+    try {
+
+        const { activityType, startPoint, endPoint } = req.body;
+
+        if (!activityType || !startPoint || !endPoint) {
+            throw new Error('Invalid request parameters');
+        }
+
+        const overpassQuery = await formatOverpassQuery(activityType, startPoint, endPoint);
+
+        console.log('overpassQuery', overpassQuery)
+
+        const response = await fetch(overpassUrl, {
+            method: 'POST',
+            body: overpassQuery,
+            headers: { 'Content-Type': 'text/plain' },
+        });
+
+        console.log('response', response)
+
+
+        if (response.ok) {
+            const responseFormat = await response.json();
+            const geojsonData = osmtogeojson(responseFormat);
             res.send(geojsonData);
+            // res.send(response.text());
         } else {
-            res.send({ message: "Went wrong" });
+            console.log(response.status, response.statusText);
+            res.send({ message: 'Something went wrong' });
         }
     } catch (error) {
-        res.send({ message: "Went wrong" });
+        console.error(error);
+        res.status(500).send({ message: 'Something went wrong' });
     }
-}
+};
+
+
+
+
--- server/routes/osmRoutes.js
+++ server/routes/osmRoutes.js
@@ -2,6 +2,10 @@ import express from "express";
 import { getOsm } from "../controllers/getOsmController.js";
 
 const router = express.Router();
+
+router.use(express.text({ limit: '50mb' })); // use text middleware to parse plain text request body
+
 router.post("/", getOsm);
 
 export default router;
+
