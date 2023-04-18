3df394d8eb4ebcb77af52bef4cef160c715ca58b
dahalaazad <aazad.dahal@gmail.com>
2023-04-18 11:26:07 +0545

    feat: Implement rtk for auth

---
+++ client/store/authStore.js
@@ -0,0 +1,94 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { auth } from "../auth/firebase";
+import { signInWithEmailAndPassword } from "firebase/auth";
+
+const initialState = {
+  user: null,
+  loading: false,
+  error: null,
+};
+
+export const signUp = createAsyncThunk(
+  "auth/signUp",
+  async ({ email, password }, { rejectWithValue }) => {
+    try {
+      const response = await createUserWithEmailAndPassword(email, password);
+      console.log(response);
+      return response.user;
+    } catch (error) {
+      console.log(error);
+      return rejectWithValue(error.message);
+    }
+  }
+);
+
+export const signIn = createAsyncThunk(
+  "auth/signIn",
+  async ({ email, password }, { rejectWithValue }) => {
+    try {
+      const response = await signInWithEmailAndPassword(auth, email, password);
+      return response.user;
+    } catch (error) {
+      return rejectWithValue(error.message);
+    }
+  }
+);
+
+export const signOut = createAsyncThunk(
+  "auth/signOut",
+  async (_, { rejectWithValue }) => {
+    try {
+      await signOut();
+    } catch (error) {
+      return rejectWithValue(error.message);
+    }
+  }
+);
+
+export const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(signUp.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(signUp.fulfilled, (state, action) => {
+        state.user = action.payload;
+        state.loading = false;
+      })
+      .addCase(signUp.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload;
+      })
+      .addCase(signIn.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(signIn.fulfilled, (state, action) => {
+        // console.log("userinfo", action.payload);
+        state.user = action.payload;
+        state.loading = false;
+      })
+      .addCase(signIn.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload;
+      })
+      .addCase(signOut.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(signOut.fulfilled, (state) => {
+        state.user = null;
+        state.loading = false;
+      })
+      .addCase(signOut.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload;
+      });
+  },
+});
+
+export default authSlice.reducer;
