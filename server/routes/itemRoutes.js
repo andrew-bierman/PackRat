31ccdde09b2c7d810bf3cba373d063376f9e687c
Andrew Bierman <94939237+andrew-bierman@users.noreply.github.com>
2023-04-26 18:00:40 -0400

    adding search for items to backend

--- server/routes/itemRoutes.js
+++ server/routes/itemRoutes.js
@@ -5,12 +5,14 @@ import {
   addItem,
   editItem,
   deleteItem,
+  searchItemsByName
 } from "../controllers/itemController.js";
 
 const router = express.Router();
 
 router.get("/:packId", getItems);
 router.get("/i/:packId", getItemById);
+router.get("/search", searchItemsByName)
 router.post("/", addItem);
 router.put("/", editItem);
 router.delete("/", deleteItem);
