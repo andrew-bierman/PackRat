8ff28772affac469f200ec5b9ae109c8bcd9af33
Andrew Bierman <94939237+andrew-bierman@users.noreply.github.com>
2023-05-04 20:01:40 -0400

    improvements to pack table logic and adding updated logo

--- client/api/getPhotonResults.js
+++ client/api/getPhotonResults.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
 export const getPhotonResults = async (addressArray) => {
+    if(!addressArray) return;
+
     let params = {
         q: addressArray,
         osm_tag: [
