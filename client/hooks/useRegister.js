0682779b4d4c8874bedfc03e65bf996571c0e50f
Andrew Bierman <94939237+andrew-bierman@users.noreply.github.com>
2023-05-05 14:40:02 -0400

    moving login to use store directly instead of hook

--- client/hooks/useRegister.js
+++ client/hooks/useRegister.js
@@ -1,33 +1,33 @@
-import fetcher from "../api/fetcher";
-import { api } from "../constants/api";
-import { useMutation } from "@tanstack/react-query";
-import { queryClient } from "../constants/queryClient";
+// import fetcher from "../api/fetcher";
+// import { api } from "../constants/api";
+// import { useMutation } from "@tanstack/react-query";
+// import { queryClient } from "../constants/queryClient";
 
-const addUser = async (newUser) => {
-  const response = await fetch(`${api}/user/`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(newUser),
-  });
-  const data = await response.json();
-  if (!response.ok) {
-    throw new Error(data.error.message);
-  }
-  return data;
-};
+// const addUser = async (newUser) => {
+//   const response = await fetch(`${api}/user/`, {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//     },
+//     body: JSON.stringify(newUser),
+//   });
+//   const data = await response.json();
+//   if (!response.ok) {
+//     throw new Error(data.error.message);
+//   }
+//   return data;
+// };
 
-export default function useRegister() {
-  const mutation = useMutation({
-    mutationFn: async (newUser) => {
-      return addUser(newUser);
-    },
-    onSuccess: (data, variables, context) => {
-      // Invalidate and refetch
-      queryClient.invalidateQueries({ queryKey: ["user"] });
-    },
-  });
+// export default function useRegister() {
+//   const mutation = useMutation({
+//     mutationFn: async (newUser) => {
+//       return addUser(newUser);
+//     },
+//     onSuccess: (data, variables, context) => {
+//       // Invalidate and refetch
+//       queryClient.invalidateQueries({ queryKey: ["user"] });
+//     },
+//   });
 
-  return { addUser: mutation };
-}
+//   return { addUser: mutation };
+// }
