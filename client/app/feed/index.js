a9eb70cdbda19234ff72e9c0a1f34a6c85a5a1eb
Andrew Bierman <94939237+andrew-bierman@users.noreply.github.com>
2023-05-08 11:55:23 -0400

    setting up pack details page

--- client/app/feed/index.js
+++ client/app/feed/index.js
@@ -3,13 +3,13 @@ import FeedMobile from "../../components/feed/FeedMobile";
 import { Platform } from "react-native";
 import { Stack as Header } from "expo-router";
 
-export default function Packs() {
+export default function FeedNav() {
   return Platform.OS === "web" ? (
     <>
       <Header.Screen
         options={{
           // https://reactnavigation.org/docs/headers#setting-the-header-title
-          title: "Packs",
+          title: "Feed",
           // https://reactnavigation.org/docs/headers#adjusting-header-styles
 
           // https://reactnavigation.org/docs/headers#replacing-the-title-with-a-custom-component
