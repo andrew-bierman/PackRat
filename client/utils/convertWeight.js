04c77c827f901067bf7a7560ac2101b3fcf7e40f
AnandHimanshu <hv7460@gmail.com>
2023-05-19 00:34:10 +0530

    fix (pack_table): calculation logic for pack items having different weighting units

--- client/utils/convertWeight.js
+++ client/utils/convertWeight.js
@@ -1,14 +1,14 @@
 export const convertWeight = (weight, fromUnit, toUnit) => {
-    const units = {
-      g: 1,
-      kg: 1000,
-      oz: 28.3495,
-      lb: 453.592,
-    };
-  
-    const weightInGrams = weight * units[fromUnit];
-    const convertedWeight = weightInGrams / units[toUnit];
-    
-    return convertedWeight;
+  const units = {
+    g: 1,
+    kg: 1000,
+    oz: 28.3495,
+    lb: 453.592,
   };
-  
+
+  const weightInGrams = weight * units[fromUnit];
+  const convertedWeight = weightInGrams / units[toUnit];
+  // console.log(weight, fromUnit, toUnit, weightInGrams, convertedWeight);
+
+  return convertedWeight;
+};
