cf99d448c94583665c0aebacf30620d59738a20b
AnandHimanshu <hv7460@gmail.com>
2023-05-20 14:41:42 +0530

    feat : added tab icon script for web platform

--- client/app/index.js
+++ client/app/index.js
@@ -25,6 +25,34 @@ export default function Index() {
   const reduxState = useSelector((state) => state);
 
   console.log("reduxState", reduxState)
+
+  if (Platform.OS === "web") {
+    // On android and iOS, trying this block of code will fail and go to the catch block.
+    try {
+      // Select the <head> tag under <body>
+      const headTag = document.querySelector("head");
+      // Create the <link/> tag.
+      const icon = document.createElement("link");
+      // Create the rel="" attribute
+      const attributeRel = document.createAttribute("rel");
+      // Assign the value "icon" to the rel attribute so that attributeRel becomes rel="icon"
+      attributeRel.value = "icon";
+      // Create the href="" attribute
+      const attributeHref = document.createAttribute("href");
+      // Assign your application icon path to the href attribute so that attributeHref becomes href="path/to/my/icon"
+      attributeHref.value =
+        "/assets/packrat-app-icon.png";
+      // Set the rel attibute to <link> so that the icon JS object becomes <link rel="icon"/>
+      icon.setAttributeNode(attributeRel);
+      // Set the href attibute to <link> so that the icon JS object becomes <link rel="icon" href="path/to/my/icon"/>
+      icon.setAttributeNode(attributeHref);
+      // Insert the <link [...] /> tag into the <head>
+      headTag.appendChild(icon);
+    } catch (e) {
+      //Browser tabs do not exist on android and iOS, so let's just do nothing here.
+    }
+  }
+
   return (
     <ScrollView>
       {Platform.OS === "web" ? (
