1ee6977eec4caaef0436c9e2818ac03682a6fbc2
Andrew Bierman <94939237+andrew-bierman@users.noreply.github.com>
2023-05-22 19:43:47 -0400

    switched trail and park logic to redux store for consistency

--- client/metro.config.js
+++ client/metro.config.js
@@ -2,6 +2,7 @@
 const { getDefaultConfig } = require("expo/metro-config");
 
 const defaultConfig = getDefaultConfig(__dirname);
+
 defaultConfig.resolver.assetExts.push("cjs");
 
 module.exports = defaultConfig;
