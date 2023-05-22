0c9c7f9d46606d1cddea232792ae7fbf1249f574
Andrew Bierman <94939237+andrew-bierman@users.noreply.github.com>
2023-05-21 22:54:18 -0400

    migrating photon route to backend

--- server/controllers/getOsmController.js
+++ server/controllers/getOsmController.js
@@ -1,5 +1,5 @@
-import fetch from "node-fetch";
 import osmtogeojson from "osmtogeojson";
+import axios from "axios";
 
 export const getOsm = async (req, res) => {
   console.log("req", req); // log the request body to see what it looks like
@@ -45,19 +45,16 @@ export const getOsm = async (req, res) => {
 
     console.log("overpassQuery", overpassQuery);
 
-    const response = await fetch(overpassUrl, {
-      method: "POST",
-      body: overpassQuery,
+    const response = await axios.post(overpassUrl, overpassQuery, {
       headers: { "Content-Type": "text/plain" },
     });
 
     console.log("response", response);
 
-    if (response.ok) {
-      const responseFormat = await response.json();
+    if (response.status === 200) {
+      const responseFormat = response.data;
       const geojsonData = osmtogeojson(responseFormat);
       res.send(geojsonData);
-      // res.send(response.text());
     } else {
       console.log(response.status, response.statusText);
       res.send({ message: "Error processing Overpass Data" });
@@ -65,5 +62,48 @@ export const getOsm = async (req, res) => {
   } catch (error) {
     console.error(error);
     res.status(500).send({ message: "Error retrieving Overpass Data" });
+  }
+};
+
+
+export const getPhotonResults = async (req, res) => {
+  const { searchString } = req.query;
+
+  if (!searchString) {
+    res.status(400).send({ message: "Invalid request parameters" });
+    return; // Return early to avoid further execution
+  }
+  
+  let params = {
+    q: searchString,
+    osm_tag: ["highway:footway", "highway:cycleway", "place"],
+    // osm_tag: "highway:footway",
+    // osm_tag: "highway:cycleway",
+    // osm_tag: "place",
+  };
+
+  const queryString = Object.entries(params)
+    .flatMap(([key, values]) =>
+      Array.isArray(values)
+        ? values.map((val) => `${key}=${val}`)
+        : `${key}=${values}`
+    )
+    .join("&");
+
+  console.log("queryString", queryString);
+
+  try {
+    const response = await axios.get(
+      `https://photon.komoot.io/api/?${queryString}`
+    );
+
+    console.log("response", response);
+
+    const resultsArray = response.data.features;
+
+    res.send(resultsArray);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({ message: "Error retrieving Photon results" });
   }
 };
