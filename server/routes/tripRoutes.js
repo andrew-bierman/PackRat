efd56fb57f389565f2f7fe7c9b8aac88043d9457
Andrew Bierman <94939237+andrew-bierman@users.noreply.github.com>
2023-05-23 14:16:19 -0400

    implementing bug fix for get routes for trips

--- server/routes/tripRoutes.js
+++ server/routes/tripRoutes.js
@@ -8,8 +8,9 @@ import {
 } from "../controllers/tripController.js";
 const router = express.Router();
 
-router.get("/", getTrips);
-router.get("/:packId", getTripById);
+// router.get("/", getPublicTrips); // getPublicTrips is not defined yet
+router.get("/:ownerId", getTrips);
+router.get("/t/:tripId", getTripById);
 router.post("/", addTrip);
 router.put("/", editTrip);
 router.delete("/", deleteTrip);
