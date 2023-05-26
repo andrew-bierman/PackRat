13ef9f22715423eceece3bd96642298b0ec49bda
Andrew Bierman <94939237+andrew-bierman@users.noreply.github.com>
2023-05-26 16:08:37 -0400

    temporary fix on mapbox css error on web

--- client/babel.config.js
+++ client/babel.config.js
@@ -2,8 +2,12 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ["babel-preset-expo"],
-    ignore: ["**/node_modules/mapbox-gl/dist/mapbox-gl.js"],
-    ignore: [ './node_modules/mapbox-gl/dist/mapbox-gl.js' ],
+    ignore: [
+      "**/node_modules/mapbox-gl/dist/mapbox-gl.js",
+      "**/node_modules/mapbox-gl/dist/mapbox-gl.js.map",
+      "./node_modules/mapbox-gl/dist/mapbox-gl.js",
+    ],
+
     plugins: [
       // NOTE: `expo-router/babel` is a temporary extension to `babel-preset-expo`.
       require.resolve("expo-router/babel"),
