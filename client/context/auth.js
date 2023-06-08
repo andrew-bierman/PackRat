9e7cab0b54942fd3b4d40f09ab339ed5b309db95
Andrew Bierman <94939237+andrew-bierman@users.noreply.github.com>
2023-06-07 21:44:32 -0400

    implementing auth provider for router to work with redux store

---
+++ client/context/auth.js
@@ -0,0 +1,49 @@
+import { useRouter, useSegments } from "expo-router";
+import { useSelector, useDispatch } from "react-redux";
+import React from "react";
+
+const AuthContext = React.createContext(null);
+
+// This hook can be used to access the user info.
+export function useAuth() {
+  return React.useContext(AuthContext);
+}
+
+// This hook will protect the route access based on user authentication.
+function useProtectedRoute(user) {
+  const segments = useSegments();
+  const router = useRouter();
+
+  React.useEffect(() => {
+    const inAuthGroup = segments[0] === "(auth)";
+
+    if (
+      // If the user is not signed in and the initial segment is not anything in the auth group.
+      !user &&
+      !inAuthGroup
+    ) {
+      // Redirect to the sign in page.
+      router.replace("/sign-in");
+    } else if (user && inAuthGroup) {
+      // if user is logged in and the route is protected
+      // Redirect away from the sign in page.
+      router.replace("/");
+    }
+  }, [user, segments]);
+}
+
+export function AuthProvider({ children }) {
+  const user = useSelector((state) => state.auth.user);
+  const dispatch = useDispatch();
+
+  useProtectedRoute(user);
+
+  const signIn = () => dispatch({ type: "SIGN_IN" });
+  const signOut = () => dispatch({ type: "SIGN_OUT" });
+
+  return (
+    <AuthContext.Provider value={{ user, signIn, signOut }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
