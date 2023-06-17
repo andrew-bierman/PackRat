1b6b92081756763e509e21f3ec5816f162a7ff8e
Muhammad Areeb Vohra <areebvohra@gmail.com>
2023-06-17 19:00:51 +0500

    fullscreen changes

--- client/components/TripCard.js
+++ client/components/TripCard.js
@@ -9,7 +9,7 @@ import { theme } from "../theme/index";
 
 import { useSelector, useDispatch } from "react-redux";
 import { addTrail, addPark } from "../store/dropdownStore";
-import MapContainer from "./map/MapContainer.web";
+import MapContainer from "./map/MapContainer";
 
 export default function TripCard({
   title,
