d1df60275de1c1101030d0c7871938651fef4d88
hassanbhattiofi <hasha361@gmail.com>
2023-06-18 18:34:23 +0500

    [Features] added base weight, calculation for water and food

--- server/models/itemModel.js
+++ server/models/itemModel.js
@@ -1,15 +1,11 @@
-import mongoose from "mongoose";
-import Pack from "./packModel.js";
-import myDB from "./dbConnection.js";
-
-const { Schema } = mongoose;
-
 const ItemSchema = new Schema(
   {
     name: { type: String, required: true },
     weight: { type: Number, required: true },
     quantity: { type: Number, required: true },
     unit: { type: String, required: true },
+    isFood: { type: Boolean, default: false },
+    isWater: { type: Boolean, default: false },
     owners: [{ type: Schema.Types.ObjectId, ref: "User", default: [] }],
     packs: [{ type: Schema.Types.ObjectId, ref: "Pack", default: [] }],
     createdAt: String,
--- server/models/packModel.js
+++ server/models/packModel.js
@@ -50,18 +50,7 @@ PackSchema.virtual("weightInOunces").get(function () {
   return Math.round(weightInOunces * 100) / 100; // Round to two decimal places
 });
 
-// PackSchema.virtual("total_weight").get(function () {
-//   if (this.items && this.items.length > 0 && this.items[0] instanceof Item) {
-//     return this.items.reduce((total, item) => {
-//       const itemWeight = item.weight * item.quantity;
-//       return total + itemWeight;
-//     }, 0);
-//   } else {
-//     return 0;
-//   }
-// });
-
-PackSchema.virtual("total_weight").get(function () {
+PackSchema.virtual("foodWeightInGrams").get(function () {
   if (this.items && this.items.length > 0 && this.items[0] instanceof Item) {
     return this.items.reduce((total, item) => {
       let itemWeight = item.weight;
@@ -76,11 +65,52 @@ PackSchema.virtual("total_weight").get(function () {
         // Convert ounces to grams: 1 ounce = 28.34952 grams
         itemWeight *= 28.34952;
       }
-      return total + itemWeight * item.quantity;
+      // Consider only items tagged as food
+      if (item.isFood) {
+        return total + itemWeight * item.quantity;
+      } else {
+        return total;
+      }
     }, 0);
   } else {
     return 0;
   }
+});
+
+PackSchema.virtual("waterVolumeInLiters").get(function () {
+  if (this.items && this.items.length > 0 && this.items[0] instanceof Item) {
+    return this.items.reduce((total, item) => {
+      let itemVolume = item.volume;
+      // Convert item volume to liters based on the unit
+      if (item.unit === "gal") {
+        // Convert gallons to liters: 1 gallon = 3.78541 liters
+        itemVolume *= 3.78541;
+      } else if (item.unit === "oz") {
+        // Convert ounces to liters: 1 ounce = 0.02957 liters
+        itemVolume *= 0.02957;
+      }
+      // Consider only items tagged as water
+      if (item.isWater) {
+        return total + itemVolume * item.quantity;
+      } else {
+        return total;
+      }
+    }, 0);
+  } else {
+    return 0;
+  }
+});
+
+PackSchema.virtual("total_weight").get(function () {
+  const foodWeightInGrams = this.foodWeightInGrams;
+  const waterVolumeInGrams = this.waterVolumeInLiters * 1000; // 1 liter = 1000 grams
+  return foodWeightInGrams + waterVolumeInGrams;
+});
+
+PackSchema.virtual("base_weight").get(function () {
+  const foodWeightInGrams = this.foodWeightInGrams;
+  const waterVolumeInGrams = this.waterVolumeInLiters * 1000; // 1 liter = 1000 grams
+  return foodWeightInGrams + waterVolumeInGrams;
 });
 
 PackSchema.set("toObject", { virtuals: true });
