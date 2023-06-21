6e98844a834a90fddbf3a535bdf8eacbdab1e850
hassanbhattiofi <hasha361@gmail.com>
2023-06-21 21:27:01 +0500

    [Completed] Total weight and base water water and food weight each item weight

--- server/models/packModel.js
+++ server/models/packModel.js
@@ -1,120 +1,159 @@
 import mongoose from "mongoose";
-import Item from "./itemModel.js";
 import myDB from "./dbConnection.js";
+import Item from "./itemModel.js";
 
 const { Schema } = mongoose;
 
-const PackSchema = new Schema({
-  name: { type: String, required: true },
-  items: [{ type: Schema.Types.ObjectId, ref: "Item" }],
-  owner_id: { type: Schema.Types.ObjectId, ref: "User" },
-  is_public: { type: Boolean },
-  favorited_by: [{ type: Schema.Types.ObjectId, ref: "User" }],
-  favorites_count: { type: Number },
-  createdAt: String,
-  owners: [{ type: Schema.Types.ObjectId, ref: "User", default: [] }],
-  grades: {
-    type: Object,
-    default: {
-      weight: "",
-      essentialItems: "",
-      redundancyAndVersatility: "",
+const PackSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    items: [{ type: Schema.Types.ObjectId, ref: "Item" }],
+    owner_id: { type: Schema.Types.ObjectId, ref: "User" },
+    is_public: { type: Boolean },
+    favorited_by: [{ type: Schema.Types.ObjectId, ref: "User" }],
+    favorites_count: { type: Number },
+    createdAt: String,
+    owners: [{ type: Schema.Types.ObjectId, ref: "User", default: [] }],
+    grades: {
+      type: Object,
+      default: {
+        weight: "", // Initialize with an empty string
+        essentialItems: "",
+        redundancyAndVersatility: "",
+      },
     },
-  },
-  scores: {
-    type: Object,
-    default: {
-      weightScore: 0,
-      essentialItemsScore: 0,
-      redundancyAndVersatilityScore: 0,
+    scores: {
+      type: Object,
+      default: {
+        weightScore: 0,
+        essentialItemsScore: 0,
+        redundancyAndVersatilityScore: 0,
+      },
     },
+    type: { type: String, default: "pack" },
   },
-  type: { type: String, default: "pack" },
-});
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
 
-PackSchema.virtual("weightInPounds").get(function () {
-  const totalWeightInGrams = this.total_weight;
-  const weightInPounds = totalWeightInGrams / 453.59237; // 1 pound = 453.59237 grams
-  return Math.round(weightInPounds * 100) / 100; // Round to two decimal places
-});
+// Virtuals
 
-PackSchema.virtual("weightInKilograms").get(function () {
-  const totalWeightInGrams = this.total_weight;
-  const weightInKilograms = totalWeightInGrams / 1000; // 1 kilogram = 1000 grams
-  return Math.round(weightInKilograms * 100) / 100; // Round to two decimal places
-});
+// Calculate the total weight of items in grams
+// PackSchema.virtual("totalWeightInGrams").get(function () {
+//   let totalWeight = 0;
+//   for (const item of this.items) {
+//     totalWeight += item.weightInGrams;
+//   }
+//   return totalWeight;
+// });
 
-PackSchema.virtual("weightInOunces").get(function () {
-  const totalWeightInGrams = this.total_weight;
+// Calculate the total weight of items in ounces
+PackSchema.virtual("totalWeightInOunces").get(function () {
+  const totalWeightInGrams = this.totalWeightInGrams;
   const weightInOunces = totalWeightInGrams / 28.34952; // 1 ounce = 28.34952 grams
   return Math.round(weightInOunces * 100) / 100; // Round to two decimal places
 });
 
-PackSchema.virtual("foodWeightInGrams").get(function () {
-  if (this.items && this.items.length > 0 && this.items[0] instanceof Item) {
-    return this.items.reduce((total, item) => {
-      let itemWeight = item.weight;
-      // Convert item weight to grams based on the unit
-      if (item.unit === "lb") {
-        // Convert pounds to grams: 1 pound = 453.59237 grams
-        itemWeight *= 453.59237;
-      } else if (item.unit === "kg") {
-        // Convert kilograms to grams: 1 kilogram = 1000 grams
-        itemWeight *= 1000;
-      } else if (item.unit === "oz") {
-        // Convert ounces to grams: 1 ounce = 28.34952 grams
-        itemWeight *= 28.34952;
-      }
-      // Consider only items tagged as food
-      if (item.isFood) {
-        return total + itemWeight * item.quantity;
-      } else {
-        return total;
-      }
-    }, 0);
-  } else {
-    return 0;
+// Calculate the total weight of items in pounds
+PackSchema.virtual("totalWeightInPounds").get(function () {
+  const totalWeightInGrams = this.totalWeightInGrams;
+  const weightInPounds = totalWeightInGrams / 453.59237; // 1 pound = 453.59237 grams
+  return Math.round(weightInPounds * 100) / 100; // Round to two decimal places
+});
+
+// Calculate the total weight of items in kilograms
+PackSchema.virtual("totalWeightInKilograms").get(function () {
+  const totalWeightInGrams = this.totalWeightInGrams;
+  const weightInKilograms = totalWeightInGrams / 1000; // 1 kilogram = 1000 grams
+  return Math.round(weightInKilograms * 100) / 100; // Round to two decimal places
+});
+
+// PackSchema.virtual("waterAndFood").get(function () {
+//   let waterAndFood = 0;
+//   for (const item of this.items) {
+//     if (item.isFood || item.isWater) {
+//       waterAndFood += item.weightInGrams;
+//     }
+//   }
+//   return waterAndFood;
+// });
+
+PackSchema.virtual("Total_weight").get(function () {
+  let totalWeight = this.baseWeight + this.waterAndFood;
+
+  // Iterate over each item in the pack
+  for (const item of this.items) {
+    if (item.unit === "lb") {
+      totalWeight += item.weight * 453.59237; // Convert pounds to grams
+    } else if (item.unit === "kg") {
+      totalWeight += item.weight * 1000; // Convert kilograms to grams
+    } else if (item.unit === "oz") {
+      totalWeight += item.weight * 28.34952; // Convert ounces to grams
+    } else {
+      totalWeight += item.weight; // Assume weight is already in grams if unit is not recognized
+    }
   }
+
+  return totalWeight;
 });
 
-PackSchema.virtual("waterVolumeInLiters").get(function () {
-  if (this.items && this.items.length > 0 && this.items[0] instanceof Item) {
-    return this.items.reduce((total, item) => {
-      let itemVolume = item.volume;
-      // Convert item volume to liters based on the unit
-      if (item.unit === "gal") {
-        // Convert gallons to liters: 1 gallon = 3.78541 liters
-        itemVolume *= 3.78541;
-      } else if (item.unit === "oz") {
-        // Convert ounces to liters: 1 ounce = 0.02957 liters
-        itemVolume *= 0.02957;
-      }
-      // Consider only items tagged as water
-      if (item.isWater) {
-        return total + itemVolume * item.quantity;
-      } else {
-        return total;
-      }
-    }, 0);
-  } else {
-    return 0;
+PackSchema.virtual("baseWeight").get(function () {
+  let baseWeight = 0;
+
+  // Iterate over each item in the pack
+  for (const item of this.items) {
+    if (item.isFood || item.isWater) {
+      continue; // Skip food and water items
+    }
+
+    // Add the weight of non-food and non-water items to the base weight
+    if (item.unit === "lb") {
+      baseWeight += item.weight * 453.59237; // Convert pounds to grams
+    } else if (item.unit === "kg") {
+      baseWeight += item.weight * 1000; // Convert kilograms to grams
+    } else if (item.unit === "oz") {
+      baseWeight += item.weight * 28.34952; // Convert ounces to grams
+    } else {
+      baseWeight += item.weight; // Assume weight is already in grams if unit is not recognized
+    }
   }
+
+  return baseWeight;
 });
 
-PackSchema.virtual("total_weight").get(function () {
-  const foodWeightInGrams = this.foodWeightInGrams;
-  const waterVolumeInGrams = this.waterVolumeInLiters * 1000; // 1 liter = 1000 grams
-  return foodWeightInGrams + waterVolumeInGrams;
+PackSchema.virtual("waterAndFood").get(function () {
+  let waterAndFood = 0;
+
+  // Iterate over each item in the pack
+  for (const item of this.items) {
+    if (item.isFood || item.isWater) {
+      waterAndFood += item.weightInGrams;
+    }
+  }
+
+  return waterAndFood;
 });
 
-PackSchema.virtual("base_weight").get(function () {
-  const foodWeightInGrams = this.foodWeightInGrams;
-  const waterVolumeInGrams = this.waterVolumeInLiters * 1000; // 1 liter = 1000 grams
-  return foodWeightInGrams + waterVolumeInGrams;
-});
+PackSchema.virtual("totalWeightInGrams").get(function () {
+  let totalWeight = this.baseWeight + this.waterAndFood;
 
-PackSchema.set("toObject", { virtuals: true });
-PackSchema.set("toJSON", { virtuals: true });
+  // Iterate over each item in the pack
+  for (const item of this.items) {
+    if (item.unit === "lb") {
+      totalWeight += item.weight * 453.59237; // Convert pounds to grams
+    } else if (item.unit === "kg") {
+      totalWeight += item.weight * 1000; // Convert kilograms to grams
+    } else if (item.unit === "oz") {
+      totalWeight += item.weight * 28.34952; // Convert ounces to grams
+    } else {
+      totalWeight += item.weight; // Assume weight is already in grams if unit is not recognized
+    }
+  }
+
+  return totalWeight;
+});
 
 const Pack = myDB.model("Pack", PackSchema);
 export default Pack;
