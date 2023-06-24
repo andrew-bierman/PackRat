208703537cdfeac94f0a643977b7d77c5c20b169
Andrew Bierman <94939237+andrew-bierman@users.noreply.github.com>
2023-06-24 17:24:55 -0400

    adding timestamps to all tables

--- server/models/tripModel.js
+++ server/models/tripModel.js
@@ -5,19 +5,22 @@ import myDB from "./dbConnection.js";
 
 const { Schema } = mongoose;
 
-const TripSchema = new Schema({
-  name: { type: String, required: true },
-  description: { type: String, required: true },
-  duration: { type: String, required: true },
-  weather: { type: String, required: true },
-  start_date: { type: Date, required: true },
-  end_date: { type: Date, required: true },
-  destination: { type: String, required: true },
-  owner_id: { type: Schema.Types.ObjectId, ref: "User" },
-  packs: { type: Schema.Types.ObjectId, ref: 'Pack' },
-  is_public: { type: Boolean },
-  type:{type:String,default: 'trip'}
-});
+const TripSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    description: { type: String, required: true },
+    duration: { type: String, required: true },
+    weather: { type: String, required: true },
+    start_date: { type: Date, required: true },
+    end_date: { type: Date, required: true },
+    destination: { type: String, required: true },
+    owner_id: { type: Schema.Types.ObjectId, ref: "User" },
+    packs: { type: Schema.Types.ObjectId, ref: "Pack" },
+    is_public: { type: Boolean },
+    type: { type: String, default: "trip" },
+  },
+  { timestamps: true }
+);
 
 const Trip = myDB.model("Trip", TripSchema);
 export default Trip;
