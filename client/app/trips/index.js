0bb997db131f33cceacd6c057be6f4c73cfee624
Andrew Bierman <94939237+andrew-bierman@users.noreply.github.com>
2023-07-02 14:49:55 -0400

    refactoring trips to be more logically structured

--- client/app/trips/index.js
+++ client/app/trips/index.js
@@ -1,134 +1,23 @@
-import Footer from "../../components/footer/Footer";
-import {
-  Stack,
-  Box,
-  Text,
-  ScrollView,
-  Button,
-  Input,
-  HStack,
-  VStack,
-} from "native-base";
+import CreateTrip from "../../components/trip/createTrip";
+
+import { Platform } from "react-native";
 import { Stack as Header } from "expo-router";
 
-import { theme } from "../../theme";
-import TripCard from "../../components/TripCard";
-import WeatherCard from "../../components/WeatherCard";
+export default function Trip() {
+  return Platform.OS === "web" ? (
+    <>
+      <Header.Screen
+        options={{
+          // https://reactnavigation.org/docs/headers#setting-the-header-title
+          title: "Create Trip",
+          // https://reactnavigation.org/docs/headers#adjusting-header-styles
 
-import { FontAwesome } from "@expo/vector-icons";
-import { FontAwesome5 } from "@expo/vector-icons";
-import { Platform, StyleSheet } from "react-native";
-
-import { useEffect, useState } from "react";
-
-import { useSelector } from "react-redux";
-
-import { GearList } from "../../components/GearList";
-
-import { MapContainer } from "../../components/map/MapContainer";
-
-import { CustomModal } from "../../components/modal";
-import { SaveTripContainer } from "../../components/trip/createTrip";
-
-export default function Trips() {
-  const [parksData, setParksData] = useState();
-  const [trails, setTrailsData] = useState();
-  const weatherObject = useSelector((state) => state.weather.weatherObject);
-  const weatherWeek = useSelector((state) => state.weather.weatherWeek);
-
-  const trailsObject = useSelector((state) => state.trails.trailNames);
-  const parksObject = useSelector((state) => state.parks.parkNames);
-
-  useEffect(() => {
-    setTrailsData(trailsObject);
-  }, [trailsObject]);
-
-  useEffect(() => {
-    setParksData(parksObject);
-  }, [parksObject]);
-
-  return (
-    <VStack>
-      {Platform.OS === "web" ? (
-        <Header.Screen
-          options={{
-            // https://reactnavigation.org/docs/headers#setting-the-header-title
-            title: "Home",
-          }}
-        />
-      ) : null}
-      <Box style={styles.mutualStyles}>
-        <Stack m={[0, 0, 12, 16]} style={{ gap: 25 }}>
-          <TripCard
-            title="Where are you heading?"
-            isSearch={true}
-            Icon={() => (
-              <FontAwesome
-                name="map"
-                size={20}
-                color={theme.colors.cardIconColor}
-              />
-            )}
-          />
-
-          <WeatherCard weatherObject={weatherObject} weatherWeek={weatherWeek} />
-
-          <TripCard
-            title="Nearby Trails"
-            value="Trail List"
-            isTrail={true}
-            data={trails || []}
-            Icon={() => (
-              <FontAwesome5
-                name="hiking"
-                size={20}
-                color={theme.colors.cardIconColor}
-              />
-            )}
-          />
-
-          <TripCard
-            title="Nearby Parks"
-            value="Parks List"
-            data={parksData}
-            Icon={() => (
-              <FontAwesome5
-                name="mountain"
-                size={20}
-                color={theme.colors.cardIconColor}
-              />
-            )}
-          />
-          <GearList />
-
-          <TripCard
-            Icon={() => (
-              <FontAwesome5
-                name="route"
-                size={24}
-                color={theme.colors.cardIconColor}
-              />
-            )}
-            title="Map"
-            isMap={true}
-           
-          />
-          <Box>
-            <SaveTripContainer />
-          </Box>
-        </Stack>
-      </Box>
-
-      {/* <Footer /> */}
-    </VStack>
+          // https://reactnavigation.org/docs/headers#replacing-the-title-with-a-custom-component
+        }}
+      />
+      <CreateTrip />
+    </>
+  ) : (
+    <CreateTrip />
   );
 }
-
-const styles = StyleSheet.create({
-  mutualStyles: {
-    backgroundColor: theme.colors.background,
-    flex: 1,
-    flexDirection: "column",
-    height: "100%",
-  },
-});
