4df00605d07bffb037a81547ccab0b1ecaf09ffb
Andrew Bierman <94939237+andrew-bierman@users.noreply.github.com>
2023-07-02 21:42:10 -0400

    adding opacity to hero

--- client/components/hero/index.js
+++ client/components/hero/index.js
@@ -39,6 +39,7 @@ const styles = StyleSheet.create({
     width: "100%",
     height: "100%",
     resizeMode: "cover",
+    opacity: 0.5,
     position: "absolute",
     // backgroundColor: theme.colors.primary,
   },
