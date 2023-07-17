b71c13c9a7dea7cb409f494521c8988cae402f19
aezazali1997 <aezazali.pro@gmail.com>
2023-07-17 23:22:45 +0500

    copy-public-pack

--- server/controllers/packController.js
+++ server/controllers/packController.js
@@ -188,6 +188,37 @@ export const deletePack = async (req, res) => {
   }
 };
 
+export const duplicatePublicPack = async (req, res) => {
+  try {
+    const { packId, ownerId } = req.body;
+
+    let pack = await Pack.findById(packId);
+    if (!pack) {
+      // Pack with specified ID not found
+      return res.status(404).json({ error: "Pack not found" });
+    }
+
+    pack = await Pack.create({
+      name: pack.name,
+      items: pack.items,
+      owner_id: pack.owner_id,
+      is_public: false,
+      favorited_by: pack.favorited_by,
+      favorites_count: pack.favorites_count,
+      createdAt: new Date().toISOString(),
+      owners: [pack.owners, ownerId],
+      grades: { ...pack.grades },
+      scores: { ...pack.scores },
+      type: pack.type,
+    });
+    res
+      .status(200)
+      .json({ msg: "pack was duplicated successfully", data: pack });
+  } catch (error) {
+    res.status(404).json({ msg: "Unable to duplicate pack" + error });
+  }
+};
+
 export const scorePack = async (req, res) => {
   try {
     const { packId } = req.params;
