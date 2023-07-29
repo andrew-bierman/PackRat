80a10aeb2500445c17be6047ad1379b014c5b96a
Andrew Bierman <94939237+andrew-bierman@users.noreply.github.com>
2023-07-29 18:55:54 -0400

    extracting add item modal to its own component

---
+++ client/components/pack/AddItemModal.js
@@ -0,0 +1,36 @@
+import { CustomModal } from "../modal";
+import { AddItem } from "../item/AddItem";
+
+export const AddItemModal = ({
+  currentPackId,
+  currentPack,
+  isAddItemModalOpen,
+  setIsAddItemModalOpen,
+}) => {
+  return (
+    <CustomModal
+      title="Add Item"
+      trigger="Add Item"
+      isActive={isAddItemModalOpen}
+      onTrigger={setIsAddItemModalOpen}
+      footerButtons={[
+        {
+          label: "Save",
+          color: "primary",
+          onClick: () => setIsAddItemModalOpen(false),
+        },
+        {
+          label: "Cancel",
+          color: "danger",
+          onClick: () => setIsAddItemModalOpen(false),
+        },
+      ]}
+    >
+      <AddItem
+        packId={currentPackId}
+        currentPack={currentPack}
+        setIsAddItemModalOpen={setIsAddItemModalOpen}
+      />
+    </CustomModal>
+  );
+};
