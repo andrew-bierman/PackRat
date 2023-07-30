40c00f8da1e7b556fa598da277f53807c2c1e1e5
Andrew Bierman <94939237+andrew-bierman@users.noreply.github.com>
2023-07-30 16:52:41 -0400

    merging into one dynamic profilecontainer

--- client/components/user/ProfileContainer.js
+++ client/components/user/ProfileContainer.js
@@ -19,21 +19,37 @@ import {
   fetchUserFavorites,
   selectAllFavorites,
 } from "../../store/favoritesStore";
+import { getUser } from '../../store/userStore';
 
-export default function ProfileContainer() {
+export default function ProfileContainer({ id = null }) {
   const dispatch = useDispatch();
-  useEffect(() => {
-    dispatch(fetchUserPacks(user?._id));
-    dispatch(fetchUserFavorites(user?._id));
-  }, [dispatch, user?._id]);
 
-  const user = useSelector((state) => state.auth.user);
-  const packsData = useSelector(selectAllPacks);
+  const authUser = useSelector((state) => state.auth.user);
+  const userStore = useSelector((state) => state.userStore);
+  const authStore = useSelector((state) => state.auth);
+  const allPacks = useSelector(selectAllPacks);
   const tripsData = useSelector((state) => state.trips);
-  const favoritesData = useSelector(selectAllFavorites);
+  const allFavorites = useSelector(selectAllFavorites);
 
-  const isLoading = useSelector((state) => state?.auth?.loading);
-  const error = useSelector((state) => state?.auth?.error);
+  const differentUser = id && id !== authUser._id;
+
+  useEffect(() => {
+    if (differentUser) {
+      dispatch(getUser(id));
+    } else {
+      dispatch(fetchUserPacks(authUser?._id));
+      dispatch(fetchUserFavorites(authUser?._id));
+    }
+  }, [dispatch, id, authUser, differentUser]);
+
+  const user = differentUser ? userStore.user : authUser;
+  
+  const isLoading = differentUser ? userStore.loading : authStore.loading;
+
+  const error = differentUser ? userStore.error : authStore.error;
+
+  const packsData = differentUser ? user?.packs : allPacks;
+  const favoritesData = differentUser ? user?.favorites : allFavorites;
 
   if (isLoading) return <Text>Loading...</Text>;
 
@@ -158,5 +174,8 @@ const styles = StyleSheet.create({
   },
   userDataContainer: {
     marginBottom: 25,
+    width: "100%",
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
