a5cc9652edfcf02c284095011a0a4dcd5902b10c
aezazali1997 <aezazali.pro@gmail.com>
2023-07-31 20:37:33 +0500

    completed

--- client/components/card/index.js
+++ client/components/card/index.js
@@ -59,34 +59,34 @@ export const CustomCard = ({
     return () => clearTimeout(resetCopyStateTimeout);
   };
 
-  if (type === "pack") {
-    return (
-      <Box
-        style={styles.mainContainer}
-        alignSelf="center"
-        alignItems={["center", "center", "flex-start", "flex-start"]}
-        w={["100%", "100%", "100%", "90%"]}
-        direction={["column", "column", "row", "row"]}
-        rounded="lg"
-        flexGrow={1}
-      >
-        <VStack space="4" width="100%" divider={<Divider />}>
-          <Box
-            px="4"
-            pt="4"
-            flexDirection="row"
-            justifyContent="space-between"
-            alignItems="center"
-          >
-            <Box>
-              <EditableInput
-                data={data}
-                title={title}
-                editTitle={editTitle}
-                setEditTitle={setEditTitle}
-                titleRef={titleRef}
-              />
-            </Box>
+  return (
+    <Box
+      style={styles.mainContainer}
+      alignSelf="center"
+      alignItems={["center", "center", "flex-start", "flex-start"]}
+      w={["100%", "100%", "100%", "90%"]}
+      direction={["column", "column", "row", "row"]}
+      rounded="lg"
+      flexGrow={1}
+    >
+      <VStack space="4" width="100%" divider={<Divider />}>
+        <Box
+          px="4"
+          pt="4"
+          flexDirection="row"
+          justifyContent="space-between"
+          alignItems="center"
+        >
+          <Box>
+            <EditableInput
+              data={data}
+              title={title}
+              editTitle={editTitle}
+              setEditTitle={setEditTitle}
+              titleRef={titleRef}
+            />
+          </Box>
+          <Box>
             {link && (
               <Box
                 flexDir={"row"}
@@ -119,80 +119,23 @@ export const CustomCard = ({
               </Box>
             )}
           </Box>
-          <Box
-            px="4"
-            style={{
-              alignItems: "center",
-              justifyContent: "center",
-            }}
-          >
-            {content}
-          </Box>
-          <Box px="4" pb="4">
-            {footer}
-          </Box>
-        </VStack>
-      </Box>
-    );
-  }
-
-  if (type === "trip") {
-    return (
-      <Box
-        style={styles.mainContainer}
-        alignSelf="center"
-        alignItems={["center", "center", "flex-start", "flex-start"]}
-        w={["100%", "100%", "100%", "90%"]}
-        direction={["column", "column", "row", "row"]}
-        rounded="lg"
-        flexGrow={1}
-      >
-        <VStack space="4" width="100%" divider={<Divider />}>
-          <Box
-            px="4"
-            pt="4"
-            flexDirection="row"
-            justifyContent="space-between"
-            alignItems="center"
-          >
-            <Box></Box>
-            {link && (
-              <Box>
-                {isCopied ? (
-                  <MaterialCommunityIcons
-                    name="check"
-                    size={24}
-                    color="green"
-                    onPress={handleCopyLink}
-                  />
-                ) : (
-                  <MaterialCommunityIcons
-                    name="link"
-                    size={24}
-                    color="black"
-                    onPress={handleCopyLink}
-                  />
-                )}
-              </Box>
-            )}
-          </Box>
-          <Box
-            px="4"
-            pb="4"
-            style={{
-              alignItems: "center",
-              justifyContent: "center",
-            }}
-          >
-            {content}
-          </Box>
-          <Box px="4" pb="4">
-            {footer}
-          </Box>
-        </VStack>
-      </Box>
-    );
-  }
+        </Box>
+        <Box
+          px="4"
+          pb="4"
+          style={{
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          {content}
+        </Box>
+        <Box px="4" pb="4">
+          {footer}
+        </Box>
+      </VStack>
+    </Box>
+  );
 };
 
 const styles = StyleSheet.create({
