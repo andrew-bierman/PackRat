81b605c78c78d2c6323ea7c6952140e482b9d241
Andrew Bierman <94939237+andrew-bierman@users.noreply.github.com>
2023-07-30 21:52:35 -0400

    refactoring

--- client/components/destination/index.js
+++ client/components/destination/index.js
@@ -23,101 +23,7 @@ import {
 } from "../../store/destinationStore";
 import { fetchWeather, fetchWeatherWeek } from "../../store/weatherStore";
 
-export const DestinationPage = () => {
-  const router = useRouter();
-
-  const dispatch = useDispatch();
-
-  const { destinationId, id, type } = useSearchParams();
-  const status = useSelector((state) => state.destination.status);
-  // const currentDestination = useSelector(
-  //   (state) => state.destination.currentDestination
-  // );
-
-  const photonDetailsStore = useSelector(
-    (state) => state.destination.photonDetails
-  );
-  const currentDestination = {
-    geoJSON: photonDetailsStore,
-  };
-
-  // const geoJSON = useSelector((state) => state.search.selectedSearchResult);
-  const geoJSON = currentDestination?.geoJSON;
-  const selectedSearchResult = useSelector(
-    (state) => state.destination.selectedSearchResult
-  );
-
-  // const weatherObject = useSelector((state) => state.weather.weatherObject);
-  // const weatherWeek = useSelector((state) => state.weather.weatherWeek);
-  const weatherObject = useSelector((state) => state.destination.weatherObject);
-  const weatherWeek = useSelector((state) => state.destination.weatherWeek);
-
-  useEffect(() => {
-    if (destinationId) {
-      console.log("destinationId", destinationId);
-      console.log("id", id);
-      console.log("type", type);
-
-      if (type && id) {
-        const matchPhotonFormattingForData = {
-          properties: {
-            osm_id: id,
-            osm_type: type,
-          },
-        };
-
-        // Fetch full details of the selected result from the Overpass API
-        dispatch(photonDetails(matchPhotonFormattingForData));
-      } else if (destinationId && !type && !id && destinationId !== "query") {
-        dispatch(getDestination(destinationId));
-      }
-    }
-  }, [destinationId]);
-
-  useEffect(() => {
-    if (geoJSON && geoJSON.features) {
-      const { coordinates } = geoJSON.features[0].geometry;
-      const [lon, lat] = coordinates;
-
-      console.log("lat", lat);
-
-      if (lat && lon) {
-        (async () => {
-          await dispatch(fetchWeather({ lat, lon }))
-            .then((res) => {
-              const payload = res.payload;
-              dispatch(setWeatherObject(payload));
-            })
-            .catch((err) => console.error(err));
-
-          await dispatch(fetchWeatherWeek({ lat, lon }))
-            .then((res) => {
-              const payload = res.payload;
-              const week = payload.list.slice(0, 4);
-              dispatch(setWeatherWeek(week));
-            })
-            .catch((err) => console.error(err));
-        })();
-      }
-    }
-  }, [geoJSON]);
-
-  if (!currentDestination) {
-    return null;
-  }
-
-  console.log("geoJSON ---->", geoJSON);
-
-  // let shape = convertPhotonGeoJsonToShape(geoJSON);
-  let shape = geoJSON ?? defaultShape;
-
-  const map = () => <MapContainer shape={shape} />;
-  const weather = () =>
-    weatherObject &&
-    weatherWeek && (
-      <WeatherCard weatherObject={weatherObject} weatherWeek={weatherWeek} />
-    );
-
+const DestinationHeader = ({ geoJSON, selectedSearchResult }) => {
   const properties = {
     ...geoJSON?.features[0]?.properties,
     ...selectedSearchResult?.properties,
@@ -145,25 +51,103 @@ export const DestinationPage = () => {
   }, {});
 
   return (
-    <View style={styles.container}>
-      <View style={styles.headerContainer}>
-        <Text style={styles.headerText}>
-          {name !== "N/A" ? name : "Destination"}
-        </Text>
-        <View style={styles.languageContainer}>
-          {Object.entries(languageNames).map(([key, value]) => (
-            <Text key={key} style={styles.languageText}>
-              {`${key.split(":")[1].toUpperCase()}: ${value}`}
-            </Text>
-          ))}
-        </View>
-        <Text style={styles.headerSubText}>
-          {county !== "N/A" && `${county}, `}
-          {state !== "N/A" && `${state}, `}
-          {country !== "N/A" ? country : ""}
-        </Text>
+    <View style={styles.headerContainer}>
+      <Text style={styles.headerText}>
+        {name !== "N/A" ? name : "Destination"}
+      </Text>
+      <View style={styles.languageContainer}>
+        {Object.entries(languageNames).map(([key, value]) => (
+          <Text key={key} style={styles.languageText}>
+            {`${key.split(":")[1].toUpperCase()}: ${value}`}
+          </Text>
+        ))}
       </View>
-      {/* Map Card */}
+      <Text style={styles.headerSubText}>
+        {county !== "N/A" && `${county}, `}
+        {state !== "N/A" && `${state}, `}
+        {country !== "N/A" ? country : ""}
+      </Text>
+    </View>
+  );
+};
+
+const WeatherData = ({ geoJSON }) => {
+  const dispatch = useDispatch();
+  const weatherObject = useSelector((state) => state.destination.weatherObject);
+  const weatherWeek = useSelector((state) => state.destination.weatherWeek);
+
+  useEffect(() => {
+    const fetchWeatherData = async () => {
+      if (geoJSON && geoJSON.features) {
+        const { coordinates } = geoJSON.features[0].geometry;
+        const [lon, lat] = coordinates;
+
+        if (lat && lon) {
+          try {
+            const weatherObjRes = await dispatch(fetchWeather({ lat, lon }));
+            const weatherWkRes = await dispatch(fetchWeatherWeek({ lat, lon }));
+            dispatch(setWeatherObject(weatherObjRes.payload));
+            dispatch(setWeatherWeek(weatherWkRes.payload.list.slice(0, 4)));
+          } catch (err) {
+            console.error(err);
+          }
+        }
+      }
+    };
+    fetchWeatherData();
+  }, [geoJSON]);
+
+  return weatherObject && weatherWeek ? (
+    <WeatherCard weatherObject={weatherObject} weatherWeek={weatherWeek} />
+  ) : null;
+};
+
+export const DestinationPage = () => {
+  const router = useRouter();
+
+  const dispatch = useDispatch();
+
+  const { destinationId, id, type } = useSearchParams();
+  const photonDetailsStore = useSelector(
+    (state) => state.destination.photonDetails
+  );
+  const currentDestination = {
+    geoJSON: photonDetailsStore,
+  };
+
+  const geoJSON = currentDestination?.geoJSON;
+  const selectedSearchResult = useSelector(
+    (state) => state.destination.selectedSearchResult
+  );
+
+  useEffect(() => {
+    if (destinationId) {
+      if (type && id) {
+        const matchPhotonFormattingForData = {
+          properties: {
+            osm_id: id,
+            osm_type: type,
+          },
+        };
+
+        dispatch(photonDetails(matchPhotonFormattingForData));
+      } else if (destinationId && !type && !id && destinationId !== "query") {
+        dispatch(getDestination(destinationId));
+      }
+    }
+  }, [destinationId]);
+
+  if (!currentDestination) {
+    return null;
+  }
+
+  let shape = geoJSON ?? defaultShape;
+
+  const map = () => <MapContainer shape={shape} />;
+
+  return (
+    <View style={styles.container}>
+      <DestinationHeader geoJSON={geoJSON} selectedSearchResult={selectedSearchResult} />
       <LargeCard
         title="Map"
         Icon={() => (
@@ -177,9 +161,7 @@ export const DestinationPage = () => {
         contentProps={{ shape }}
         type="map"
       />
-      {/* Weather Card */}
-      {/* <WeatherCard weatherObject={weatherObject} weatherWeek={weatherWeek} /> */}
-      {weather()}
+      <WeatherData geoJSON={geoJSON} />
     </View>
   );
 };
@@ -187,12 +169,12 @@ export const DestinationPage = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "flex-start", // changed from 'center' to align content to the top
+    justifyContent: "flex-start",
     padding: 20,
     paddingBottom: 12,
     paddingLeft: 16,
     width: "100%",
-    backgroundColor: theme.colors.background, // if you have a background color in your theme
+    backgroundColor: theme.colors.background,
   },
   headerContainer: {
     width: "100%",
@@ -209,16 +191,16 @@ const styles = StyleSheet.create({
   headerSubText: {
     color: theme.colors.white,
     fontSize: 16,
-    marginTop: 5, // added a margin for better visual separation
+    marginTop: 5,
   },
   languageContainer: {
-    flexDirection: "row", // to display languages in a line
-    flexWrap: "wrap", // if there are many languages, start a new line
-    marginTop: 10, // added a margin for better visual separation
+    flexDirection: "row",
+    flexWrap: "wrap",
+    marginTop: 10,
   },
   languageText: {
     color: theme.colors.white,
-    fontSize: 14, // smaller font size for languages
-    marginRight: 10, // spacing between languages
+    fontSize: 14,
+    marginRight: 10,
   },
 });
