f91a33a03b798597c982ad9f1239d778f4bc2002
vipin <mainvipin@gmail.com>
2023-08-03 12:40:10 +0530

    commit

--- client/components/feed/Feed.js
+++ client/components/feed/Feed.js
@@ -202,6 +202,7 @@ const Feed = ({ feedType = "public" }) => {
 
     return Platform.OS === "web" ? (
       <View style={styles.cardContainer}>
+        {console.log({data})}
         {feedSearchFilterComponent}
         {data?.map((item) => (
           <Card key={item._id} type={item.type} {...item} />
