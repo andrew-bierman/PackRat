d0c153b6e0e7304bc125654396d01672648972b7
Andrew Bierman <94939237+andrew-bierman@users.noreply.github.com>
2023-08-10 22:57:36 -0400

    current error on nativebase, but this layout would be easier to follow i think.

--- client/app/_layout.js
+++ client/app/_layout.js
@@ -7,24 +7,22 @@ import Navigation from "../screens/Navigation";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 
-// import { NativeBaseProvider } from "native-base";
-
 import { store, persistor } from "../store/store";
 
 import { AuthProvider } from "../context/auth";
-// import Footer from "../components/footer/Footer";
 import { ThemeProvider } from "../context/theme";
-// import config from "../theme/tamagui.config";
-// import  { TamaguiProvider } from 'tamagui'
-import { ThemeLayout } from "./ThemeLayout";
+import Footer from "../components/footer/Footer";
+
 export default function HomeLayout() {
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <AuthProvider>
-        <ThemeProvider>
-          <ThemeLayout />
-          </ThemeProvider>
+          {/* <ThemeProvider> */}
+            <Navigation />
+            <Slot />
+            {Platform.OS === "web" ? <Footer /> : null}
+          {/* </ThemeProvider> */}
         </AuthProvider>
       </PersistGate>
     </Provider>
