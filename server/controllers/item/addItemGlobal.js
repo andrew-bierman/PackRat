9b776cef08098f517ed64f52d4c81c9d377be1b2
raghavyuva <vikramnbhat15@gmail.com>
2023-08-12 18:10:14 +0530

    Moved Items controller into its directory with seperate smaller functions

---
+++ server/controllers/item/addItemGlobal.js
@@ -0,0 +1,78 @@
+import { ItemCategoryModel } from "../../models/itemCategory.js";
+import Item from "../../models/itemModel.js";
+import { ItemCategory, ItemCategoryEnum } from "../../utils/itemCategory.js";
+
+/**
+ * Adds an item globally.
+ * @param {object} req - The request object.
+ * @param {object} res - The response object.
+ * @return {object} The added item.
+ */
+export const addItemGlobal = async (req, res) => {
+    try {
+      const { name, weight, quantity, unit, type } = req.body;
+  
+      let category = null;
+      let newItem = null;
+      // need to look into below logic. open window issue
+      switch (type) {
+        case ItemCategoryEnum.FOOD: {
+          category = await ItemCategoryModel.findOne({
+            name: ItemCategoryEnum.FOOD,
+          });
+  
+          newItem = await Item.create({
+            name,
+            weight,
+            quantity,
+            unit,
+            category: category._id,
+            global: true,
+          });
+          newItem = await Item.findById(newItem.id).populate("category", "name");
+  
+          break;
+        }
+        case ItemCategoryEnum.WATER: {
+          category = await ItemCategoryModel.findOne({
+            name: ItemCategoryEnum.WATER,
+          });
+          newItem = await Item.create({
+            name,
+            weight,
+            quantity: 1,
+            unit,
+            category: category._id,
+            global: true,
+          });
+          newItem = await Item.findById(newItem.id).populate("category", "name");
+          break;
+        }
+        default: {
+          category = await ItemCategoryModel.findOne({
+            name: ItemCategoryEnum.ESSENTIALS,
+          });
+  
+          newItem = await Item.create({
+            name,
+            weight,
+            quantity,
+            unit,
+            category: category._id,
+            global: true,
+          });
+  
+          newItem = await Item.findById(newItem.id).populate("category", "name");
+  
+          break;
+        }
+      }
+  
+      res.status(200).json({
+        msg: "success",
+        newItem: newItem,
+      });
+    } catch (error) {
+      res.status(404).json({ msg: "Unable to add item", error: error.message });
+    }
+  };
