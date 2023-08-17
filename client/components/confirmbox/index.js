3923aadfd92895438d58c4b20df2b35ec7f8ce8c
aezazali1997 <aezazali.pro@gmail.com>
2023-08-17 22:02:22 +0500

    refactored global confirmation box

--- client/components/confirmbox/index.js
+++ client/components/confirmbox/index.js
@@ -1,45 +1,87 @@
-import React, { useState } from "react";
-import { View, Text, TouchableOpacity } from "react-native";
-import Modal from "react-native-modal";
+import React from "react";
+import { Modal as NBModal, Box, Heading, Button } from "native-base";
 import Icon from "react-native-vector-icons/MaterialIcons";
-export const GlobalConfirmationBox = ({
+
+const GlobalConfirmationBox = ({
   title,
   options,
   numOptions,
   type,
-  openModal,
-  closeModal,
-  isVisible,
+  onConfirm,
+  onCancel,
+  isActive,
+  buttonColor = "primary",
+  buttonText = "Confirm",
+  ...rest
 }) => {
+  const closeModal = () => {
+    if (onCancel) {
+      onCancel();
+    }
+  };
+
+  const handleOptionClick = (optionIndex) => {
+    if (onConfirm) {
+      onConfirm(options[optionIndex]);
+    }
+    closeModal();
+  };
+
   const renderOptions = () => {
     return options.map((option, index) => (
-      <TouchableOpacity key={index} onPress={closeModal}>
-        <Text>{option}</Text>
-      </TouchableOpacity>
+      <Button
+        key={index}
+        onPress={() => handleOptionClick(index)}
+        colorScheme={type}
+        mt={3}
+      >
+        {option}
+      </Button>
     ));
   };
 
   return (
-    <>
-      <Modal isVisible={isVisible}>
-        <View style={{ backgroundColor: "white", padding: 20 }}>
-          <Icon
-            name={
-              type === "success"
-                ? "check-circle"
-                : type === "error"
-                ? "error"
-                : "warning"
-            }
-            size={30}
-            color={
-              type === "success" ? "green" : type === "error" ? "red" : "yellow"
-            }
-          />
-          <Text style={{ fontSize: 20, marginTop: 10 }}>{title}</Text>
-          {renderOptions()}
-        </View>
-      </Modal>
-    </>
+    <NBModal
+      isOpen={isActive}
+      onClose={closeModal}
+      placement="center"
+      {...rest}
+    >
+      <NBModal.Content maxWidth="400px">
+        <NBModal.CloseButton />
+        <NBModal.Header>
+          <Box flexDirection="row" alignItems="center">
+            <Icon
+              name={
+                type === "success"
+                  ? "check-circle"
+                  : type === "error"
+                  ? "error"
+                  : "warning"
+              }
+              size={30}
+              color={
+                type === "success"
+                  ? "green"
+                  : type === "error"
+                  ? "red"
+                  : "yellow"
+              }
+            />
+            <Heading ml={2}>{title}</Heading>
+          </Box>
+        </NBModal.Header>
+        <NBModal.Body>
+          <Box>{renderOptions()}</Box>
+        </NBModal.Body>
+        <NBModal.Footer>
+          <Button colorScheme={buttonColor} onPress={onCancel}>
+            Cancel
+          </Button>
+        </NBModal.Footer>
+      </NBModal.Content>
+    </NBModal>
   );
 };
+
+export default GlobalConfirmationBox;
