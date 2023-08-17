ade652337e69296e3a6b8a93b4362e5e2ddd0bc4
Andrew Bierman <94939237+andrew-bierman@users.noreply.github.com>
2023-08-17 19:11:35 -0400

    adding html.tsx file

---
+++ client/app/+html.tsx
@@ -0,0 +1,40 @@
+import { ScrollViewStyleReset } from 'expo-router/html';
+import type { PropsWithChildren } from 'react';
+
+// This file is web-only and used to configure the root HTML for every
+// web page during static rendering.
+// The contents of this function only run in Node.js environments and
+// do not have access to the DOM or browser APIs.
+export default function Root({ children }: PropsWithChildren) {
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+
+        {/*
+          This viewport disables scaling which makes the mobile website act more like a native app.
+          However this does reduce built-in accessibility. If you want to enable scaling, use this instead:
+            <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover" />
+        */}
+        <meta
+          name="viewport"
+          content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1.00001,viewport-fit=cover"
+        />
+        {/*
+          Disable body scrolling on web. This makes ScrollView components work closer to how they do on native.
+          However, body scrolling is often nice to have for mobile web. If you want to enable it, remove this line.
+        */}
+        <ScrollViewStyleReset />
+
+        {/* Add any additional <head> elements that you want globally available on web... */}
+        <link rel="icon" type="image/png" sizes="32x32" href="../assets/packrat-app-icon.png"/>
+        <link rel="icon" type="image/png" sizes="16x16" href="../assets/packrat-app-icon.png"/>
+        <link rel="apple-touch-icon" sizes="180x180" href="../assets/packrat-app-icon.png"/>
+        <meta name="theme-color" content="#ffffff"/>
+
+      </head>
+      <body>{children}</body>
+    </html>
+  );
+}
