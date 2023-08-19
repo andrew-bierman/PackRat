b8fc3782cdfb3c41efae7bf0fa6ef9455dc12428
Andrew Bierman <abbierman101@gmail.com>
2023-08-19 14:42:28 -0400

    restoring back to responsive version

--- client/components/dashboard/index.js
+++ client/components/dashboard/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Dimensions, Platform, StyleSheet } from "react-native";
+import { Platform, StyleSheet } from "react-native";
 import { VStack, Box, ScrollView } from "native-base";
 import { theme } from "../../theme";
 import HeroBanner from "./HeroBanner";
@@ -8,12 +8,9 @@ import FeedPreview from "./FeedPreview";
 import Section from "./Section";
 import SectionHeader from "./SectionHeader";
 
-const { height, width } = Dimensions.get('window')
-
 const Dashboard = () => {
   return (
     <ScrollView contentContainerStyle={styles.content} horizontal={false}>
-
       <VStack
         style={[
           styles.container,
@@ -22,12 +19,10 @@ const Dashboard = () => {
       >
         <Box>
           <HeroBanner style={styles.cardContainer} />
-
           <Section>
             <SectionHeader iconName="add-circle-outline" text="Quick Actions" />
             <QuickActionsSection />
           </Section>
-
           <Section>
             <SectionHeader iconName="newspaper-outline" text="Feed" />
             <FeedPreview />
@@ -37,20 +32,18 @@ const Dashboard = () => {
     </ScrollView>
   );
 };
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexGrow: 1,
     backgroundColor: theme.colors.background,
-    width: width,
-    alignSelf: 'center'
-
+    width: "100%",
   },
   content: {
-    // flexGrow: 1,
-    // justifyContent: "flex-start",
-    // alignItems: "stretch",
-    // paddingHorizontal: 20,
+    flexGrow: 1,
+    justifyContent: "flex-start",
+    alignItems: "stretch",
+    paddingHorizontal: 20,
   },
   cardContainer: {
     flexDirection: "column",
@@ -59,5 +52,4 @@ const styles = StyleSheet.create({
     width: "100%",
   },
 });
-
 export default Dashboard;
