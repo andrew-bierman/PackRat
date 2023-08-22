c28e6946b659f32106ec4da7f40502e9ed913ec9
Raghavyuva <vikramnbhat15@gmail.com>
2023-08-23 01:03:51 +0530

    encryption and decryption logic enhanced with usage of mode, padding and iv

--- client/store/store.js
+++ client/store/store.js
@@ -49,25 +49,57 @@ const rootReducer = combineReducers({
   userStore,
 });
 
+function getUserEncryptionKey() {
+  return process.env.ENCRYPTION_KEY ?? "fd2aafcbf5c604e2613b68e9ef794dc5a18a2715927e9a968d3054c1f2be345a"
+}
+
+const userKey = getUserEncryptionKey();
+
 const persistConfig = {
   key: "root",
   storage: AsyncStorage,
   whitelist: ["auth"],
   transforms: [
     {
-      in: (state) => {
-        const encryptedState = CryptoJS.AES.encrypt(JSON.stringify(state), process.env.ENCRYPTION_KEY).toString();
-        return encryptedState;
-      },
-      out: (encryptedState) => {
-        const bytes = CryptoJS.AES.decrypt(encryptedState, process.env.ENCRYPTION_KEY);
-        const decryptedState = JSON.parse(bytes.toString(CryptoJS.enc.Utf8));
-        return decryptedState;
-      }
-    }
-  ]
-};
+      /**
+       * Encrypts the given state using AES encryption with the provided user key.
+       * @param {Object} state - The state to be encrypted.
+       * @param {string} key - The user key used for encryption.
+       * @return {string} The encrypted state in the format "IV:encryptedState".
+       */
+      in: (state, key) => {
+        const iv = CryptoJS.lib.WordArray.random(128 / 8);
 
+        const encryptedState = CryptoJS.AES.encrypt(JSON.stringify(state), userKey, {
+          iv: iv,
+          mode: CryptoJS.mode.CBC, 
+          padding: CryptoJS.pad.Pkcs7,
+        }).toString();
+
+        return iv.toString(CryptoJS.enc.Hex) + ":" + encryptedState;
+      },
+      
+      /**
+       * Decrypts the encrypted state with the given IV and returns the decrypted state.
+       * @param {string} encryptedStateWithIv - The encrypted state along with the IV, separated by a colon.
+       * @return {Object} The decrypted state.
+       */
+      out: (encryptedStateWithIv) => {
+        const parts = encryptedStateWithIv.split(":");
+        const iv = CryptoJS.enc.Hex.parse(parts[0]);
+        const encryptedState = parts[1];
+
+        const decryptedState = CryptoJS.AES.decrypt(encryptedState, userKey, {
+          iv: iv,
+          mode: CryptoJS.mode.CBC, 
+          padding: CryptoJS.pad.Pkcs7, 
+        }).toString(CryptoJS.enc.Utf8);
+
+        return JSON.parse(decryptedState);
+      },
+    },
+  ],
+};
 
 // create persisted reducer
 const persistedReducer = persistReducer(persistConfig, rootReducer);
