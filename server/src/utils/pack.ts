2cbc511881da88b24cd8ea13617208311363a5ca
Raghavyuva <vikramnbhat15@gmail.com>
2023-08-22 11:25:30 +0530

    Added JSDoc Strings to all the functions of server

--- server/src/utils/pack.ts
+++ server/src/utils/pack.ts
@@ -1,5 +1,18 @@
 import Pack from "../models/packModel";
 
+/**
+ * Creates a new pack validation.
+ *
+ * @param {string} name - The name of the pack.
+ * @param {string[]} items - The items in the pack.
+ * @param {string} owner_id - The ID of the owner.
+ * @param {boolean} is_public - Whether the pack is public.
+ * @param {string[]} favorited_by - The users who have favorited the pack.
+ * @param {number} favorites_count - The number of times the pack has been favorited.
+ * @param {string} createdAt - The creation date of the pack.
+ * @throws {Error} If any of the required fields are missing.
+ * @return {Promise<Object>} The created pack.
+ */
 export const packValidation = async ({
   name,
   items,
