6958e40047282eef5e295386656b4662e92de8d4
Raghavyuva <vikramnbhat15@gmail.com>
2023-08-22 11:29:02 +0530

    Added JSDoc Strings to utils folder

--- server/src/utils/osmFunctions/coordinateHandlers.ts
+++ server/src/utils/osmFunctions/coordinateHandlers.ts
@@ -1,5 +1,12 @@
 
 
+/**
+ * Creates an instance from the given coordinates.
+ *
+ * @param {any} Model - The model to create an instance from.
+ * @param {[number, number]} [lon, lat] - The longitude and latitude coordinates.
+ * @return {Promise<string>} The ID of the created instance.
+ */
 export async function createInstanceFromCoordinates(Model: any, [lon, lat]: [number, number]) {
   if (typeof lon !== "number" || typeof lat !== "number") {
     console.error("Invalid coordinate format");
@@ -11,6 +18,13 @@ export async function createInstanceFromCoordinates(Model: any, [lon, lat]: [num
   return instance._id;
 }
 
+/**
+ * Converts coordinates to instances using the given Model.
+ *
+ * @param {any} Model - The model to use for creating instances.
+ * @param {any} coordinates - The coordinates to convert to instances.
+ * @return {Promise<any[]>} An array of instances created from the coordinates.
+ */
 export async function coordinatesToInstances(Model: any, coordinates: any) {
   if (!Array.isArray(coordinates)) {
     console.error("Coordinates is not an array");
@@ -43,6 +57,13 @@ export async function coordinatesToInstances(Model: any, coordinates: any) {
   }
 }
 
+/**
+ * Handles the geometry data and creates instances based on the coordinates.
+ *
+ * @param {any} Model - The model to create instances from.
+ * @param {any} geometry - The geometry data to handle.
+ * @return {Promise<Array<any>>} An array of instance IDs.
+ */
 export async function handleGeometry(Model: any, geometry: any) {
   // console.log("handleGeometry");
   // console.log("handleGeometry geometry", geometry);
@@ -70,6 +91,12 @@ export async function handleGeometry(Model: any, geometry: any) {
   return nodes;
 }
 
+/**
+ * Handles the GeoJSON geometry by extracting the coordinates and returning them as an array.
+ *
+ * @param {any} geometry - The GeoJSON geometry object.
+ * @return {Array} - An array of coordinates extracted from the geometry.
+ */
 export function handleGeoJSONGeometry(geometry: any) {
   // console.log("handleGeometry");
   // console.log("handleGeometry geometry", geometry);
