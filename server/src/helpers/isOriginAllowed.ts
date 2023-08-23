34805ffad9052862c4b2663b219ed02c127bd121
Andrew Bierman <94939237+andrew-bierman@users.noreply.github.com>
2023-08-22 23:07:44 -0400

    hotfix on cors for pr preview

--- server/src/helpers/isOriginAllowed.ts
+++ server/src/helpers/isOriginAllowed.ts
@@ -10,12 +10,13 @@ export const isOriginAllowed = (origin: string, allowedOrigin: string) => {
   const allowedOriginDomain = new URL(allowedOrigin).hostname;
 
   // Patterns for different domain structures.
-  const packratPattern = 'packrat-pr-\\d+\\.onrender\\.com';
-  const packratDevPattern = 'packrat-dev-qj5h-pr-\\d+\\.onrender\\.com';
+  const packratMasterPattern = 'packrat.onrender.com';
+  const packratPrPattern = 'packrat-pr-\\d+\\.onrender\\.com';
+  const packratDevPrPattern = 'packrat-dev-pr-\\d+\\.onrender\\.com';
   const allowedPattern = allowedOriginDomain.replace(/\./g, '\\.');
 
   // Construct the final regex using the patterns.
-  const allowedOriginRegex = new RegExp(`^(${packratPattern}|${packratDevPattern}|${allowedPattern})$`);
+  const allowedOriginRegex = new RegExp(`^(${packratMasterPattern}|${packratPrPattern}|${packratDevPrPattern}|${allowedPattern})$`);
 
   return allowedOriginRegex.test(originDomain);
 };
