243ca7b5bd4ca3032ba0cecb634b20ccec61c25a
Andrew Bierman <94939237+andrew-bierman@users.noreply.github.com>
2023-08-26 13:22:39 -0400

    adding some utility functions and classes for responses

--- server/src/helpers/errorHandler.ts
+++ server/src/helpers/errorHandler.ts
@@ -1,4 +1,5 @@
-import express from "express";
+import express from 'express';
+import { ERRORS } from './errorClasses';
 
 /**
  * Handles errors that occur during the execution of a request.
@@ -7,69 +8,36 @@ import express from "express";
  * @param {express.Response} res - The response object.
  * @param {express.NextFunction} next - The next function in the middleware chain.
  */
-export function errorHandler(error: any, req: express.Request, res: express.Response, next: express.NextFunction) {
-    console.error(error);
-    let statusCode;
-    let errorMessage;
-    if (error instanceof Error) {
-        switch (error.name) {
-            case "ValidationError":
-                statusCode = 422;
-                errorMessage = "Invalid input data";
-                break;
-            case "NotFoundError":
-                statusCode = 404;
-                errorMessage = "Resource not found";
-                break;
-            case "UnauthorizedError":
-                statusCode = 401;
-                errorMessage = "Unauthorized access";
-                break;
-            case "PermissionDeniedError":
-                statusCode = 403;
-                errorMessage = "Permission denied";
-                break;
-            case "TimeoutError":
-                statusCode = 504;
-                errorMessage = "Request timed out";
-                break;
-            case "NetworkError":
-                statusCode = 500;
-                errorMessage = "Network error";
-                break;
-            case "InvalidCredentialsError":
-                statusCode = 401;
-                errorMessage = "Invalid credentials";
-                break;
-            case "AuthenticationError":
-                statusCode = 401;
-                errorMessage = "Authentication failed";
-                break;
-            case "InvalidTokenError":
-                statusCode = 401;
-                errorMessage = "Invalid token";
-                break;
-            case "InvalidRequestError":
-                statusCode = 400;
-                errorMessage = "Invalid request";
-                break;
-            case "InvalidResponseError":
-                statusCode = 500;
-                errorMessage = "Invalid response";
-                break;
-            case "MongoError":
-                statusCode = 500;
-                errorMessage = "MongoDB error";
-                break;
-            default:
-                statusCode = 500;
-                errorMessage = "It's not you, It's us having trouble processing your request";
-                break;
-        }
+export function errorHandler(
+  error: any,
+  req: express.Request,
+  res: express.Response,
+  next: express.NextFunction,
+) {
+  console.error(error);
+
+  // Check for new class-based errors
+  if (error.errorType) {
+    const mappedError = ERRORS[error.errorType] || ERRORS['AppError'];
+    if (mappedError) {
+      res
+        .status(mappedError.statusCode)
+        .json({ error: mappedError.message, code: mappedError.statusCode });
+      return;
     }
-    if (error.statusCode && error.message) {
-        statusCode = error.statusCode;
-        errorMessage = error.message;
-    }
-    res.status(statusCode).json({ error: errorMessage, code: statusCode });
+  }
+
+  // Check if the error is one of the old exported error objects
+  if (error.statusCode && error.message) {
+    res
+      .status(error.statusCode)
+      .json({ error: error.message, code: error.statusCode });
+    return;
+  }
+
+  // Default case
+  const AppError = ERRORS['AppError'];
+  res
+    .status(AppError.statusCode)
+    .json({ error: AppError.message, code: AppError.statusCode });
 }
--- server/src/helpers/responseHandler.ts
+++ server/src/helpers/responseHandler.ts
@@ -1,4 +1,17 @@
-export function responseHandler(res) {
-    const data = res.locals?.data ?? { message: "Success" };
-    return res.status(200).json(data);
+import express from 'express';
+
+export function responseHandler(
+  res: express.Response,
+  message: string = 'Success',
+) {
+  const data = res.locals?.data ?? { message: 'Success' };
+
+  // Set the custom header
+  res.setHeader('X-Response-Message', message);
+
+  res.setHeader('Access-Control-Expose-Headers', 'X-Response-Message');
+
+  console.log('message', message);
+
+  return res.status(200).json(data);
 }
