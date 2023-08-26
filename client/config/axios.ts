6d70d6e89d49bd853795ff4fa8fd4b85eb83fff3
Andrew Bierman <94939237+andrew-bierman@users.noreply.github.com>
2023-08-26 13:23:07 -0400

    testing out putting message in the response header instead of payload

--- client/config/axios.ts
+++ client/config/axios.ts
@@ -1,44 +1,63 @@
-import axios from "axios";
-import { api } from "~/constants/api";
-import { store } from "../store/store";
-// import { showMessage } from "react-native-flash-message";
+import axios from 'axios';
+import { api } from '~/constants/api';
+import { store } from '../store/store';
+import { InformUser } from '~/utils/ToastUtils';
 
 // Helper function to get the token
 const getTokenFromState = () => {
-    const state = store.getState();
-    return state?.auth?.user?.token || null;
+  const state = store.getState();
+  return state?.auth?.user?.token || null;
 };
 
 const requestInterceptor = (config) => {
-    config.baseURL = api;
+  config.baseURL = api;
 
-    const token = getTokenFromState();
-    if (token) {
-        config.headers.Authorization = `Bearer ${token}`;
-    }
+  const token = getTokenFromState();
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
 
-    return config;
+  return config;
 };
 
 const requestErrorInterceptor = (error) => {
-    return Promise.reject(error.response ? error.request : error);
+  return Promise.reject(error.response ? error.request : error);
 };
 
-// const responseErrorInterceptor = (error) => {
-//     if ("code" in error && error.code === "ERR_CANCELED") {
-//         return;
-//     }
+const responseInterceptor = (response) => {
+  // Check for the custom header
+  const responseMessage = response.headers['x-response-message'];
 
-//     const errorMessage = "message" in error ? error.message : "Something went wrong";
-//     showMessage({
-//         message: errorMessage,
-//         type: "danger",
-//     });
+  if (responseMessage) {
+    InformUser({
+      title: responseMessage,
+      placement: 'bottom',
+      duration: 3000,
+      style: { backgroundColor: response.status === 200 ? 'green' : 'red' },
+    });
+  }
 
-//     return Promise.reject(error);
-// };
+  return response;
+};
+
+const responseErrorInterceptor = (error) => {
+  if ('code' in error && error.code === 'ERR_CANCELED') {
+    return;
+  }
+
+  const errorMessage =
+    'message' in error ? error.message : 'Something went wrong';
+  InformUser({
+    title: errorMessage,
+    placement: 'bottom',
+    duration: 3000,
+    style: { backgroundColor: 'red' },
+  });
+
+  return Promise.reject(error);
+};
 
 axios.interceptors.request.use(requestInterceptor, requestErrorInterceptor);
-// axios.interceptors.response.use(undefined, responseErrorInterceptor);
+axios.interceptors.response.use(responseInterceptor, responseErrorInterceptor);
 
 export default axios;
