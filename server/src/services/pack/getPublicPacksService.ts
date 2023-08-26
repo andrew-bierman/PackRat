f45a87ad0932e03dd5e0f9b505e1f60e51df176c
Andrew Bierman <94939237+andrew-bierman@users.noreply.github.com>
2023-08-26 00:52:12 -0400

    improving query factoring

--- server/src/services/pack/getPublicPacksService.ts
+++ server/src/services/pack/getPublicPacksService.ts
@@ -1,13 +1,32 @@
 import Pack from '../../models/packModel';
 import { computeTotalWeightInGrams } from '../../utils/convertWeight';
 
+const SORT_OPTIONS = {
+  Favorite: { favorites_count: -1 },
+  Lightest: { total_weight: 1 },
+  Heaviest: { total_weight: -1 },
+  'Most Items': { 'items.length': -1 },
+  'Fewest Items': { 'items.length': 1 },
+  Oldest: { createdAt: 1 },
+  'Most Recent': { updatedAt: -1 },
+  'Highest Score': { 'scores.totalScore': -1 },
+  'Lowest Score': { 'scores.totalScore': 1 },
+  'A-Z': { name: 1 },
+  'Z-A': { name: -1 },
+  'Most Owners': { 'owners.length': -1 },
+};
+
+// Default sorting in case none of the above keys match
+// const DEFAULT_SORT = { _id: -1 };
+const DEFAULT_SORT = { createdAt: -1 };
+
 /**
  * Retrieves public packs based on the provided query parameter.
  *
  * @param {string} queryBy - Specifies how the public packs should be sorted.
  * @return {Promise<any[]>} An array of public packs.
  */
-export async function getPublicPacksService(queryBy: string) {
+export async function getPublicPacksService(queryBy: string = null) {
   try {
     const publicPacksPipeline: any = [
       {
@@ -50,6 +69,7 @@ export async function getPublicPacksService(queryBy: string) {
           favorited_by: { $first: '$favorited_by' },
           favorites_count: { $first: '$favorites_count' },
           createdAt: { $first: '$createdAt' },
+          updatedAt: { $first: '$updatedAt' },
           owners: { $first: '$owners' },
           grades: { $first: '$grades' },
           scores: { $first: '$scores' },
@@ -60,11 +80,8 @@ export async function getPublicPacksService(queryBy: string) {
       },
     ];
 
-    if (queryBy === 'Favorite') {
-      publicPacksPipeline.push({ $sort: { favorites_count: -1 } });
-    } else {
-      publicPacksPipeline.push({ $sort: { _id: -1 } });
-    }
+    const sortCriteria = SORT_OPTIONS[queryBy] || DEFAULT_SORT;
+    publicPacksPipeline.push({ $sort: sortCriteria });
 
     const publicPacks = await Pack.aggregate(publicPacksPipeline);
 
