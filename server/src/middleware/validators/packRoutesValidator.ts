ee35e352f0906368a9f222c3685a99b9e109fa55
raghavyuva <vikramnbhat15@gmail.com>
2023-08-28 23:29:23 +0530

    Zod validation integrated by replacing celebrate library

--- server/src/middleware/validators/packRoutesValidator.ts
+++ server/src/middleware/validators/packRoutesValidator.ts
@@ -1,47 +1,75 @@
-import { celebrate, Joi, Segments } from 'celebrate';
+import { z } from "zod";
+import { Request } from "express";
+
+type ZodParserFunction = (schema: z.ZodSchema<any>, input: any) => any;
 
 /**
- * Creates a Joi validation rule for ObjectId strings.
+ * Creates a Zod validation rule for ObjectId strings.
  *
  * @param {string} [message="valid id"] - The validation error message.
- * @return {Joi.Schema} A Joi validation rule for ObjectId strings.
+ * @return {z.ZodString} A Zod validation rule for ObjectId strings.
  */
-export const JoiObjectId = (message = 'valid id') =>
-  Joi.string().regex(/^[0-9a-fA-F]{24}$/, message);
+export const JoiObjectId = (message: any = "valid id"): z.ZodString =>
+  z.string().regex(/^[0-9a-fA-F]{24}$/, { message });
 
-export const getPacks = celebrate({
-  [Segments.PARAMS]: Joi.object().keys({
-    ownerId: JoiObjectId().required(),
-  }),
-});
-export const getPackById = celebrate({
-  [Segments.PARAMS]: Joi.object().keys({
-    packId: JoiObjectId().required(),
-  }),
-});
-export const addPack = celebrate({
-  [Segments.BODY]: Joi.object().keys({
-    name: Joi.string().required(),
-    owner_id: JoiObjectId().required(),
-    is_public: Joi.boolean(),
-  }),
-});
-export const editPack = celebrate({
-  [Segments.BODY]: Joi.object().keys({
-    _id: JoiObjectId().required(),
-    name: Joi.string(),
-    is_public: Joi.boolean(),
-  }),
-});
-export const deletePack = celebrate({
-  [Segments.BODY]: Joi.object().keys({
-    packId: JoiObjectId().required(),
-  }),
-});
-export const duplicatePublicPack = celebrate({
-  [Segments.BODY]: Joi.object().keys({
-    packId: JoiObjectId().required(),
-    ownerId: JoiObjectId().required(),
-    items: Joi.array().optional(),
-  }),
-});
+const zodParser: ZodParserFunction = (schema, input) => schema.parse(input);
+
+export const getPacks = (req: Request) => {
+  zodParser(
+    z.object({
+      ownerId: JoiObjectId(),
+    }),
+    req.params
+  );
+};
+
+export const getPackById = (req: Request) => {
+  zodParser(
+    z.object({
+      packId: JoiObjectId(),
+    }),
+    req.params
+  );
+};
+
+export const addPack = (req: Request) => {
+  zodParser(
+    z.object({
+      name: z.string().nonempty(),
+      owner_id: JoiObjectId(),
+      is_public: z.boolean(),
+    }),
+    req.body
+  );
+};
+
+export const editPack = (req: Request) => {
+  zodParser(
+    z.object({
+      _id: JoiObjectId(),
+      name: z.string().nonempty(),
+      is_public: z.boolean(),
+    }),
+    req.body
+  );
+};
+
+export const deletePack = (req: Request) => {
+  zodParser(
+    z.object({
+      packId: JoiObjectId(),
+    }),
+    req.body
+  );
+};
+
+export const duplicatePublicPack = (req: Request) => {
+  zodParser(
+    z.object({
+      packId: JoiObjectId(),
+      ownerId: JoiObjectId(),
+      items: z.array(z.object({})),
+    }),
+    req.body
+  );
+};
