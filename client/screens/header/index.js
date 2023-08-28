c6b314ac315b156cfe9c364aec78382dbe0c3746
Andrew Bierman <94939237+andrew-bierman@users.noreply.github.com>
2023-08-27 23:28:47 -0400

    setting up progress bar in header to track request percentage

---
+++ client/screens/header/index.js
@@ -0,0 +1,11 @@
+import Navigation from '../Navigation';
+import ProgressBarComponent from '~/components/progress';
+
+export default function Header() {
+  return (
+    <>
+      <Navigation />
+      <ProgressBarComponent />
+    </>
+  );
+}
