3c4a431b72d00f1087ec2fc091247ad08fa17aaf
umair <umair@getbee.com>
2023-09-03 11:49:20 +0500

    add check for favorite count so it should not be negative

--- server/src/services/favorite/addToFavoriteService.ts
+++ server/src/services/favorite/addToFavoriteService.ts
@@ -2,18 +2,21 @@ import User from "../../models/userModel";
 import Pack from "../../models/packModel";
 
 export const addToFavoriteService = async (packId, userId) => {
-    const exists = await User.find(
-        { favorites: { $in: [packId] } },
-        { _id: userId }
+  const exists = await User.find(
+    { favorites: { $in: [packId] } },
+    { _id: userId }
+  );
+  console.log(exists);
+  if (exists.length > 0) {
+    await User.updateOne({ _id: userId }, { $pull: { favorites: packId } });
+    await Pack.updateOne({ _id: packId }, { $pull: { favorited_by: userId } });
+    await Pack.updateOne(
+      { _id: packId, favorites_count: { $gt: 0 } },
+      { $inc: { favorites_count: -1 } }
     );
-
-    if (exists.length > 0) {
-        await User.updateOne({ _id: userId }, { $pull: { favorites: packId } });
-        await Pack.updateOne({ _id: packId }, { $pull: { favorited_by: userId } });
-        await Pack.updateOne({ _id: packId }, { $inc: { favorites_count: -1 } });
-    } else {
-        await User.updateOne({ _id: userId }, { $push: { favorites: packId } });
-        await Pack.updateOne({ _id: packId }, { $push: { favorited_by: userId } });
-        await Pack.updateOne({ _id: packId }, { $inc: { favorites_count: 1 } });
-    }
+  } else {
+    await User.updateOne({ _id: userId }, { $push: { favorites: packId } });
+    await Pack.updateOne({ _id: packId }, { $push: { favorited_by: userId } });
+    await Pack.updateOne({ _id: packId }, { $inc: { favorites_count: 1 } });
+  }
 };
