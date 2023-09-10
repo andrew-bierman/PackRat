93743e18093678712c67bff53293687c197da5f7
Andrew Bierman <94939237+andrew-bierman@users.noreply.github.com>
2023-09-10 14:42:07 -0400

    Rough draft of shared packrat packages folder

---
+++ packages/src/validations/itemRoutesValidator.ts
@@ -0,0 +1,42 @@
+import { z } from 'zod';
+import JoiObjectId from './objectIdValidator';
+
+export const getItems = z.object({
+  packId: JoiObjectId(),
+});
+
+export const getItemById = z.object({
+  _id: JoiObjectId(),
+});
+
+export const addItem = z.object({
+  name: z.string().nonempty(),
+  weight: z.string().nonempty(),
+  quantity: z.string().nonempty(),
+  unit: z.string().nonempty(),
+  packId: JoiObjectId(),
+  type: z.string().optional(),
+  ownerId: z.string().optional(),
+});
+
+export const editItem = z.object({
+  _id: JoiObjectId(),
+  name: z.string().nonempty(),
+  weight: z.string().nonempty(),
+  quantity: z.string().nonempty(),
+  unit: z.string().nonempty(),
+  type: z.string(),
+});
+
+export const deleteItem = z.object({
+  itemId: JoiObjectId().nonempty(),
+  packId: JoiObjectId().nonempty(),
+});
+
+export const addItemGlobal = z.object({
+  name: z.string().nonempty(),
+  weight: z.string().nonempty(),
+  quantity: z.string().nonempty(),
+  unit: z.string().nonempty(),
+  type: z.string().optional(),
+});
