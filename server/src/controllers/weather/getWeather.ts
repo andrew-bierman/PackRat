115434276e6e6b3d204527551f7402d93e7cb577
raghavyuva <vikramnbhat15@gmail.com>
2023-09-13 15:27:41 +0530

    Generalized service files for trpc and rest functions

--- server/src/controllers/weather/getWeather.ts
+++ server/src/controllers/weather/getWeather.ts
@@ -1,4 +1,4 @@
-import axios from 'axios';
+import { getWeatherService } from '../../services/weather/getWeatherService';
 import { RetrievingWeatherFromOpenWeatherError } from '../../helpers/errors';
 import { responseHandler } from '../../helpers/responseHandler';
 import { publicProcedure } from '../../trpc';
@@ -11,23 +11,8 @@ import { z } from 'zod';
  * @return {Object} The weather data retrieved from OpenWeather API.
  */
 export const getWeather = async (req, res, next) => {
-  const root = process.env.WEATHER_URL;
-  const OPENWEATHER_KEY = process.env.OPENWEATHER_KEY;
-  const latParams = req.query.lat;
-  const lonParams = req.query.lon;
-  const unitParams = 'imperial';
-  const apiParams = true;
-
-  let params = '?';
-  if (latParams) params += `lat=${latParams}`;
-  if (lonParams) params += `&lon=${lonParams}`;
-  if (unitParams) params += `&units=${unitParams}`;
-  if (apiParams) params += `&appid=${OPENWEATHER_KEY}`;
-
-  const url = root + params;
-
   try {
-    const response = await axios.get(url);
+    const response = await getWeatherService(req.query.lat, req.query.lon);
     res.locals.data = response.data;
     responseHandler(res);
   } catch (error) {
@@ -39,20 +24,7 @@ export const getWeather = async (req, res, next) => {
 export function getWeatherRoute() {
   return publicProcedure.input(z.object({ lat: z.number(), lon: z.number() })).query(async (opts) => {
     const { lat, lon } = opts.input;
-    const root = process.env.WEATHER_URL;
-    const OPENWEATHER_KEY = process.env.OPENWEATHER_KEY;
-    const latParams = lat
-    const lonParams = lon
-    const unitParams = 'imperial';
-    const apiParams = true;
-    let params = '?';
-    if (latParams) params += `lat=${latParams}`;
-    if (lonParams) params += `&lon=${lonParams}`;
-    if (unitParams) params += `&units=${unitParams}`;
-    if (apiParams) params += `&appid=${OPENWEATHER_KEY}`;
-
-    const url = root + params;
-    const response = await axios.get(url);
+    const response = await getWeatherService(lat, lon);
     return response.data;
   })
 }
---
+++ server/src/services/parks/getParksService.ts
@@ -0,0 +1,29 @@
+import { oneEntity } from '../../utils/oneEntity';
+import { RetrievingParksDataError } from '../../helpers/errors';
+
+export async function getParksService(abbrStates) {
+    const abbrState = await oneEntity(abbrStates);
+
+    const X_RAPIDAPI_KEY = process.env.X_RAPIDAPI_KEY;
+    const NPS_API = process.env.NPS_API;
+    const PARKS_HOST = process.env.PARKS_HOST;
+
+    const host = `${PARKS_HOST}?stateCode=${abbrState}`;
+
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-Api-Key': `${NPS_API}`,
+            'X-RapidAPI-Key': `${X_RAPIDAPI_KEY}`,
+            'X-RapidAPI-Host': 'jonahtaylor-national-park-service-v1.p.rapidapi.com',
+            'User-Agent': 'PackRat',
+        },
+    };
+
+    return await fetch(host, options)
+        .then(async (res) => await res.json())
+        .then((json) => {
+            return json
+        })
+        .catch(() => { throw RetrievingParksDataError })
+}
