4484f7f10445dd802395bb97475bea6a5343ae5f
raghavyuva <vikramnbhat15@gmail.com>
2023-09-13 15:27:41 +0530

    Generalized service files for trpc and rest functions

--- server/src/controllers/getTrail/getTrails.ts
+++ server/src/controllers/getTrail/getTrails.ts
@@ -1,7 +1,7 @@
 import { publicProcedure } from '../../trpc';
-import { RetrievingTrailsDataError } from '../../helpers/errors';
 import { responseHandler } from '../../helpers/responseHandler';
-import { z } from 'zod';
+import * as validators from "@packrat/packages"
+import { getTrailsService } from '../../services/trails/getTrailsService';
 
 const fetch = async (...args) =>
   import('node-fetch').then(async ({ default: fetch }) =>
@@ -16,7 +16,6 @@ const fetch = async (...args) =>
 export const getTrails = async (req, res, next) => {
   const radiusParams = 25;
   const activityParams = true;
-  const X_RAPIDAPI_KEY = process.env.X_RAPIDAPI_KEY;
   const {
     administrative_area_level_1,
     country,
@@ -24,47 +23,18 @@ export const getTrails = async (req, res, next) => {
     latitude,
     longitude,
   } = req.body;
-  const state = administrative_area_level_1;
-  const city = locality;
-
-  let paramsConditional = '';
-
-  const root = process.env.GET_TRAIL_ROOT_URL;
-
-  if (latitude) paramsConditional += `lat=${latitude}`;
-  if (longitude) paramsConditional += `&lon=${longitude}`;
-
-  if (city) paramsConditional += `&q-city_cont=${city.replace(/\s/g, '')}`;
-
-  if (radiusParams) paramsConditional += `&radius=${radiusParams}`;
-  if (activityParams) {
-    paramsConditional += '&q-activities_activity_type_name_eq=hiking';
-  }
-
-  const url1 = root + paramsConditional;
-
-  const url =
-    'https://trailapi-trailapi.p.rapidapi.com/activity/?lat=34.1&lon=-105.2&q-city_cont=Denver&radius=25&q-activities_activity_type_name_eq=hiking';
-
-  const options = {
-    method: 'GET',
-    headers: {
-      'X-RapidAPI-Key': `${X_RAPIDAPI_KEY}`,
-      'X-RapidAPI-Host': 'trailapi-trailapi.p.rapidapi.com',
-    },
-  };
-
-  await fetch(url1, options)
-    .then(async (res) => await res.json())
-    .then((json) => {
-      res.locals.data = json;
-      responseHandler(res);
-    })
-    .catch((_err) => {
-      next(RetrievingTrailsDataError);
-    });
+  const response = await getTrailsService(
+    administrative_area_level_1,
+    country,
+    locality,
+    latitude,
+    longitude,
+    radiusParams,
+    activityParams,
+  )
+  res.locals.data = response;
+  responseHandler(res);
 };
-
 
 export function getTrailsRoute() {
   return publicProcedure.input(z.object({
@@ -77,46 +47,7 @@ export function getTrailsRoute() {
     .mutation(async (opts) => {
       const radiusParams = 25;
       const activityParams = true;
-      const X_RAPIDAPI_KEY = process.env.X_RAPIDAPI_KEY;
       const { administrative_area_level_1, country, locality, latitude, longitude } = opts.input;
-
-      const state = administrative_area_level_1;
-      const city = locality;
-
-      let paramsConditional = '';
-
-      const root = process.env.GET_TRAIL_ROOT_URL;
-
-      if (latitude) paramsConditional += `lat=${latitude}`;
-      if (longitude) paramsConditional += `&lon=${longitude}`;
-
-      if (city) paramsConditional += `&q-city_cont=${city.replace(/\s/g, '')}`;
-
-      if (radiusParams) paramsConditional += `&radius=${radiusParams}`;
-      if (activityParams) {
-        paramsConditional += '&q-activities_activity_type_name_eq=hiking';
-      }
-
-      const url1 = root + paramsConditional;
-
-      const url =
-        'https://trailapi-trailapi.p.rapidapi.com/activity/?lat=34.1&lon=-105.2&q-city_cont=Denver&radius=25&q-activities_activity_type_name_eq=hiking';
-
-      const options = {
-        method: 'GET',
-        headers: {
-          'X-RapidAPI-Key': `${X_RAPIDAPI_KEY}`,
-          'X-RapidAPI-Host': 'trailapi-trailapi.p.rapidapi.com',
-        },
-      };
-
-      await fetch(url1, options)
-        .then(async (res) => await res.json())
-        .then((json) => {
-          return json
-        })
-        .catch((_err) => {
-          throw RetrievingTrailsDataError
-        });
+      return await getTrailsService(administrative_area_level_1, country, locality, latitude, longitude, radiusParams, activityParams);
     });
 }
