86be10c4912b1bf5f7b9e95cb0e813103b384a80
witehound <netvid00@gmail.com>
2023-09-18 19:55:07 +0100

    fixed protected route

--- client/app/(app)/_layout.js
+++ client/app/(app)/_layout.js
@@ -1,11 +1,8 @@
+import { AuthProvider } from 'context/auth';
 import { Redirect, Stack } from 'expo-router';
-import { useSelector } from 'react-redux';
 
 export default function AppLayout() {
-  const user = useSelector((state) => state.auth.user);
-  if (!user) {
-    return <Redirect href="/sign-in" />;
-  }
-
-  return <Stack />;
+  <AuthProvider>
+    <Stack />
+  </AuthProvider>;
 }
