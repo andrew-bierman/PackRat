9cecd3f477bc65412e9995825cb8f6acc614e26a
Andrew Bierman <94939237+andrew-bierman@users.noreply.github.com>
2023-09-18 21:06:39 -0400

    missing imports fixed

--- server/src/controllers/getTrail/getTrails.ts
+++ server/src/controllers/getTrail/getTrails.ts
@@ -1,7 +1,8 @@
 import { publicProcedure } from '../../trpc';
 import { responseHandler } from '../../helpers/responseHandler';
-import * as validators from "@packrat/packages"
+import * as validators from '@packrat/packages';
 import { getTrailsService } from '../../services/trails/getTrailsService';
+import { z } from 'zod';
 
 const fetch = async (...args) =>
   import('node-fetch').then(async ({ default: fetch }) =>
@@ -31,23 +32,40 @@ export const getTrails = async (req, res, next) => {
     longitude,
     radiusParams,
     activityParams,
-  )
+  );
   res.locals.data = response;
   responseHandler(res);
 };
 
 export function getTrailsRoute() {
-  return publicProcedure.input(z.object({
-    administrative_area_level_1: z.string(),
-    country: z.string(),
-    locality: z.string(),
-    latitude: z.number(),
-    longitude: z.number(),
-  }))
+  return publicProcedure
+    .input(
+      z.object({
+        administrative_area_level_1: z.string(),
+        country: z.string(),
+        locality: z.string(),
+        latitude: z.number(),
+        longitude: z.number(),
+      }),
+    )
     .mutation(async (opts) => {
       const radiusParams = 25;
       const activityParams = true;
-      const { administrative_area_level_1, country, locality, latitude, longitude } = opts.input;
-      return await getTrailsService(administrative_area_level_1, country, locality, latitude, longitude, radiusParams, activityParams);
+      const {
+        administrative_area_level_1,
+        country,
+        locality,
+        latitude,
+        longitude,
+      } = opts.input;
+      return await getTrailsService(
+        administrative_area_level_1,
+        country,
+        locality,
+        latitude,
+        longitude,
+        radiusParams,
+        activityParams,
+      );
     });
 }
