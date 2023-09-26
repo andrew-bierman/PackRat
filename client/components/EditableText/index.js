4fdaa647ad02cf9694831d396be1ff245672d52d
aezazali1997 <aezazali.pro@gmail.com>
2023-09-26 08:12:51 +0500

    added skeleton on pack details

--- client/components/EditableText/index.js
+++ client/components/EditableText/index.js
@@ -23,48 +23,45 @@ export const EditableInput = ({
   const dispatch = useDispatch();
   return (
     <>
-      {loading ? (
-        <div>Loading</div>
-      ) : (
-        <TextInput
-          style={{
-            fontSize: '20px',
-            fontWeight: 'bold',
-            color: theme.colors.textPrimary,
-          }}
-          ref={titleRef}
-          editable={editTitle}
-          onChange={(e) => {
-            setHeaderTitle(e.target.value);
-          }}
-          value={headerTitle}
-          onBlur={() => {
-            if (data.type === 'pack') {
-              const packDetails = {
-                _id: data._id,
-                name: headerTitle,
-                is_public: data.is_public,
-              };
-              setEditTitle(false);
-              titleRef.current.style =
-                'font-size:20px !important;font-weight:bold;color: #22c67c;';
+      {loading && <LoadingPlaceholder color="#e2e1eb" />}
+      <TextInput
+        style={{
+          fontSize: '20px',
+          fontWeight: 'bold',
+          color: theme.colors.textPrimary,
+        }}
+        ref={titleRef}
+        editable={editTitle}
+        onChange={(e) => {
+          setHeaderTitle(e.target.value);
+        }}
+        value={headerTitle}
+        onBlur={() => {
+          if (data.type === 'pack') {
+            const packDetails = {
+              _id: data._id,
+              name: headerTitle,
+              is_public: data.is_public,
+            };
+            setEditTitle(false);
+            titleRef.current.style =
+              'font-size:20px !important;font-weight:bold;color: #22c67c;';
 
-              dispatch(updatePack(packDetails));
-            } else {
-              const tripDetails = {
-                _id: data._id,
-                name: headerTitle,
-                is_public: data.is_public,
-              };
-              setEditTitle(false);
-              titleRef.current.style =
-                'font-size:20px !important;font-weight:bold;color: #22c67c;';
+            dispatch(updatePack(packDetails));
+          } else {
+            const tripDetails = {
+              _id: data._id,
+              name: headerTitle,
+              is_public: data.is_public,
+            };
+            setEditTitle(false);
+            titleRef.current.style =
+              'font-size:20px !important;font-weight:bold;color: #22c67c;';
 
-              dispatch(editTrip(tripDetails));
-            }
-          }}
-        />
-      )}
+            dispatch(editTrip(tripDetails));
+          }
+        }}
+      />
     </>
   );
 };
