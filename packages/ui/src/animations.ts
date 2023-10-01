beeb75410e36903a7e05ff6792b485b15a772eaa
Andrew Bierman <94939237+andrew-bierman@users.noreply.github.com>
2023-09-30 22:30:52 -0400

    WIP - initial set up of ui package

---
+++ packages/ui/src/animations.ts
@@ -0,0 +1,22 @@
+import { createAnimations } from '@tamagui/animations-react-native'
+import type { AnimationDriver } from '@tamagui/web'
+
+export const animations: AnimationDriver = createAnimations({
+  bouncy: {
+    type: 'spring',
+    damping: 10,
+    mass: 0.9,
+    stiffness: 100,
+  },
+  lazy: {
+    type: 'spring',
+    damping: 20,
+    stiffness: 60,
+  },
+  quick: {
+    type: 'spring',
+    damping: 20,
+    mass: 1.2,
+    stiffness: 250,
+  },
+})
