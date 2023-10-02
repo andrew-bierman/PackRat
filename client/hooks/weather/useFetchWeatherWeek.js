c5bec0ce3609bc8210bb70e9afb4093e9cfdf314
Andrew Bierman <94939237+andrew-bierman@users.noreply.github.com>
2023-10-01 21:30:04 -0400

    rerganizing weather hooks

---
+++ client/hooks/weather/useFetchWeatherWeek.js
@@ -0,0 +1,18 @@
+import { queryTrpc } from '../../trpc';
+import { store } from '../../store/store';
+
+export const useFetchWeatherWeak = (latLng) => {
+  const { lat, lon } = latLng;
+  const isEnabled = Boolean(lat && lon);
+  const { refetch, data, error, isLoading, isError } =
+    queryTrpc.getWeatherWeek.useQuery(
+      { lat, lon },
+      {
+        enabled: isEnabled,
+        refetchOnWindowFocus: false,
+        keepPreviousData: true,
+      },
+    );
+  const isOwner = data && user && data.owner_id === user._id;
+  return { refetch, data, error, isLoading, isOwner, isError };
+};
