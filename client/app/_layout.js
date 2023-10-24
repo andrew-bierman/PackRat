2e07a0e7c028b7311586839a23cc309bf91ca521
Andrew Bierman <94939237+andrew-bierman@users.noreply.github.com>
2023-10-23 21:04:03 -0400

    WIP - experimenting with tabs on native

--- client/app/_layout.js
+++ client/app/_layout.js
@@ -1,5 +1,5 @@
-import { Slot } from 'expo-router';
-
+import { Slot, Tabs } from 'expo-router';
+import { useState, useEffect } from 'react';
 import { Platform, View } from 'react-native';
 
 import Navigation from '../screens/Navigation';
@@ -13,6 +13,92 @@ import { SessionProvider } from '../context/auth';
 import { ThemeProvider } from '../context/theme';
 import FlashMessage from 'react-native-flash-message';
 import Footer from '../components/footer/Footer';
+import { MaterialCommunityIcons, MaterialIcons } from '@expo/vector-icons';
+
+const WebLayout = () => {
+  return (
+    <View style={{ flex: 1 }}>
+      <Navigation />
+      <Slot />
+      <Footer />
+    </View>
+  );
+};
+
+const MobileLayout = () => {
+  return (
+    <Tabs initialRouteName="home">
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: 'Home',
+          tabBarIcon: ({ color, size }) => (
+            <MaterialIcons name="home" color={color} size={size} />
+          ),
+          tabBarStyle: Platform.OS === 'ios' && {
+            backgroundColor: 'transparent',
+          },
+          headerShown: true,
+        }}
+      />
+      <Tabs.Screen
+        name="_feed"
+        options={{
+          title: 'Feed',
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="post" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="_profile"
+        options={{
+          title: 'Profile',
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="account" color={color} size={size} />
+          ),
+        }}
+      />
+
+      <Tabs.Screen
+        // Name of the route to hide.
+        name="(app)"
+        options={{
+          // This tab will no longer show up in the tab bar.
+          href: null,
+        }}
+      />
+      <Tabs.Screen
+        // Name of the route to hide.
+        name="(auth)"
+        options={{
+          // This tab will no longer show up in the tab bar.
+          href: null,
+        }}
+      />
+      {/* <Tabs.Screen
+          name="profile"
+          options={{
+            title: 'Profile',
+            tabBarIcon: ({ color, size }) => {
+              const { avatarUrl } = useUser()
+              return (
+                <YStack borderWidth="$1" borderColor={color} borderRadius="$10">
+                  <Avatar circular p="$1" size={size}>
+                    <SolitoImage src={avatarUrl} alt="your avatar" width={size} height={size} />
+                  </Avatar>
+                </YStack>
+              )
+            },
+          }}
+        /> */}
+    </Tabs>
+  );
+};
+
+const Layout = () => {
+  return Platform.OS === 'web' ? <WebLayout /> : <MobileLayout />;
+};
 
 export default function HomeLayout() {
   return (
@@ -20,10 +106,7 @@ export default function HomeLayout() {
       <PersistGate loading={null} persistor={persistor}>
         <SessionProvider>
           <ThemeProvider>
-            <FlashMessage position="top" />
-            <Navigation />
-            <Slot />
-            {/* {Platform.OS === 'web' ? <Footer /> : null} */}
+            <Layout />
           </ThemeProvider>
         </SessionProvider>
       </PersistGate>
