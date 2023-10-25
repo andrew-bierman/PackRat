3a8672d982fadc7608554cb0a34ae8b9841e4d1b
nandwere <mnandwere@gmail.com>
2023-10-25 22:08:30 +0300

    bottom tabs update

---
+++ client/app/(tabs)/_create.tsx
@@ -0,0 +1,4 @@
+
+export default function Screen() {
+  return <></>;
+}
---
+++ client/app/(tabs)/dashboard.tsx
@@ -0,0 +1,8 @@
+import { View, Text } from 'react-native';
+import { usePathname } from 'expo-router';
+import Dashboard from '../../screens/dashboard';
+
+export default function Home() {
+  
+  return <Dashboard />;
+}
