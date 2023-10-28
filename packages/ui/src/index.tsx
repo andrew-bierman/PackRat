603bad193b416b4bec42210bc65cb8b8fcba5828
Andrew Bierman <94939237+andrew-bierman@users.noreply.github.com>
2023-10-28 00:31:31 -0400

    fix on broken imports, WIP this needs to be optimized

--- packages/ui/src/index.tsx
+++ packages/ui/src/index.tsx
@@ -1,3 +1,25 @@
+import RStack from './RStack';
+import RText from './RText';
+import RCard from './RCard';
+import RParagraph from './Rparagraph';
+import RInput from './RInput';
+import RButton from './RButton';
+import RImage from './RImage';
+import RScrollView from './RScrollview';
+import TSkeleton from './Skeleton';
+
+export {
+  RStack,
+  RText,
+  RCard,
+  RParagraph,
+  RInput,
+  RButton,
+  RImage,
+  RScrollView,
+  TSkeleton,
+};
+
 export { config } from './tamagui.config'
 export * from 'tamagui'
 export * from '@tamagui/toast'
