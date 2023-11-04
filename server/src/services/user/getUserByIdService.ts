933698512124b066772c046fe9ba4db89b9cf2f8
amschel99 <6290.2020@students.ku.ac.ke>
2023-11-04 22:38:40 +0300

    fix:modified the prisma schema

--- server/src/services/user/getUserByIdService.ts
+++ server/src/services/user/getUserByIdService.ts
@@ -7,21 +7,22 @@ import { prisma } from "../../prisma/index";
  */
 export const getUserByIdService = async (userId: string): Promise<object> => {
   try {
+    console.log(  `the user id is ${userId}`)
     const user = await prisma.user.findUnique({
       where: {
         id: userId,
-      },
-      include: {
-        favorites: true,
-      },
-    } as any);
-
+      }
+     
+    });
     if (user) {
+      console.log(`the user exists ${JSON.stringify(user)}`)
       return user;
     } else {
+      console.log(`there is no user`)
       throw new Error('User cannot be found');
     }
   } catch (error) {
+    console.log("there was an error" + error.message +JSON.stringify(error))
     throw new Error('Server Error');
   }
 };
