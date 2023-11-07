454bab82ec5e5e4f63ffdf8dafab8b81402d6815
nandwere <mnandwere@gmail.com>
2023-11-07 17:36:00 +0300

    reanimated fix

--- client/babel.config.js
+++ client/babel.config.js
@@ -13,7 +13,6 @@ module.exports = function (api) {
     plugins: [
       // NOTE: `expo-router/babel` is a temporary extension to `babel-preset-expo`.
       require.resolve('expo-router/babel'),
-      ['react-native-reanimated/plugin'],
       [
         'module:react-native-dotenv',
         {
