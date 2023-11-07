47b0b356181cff1abb82c1a1e94f9bd7813357af
Andrew Bierman <94939237+andrew-bierman@users.noreply.github.com>
2023-11-06 21:29:04 -0500

    adding provider folder

---
+++ client/provider/index.tsx
@@ -0,0 +1,5 @@
+import { CombinedProvider } from './CombinedProvider';
+
+export function Provider({ children }: { children: React.ReactNode }) {
+  return <CombinedProvider>{children}</CombinedProvider>;
+}
