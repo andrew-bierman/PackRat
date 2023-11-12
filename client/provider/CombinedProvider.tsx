ef4c4332ab1d950794489b842d0068117c077c21
Andrew Bierman <94939237+andrew-bierman@users.noreply.github.com>
2023-11-11 22:23:07 -0500

    fixing merge conflicts

--- client/provider/CombinedProvider.tsx
+++ client/provider/CombinedProvider.tsx
@@ -1,19 +1,80 @@
 // src/provider/CombinedProvider.tsx
 
+// Standard imports
 import { Provider as ReduxProvider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import { SessionProvider } from '../context/auth';
 import { ThemeProvider } from '../context/theme';
 import { store, persistor } from '../store/store';
 
+// Additional imports from the branch
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { createAsyncStoragePersister } from '@tanstack/query-async-storage-persister';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { PersistQueryClientProvider } from '@tanstack/react-query-persist-client';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { onlineManager } from '@tanstack/react-query';
+import NetInfo from '@react-native-community/netinfo';
+import { Slot } from 'expo-router';
+import FlashMessage from 'react-native-flash-message';
+import Navigation from '../screens/Navigation';
+import Footer from '../components/footer/Footer';
+import { queryTrpc, getToken } from '../trpc';
+import { httpBatchLink } from '@trpc/client';
+import { api } from '~/constants/api';
+import { useEffect } from 'react';
+
 export function CombinedProvider({ children }: { children: React.ReactNode }) {
+  // Setup for React Query and TRPC
+  const queryClient = new QueryClient();
+  const persister = createAsyncStoragePersister({
+    storage: AsyncStorage,
+    throttleTime: 3000,
+  });
+
+  const trpcClient = queryTrpc.createClient({
+    links: [
+      httpBatchLink({
+        url: `${api}/trpc`,
+        async headers() {
+          const token = await getToken('session');
+          return {
+            authorization: token ? `Bearer ${token}` : '',
+          };
+        },
+      }),
+    ],
+  });
+
+  useEffect(() => {
+    return NetInfo.addEventListener((state) => {
+      const status = !!state.isConnected;
+      onlineManager.setOnline(status);
+    });
+  }, []);
+
   return (
     <ReduxProvider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <SessionProvider>
-          <ThemeProvider>{children}</ThemeProvider>
-        </SessionProvider>
-      </PersistGate>
+      <PersistQueryClientProvider
+        onSuccess={async () =>
+          queryClient
+            .resumePausedMutations()
+            .then(async () => queryClient.invalidateQueries())
+        }
+        persistOptions={{ persister }}
+        client={queryClient}
+      >
+        <queryTrpc.Provider client={trpcClient} queryClient={queryClient}>
+          <QueryClientProvider client={queryClient}>
+            <PersistGate loading={null} persistor={persistor}>
+              <SessionProvider>
+                <ThemeProvider>{children}</ThemeProvider>
+              </SessionProvider>
+            </PersistGate>
+            <ReactQueryDevtools initialIsOpen={false} />
+          </QueryClientProvider>
+        </queryTrpc.Provider>
+      </PersistQueryClientProvider>
     </ReduxProvider>
   );
 }
