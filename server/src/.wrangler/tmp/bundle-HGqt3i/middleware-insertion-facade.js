e8155ff1a06855d5e536c061e60ec6394a45f2df
awais-codes <awaiscodes1@gmail.com>
2023-11-16 03:02:43 +0500

    chore: remove wrangler dir

--- server/src/.wrangler/tmp/bundle-HGqt3i/middleware-insertion-facade.js
+++
@@ -1,3 +0,0 @@
-import { __facade_registerInternal__ } from "/home/awais/PackRat/node_modules/wrangler/templates/middleware/loader-sw.ts";
-import * as __MIDDLEWARE_0__ from "/home/awais/PackRat/node_modules/wrangler/templates/middleware/middleware-miniflare3-json-error.ts";
-__facade_registerInternal__([__MIDDLEWARE_0__.default])
