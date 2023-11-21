35c2792a01d6862d2b8a0080a8a44e9ceb9cb1d9
maria-bonete <bonetesalmeron@gmail.com>
2023-11-21 17:07:58 +0200

    refactor(packoptions): updates to tamagui

--- client/components/PackOptions/index.tsx
+++ client/components/PackOptions/index.tsx
@@ -1,27 +1,40 @@
-import { Pressable } from 'react-native';
-import React from 'react';
-import { Box, Menu, ThreeDotsIcon } from 'native-base';
+import { useState } from 'react';
+import { View } from 'react-native';
 import { useDispatch } from 'react-redux';
 import { useRouter } from 'expo-router';
+import { CustomModal } from '../modal';
+import { RStack, RIconButton } from '@packrat/ui';
+import { Entypo } from '@expo/vector-icons'; 
 export const PackOptions = ({ Edit, Delete, Ignore }) => {
   const dispatch = useDispatch();
   const router = useRouter();
+  const [ isMenuOpen, setIsMenuOpen ] = useState(false);
   return (
-    <Box h="80%" w="90%">
-      <Menu
-        w="140"
-        trigger={(triggerProps) => {
-          return (
-            <Pressable accessibilityLabel="More options menu" {...triggerProps}>
-              <ThreeDotsIcon />
-            </Pressable>
-          );
-        }}
-      >
-        <Menu.Item>{Edit} Edit </Menu.Item>
-        <Menu.Item>{Delete} Delete </Menu.Item>
-        <Menu.Item>{Ignore} Ignore </Menu.Item>
-      </Menu>
-    </Box>
+    <View>
+      <CustomModal
+        isActive={isMenuOpen}
+        onTrigger={setIsMenuOpen}
+        triggerComponent={<View
+          style={{
+            display: 'flex',
+            flexDirection: 'row',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+        >
+          <RIconButton
+            backgroundColor="transparent"
+            icon={<Entypo name="dots-three-horizontal" size={24} color="black" />}
+            onPress={() => {
+              setIsMenuOpen(true);
+            }}
+          />
+        </View>}
+      >    
+        <RStack style={{flexDirection: "row", alignItems: "center"}}>{Edit} Edit </RStack>
+        <RStack style={{flexDirection: "row", alignItems: "center"}}>{Delete} Delete </RStack>
+        <RStack style={{flexDirection: "row", alignItems: "center", justifyContent: "flex-end"}}>{Ignore} </RStack>
+      </CustomModal>
+    </View>
   );
 };
