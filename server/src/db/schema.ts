e5b4382580f7f09f2c84e85e81f71c7b78c4631b
Andrew Bierman <94939237+andrew-bierman@users.noreply.github.com>
2023-12-16 19:44:31 -0500

    client and example schema

--- server/src/db/schema.ts
+++ server/src/db/schema.ts
@@ -2,6 +2,19 @@ import { InferInsertModel, InferSelectModel, sql } from 'drizzle-orm';
 import { integer, real, sqliteTable, text } from 'drizzle-orm/sqlite-core';
 import { createInsertSchema, createSelectSchema } from 'drizzle-zod';
 
+// EXAMPLE ONLY
+
+// User
+export const UserTable = sqliteTable('User', {
+  id: text('id').primaryKey(),
+  email: text('email').notNull(),
+});
+
+export type User = InferSelectModel<typeof UserTable>;
+export type InsertUser = InferInsertModel<typeof UserTable>;
+export const insertUserSchema = createInsertSchema(UserTable);
+export const selectUserSchema = createSelectSchema(UserTable);
+
 // TODO - fix all schemas below and relationships
 /*
 
