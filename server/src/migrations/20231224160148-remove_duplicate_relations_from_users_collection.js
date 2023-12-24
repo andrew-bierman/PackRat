24df30b4e830e4aa548678986ce9d6ade820b3e1
awais-codes <awais.codes1@gmail.com>
2023-12-24 21:11:14 +0500

    chore: added migration to remove duplicates in users

--- server/src/migrations/20231224160148-remove_duplicate_relations_from_users_collection.js
+++ server/src/migrations/20231224160148-remove_duplicate_relations_from_users_collection.js
@@ -1,12 +1,33 @@
-export const up = async (db, client) => {
-    // TODO write your migration here.
-    // See https://github.com/seppevs/migrate-mongo/#creating-a-new-migration-script
-    // Example:
-    // await db.collection('albums').updateOne({artist: 'The Beatles'}, {$set: {blacklisted: true}});
-};
+const { ObjectId } = require('mongodb');
 
-export const down = async (db, client) => {
-    // TODO write the statements to rollback your migration (if possible)
-    // Example:
-    // await db.collection('albums').updateOne({artist: 'The Beatles'}, {$set: {blacklisted: false}});
-};
+const filterNullAndParseToString = (arr) => arr.filter((id) => id != null).map(String)
+const mapStringIdsToObjectIds = (arr) => arr.map((id) => new ObjectId(id))
+
+module.exports = {
+    async up (db, client) {
+        const usersCollection = db.collection('users');
+    
+        // Step 1: Get all users from the "users" collection
+        const allUsers = await usersCollection.find({}).toArray();
+        
+        // Step 2: Loop through each user and update the "favorites" and  "packs" fields
+        for (const user of allUsers) {
+            // Remove duplicate ObjectIDs in the "favorites" array
+            const uniqueFavorites = user.favorites ? [...new Set(filterNullAndParseToString(user.favorites))] : [];
+            
+            // Remove duplicate ObjectIDs in the "packs" array
+            const uniquePacks = user.packs ? [...new Set(filterNullAndParseToString(user.packs))] : [];
+    
+            // Update the user with the unique values
+            await usersCollection.updateOne(
+                { _id: user._id },
+                { $set: { 
+                    favorites: mapStringIdsToObjectIds(uniqueFavorites),
+                    packs: mapStringIdsToObjectIds(uniquePacks),
+                } }
+            );
+        }
+    },
+    async down(db, client)  {}
+}
+
