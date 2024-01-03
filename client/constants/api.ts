9b1c4dff29a7444f2fb6d6a8cb02c3306c611788
rowda <rowdaabdella1@gmail.com>
2024-01-04 00:45:12 +0300

    commit

--- client/constants/api.ts
+++ client/constants/api.ts
@@ -1,15 +1,15 @@
 // export const api = "http://add your ip address";
 
 import { NODE_ENV, API_URL } from '@env';
-
-let api = 'http://localhost:3000/api';
+let api = 'https://packrat.onrender.com';
+// let api = 'http://localhost:3000/api';
 
 if (NODE_ENV === 'production') {
   api = API_URL;
 } else {
-  api = 'http://localhost:3000/api';
+  // api = 'http://localhost:3000/api';
   // api = 'https://packrat-dev-qj5h.onrender.com/api';
-  // api = "https://packrat.onrender.com";
+  api = 'https://packrat.onrender.com';
 }
 
 export { api };
