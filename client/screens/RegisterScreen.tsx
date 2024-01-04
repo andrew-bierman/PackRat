38a897a159523c4b29ff56a0f4b9a248e45d63fa
= <=>
2024-01-04 11:05:23 -0800

    refactor: register screen

--- client/screens/RegisterScreen.tsx
+++ client/screens/RegisterScreen.tsx
@@ -2,10 +2,6 @@ import { useState, useEffect } from 'react';
 import { View } from 'react-native';
 import { RHeading, RStack, RButton, RText, RIconButton } from '@packrat/ui';
 import { FontAwesome } from '@expo/vector-icons';
-import * as WebBrowser from 'expo-web-browser';
-import * as Google from 'expo-auth-session/providers/google';
-import { WEB_CLIENT_ID } from '@env';
-// import useRegister from "../hooks/useRegister";
 import { useRouter } from 'expo-router';
 // import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 import { Link } from 'expo-router';
@@ -16,87 +12,20 @@ import useTheme from '../hooks/useTheme';
 import { useForm } from 'react-hook-form';
 import { InputText, InputTextRules } from '~/components/InputText';
 import { useSession } from '../context/auth';
+import { useGoogleAuth } from '~/hooks/login';
+import { useRegisterUser } from '~/hooks/user';
 
 export default function Register() {
-  const { enableDarkMode, enableLightMode, isDark, isLight, currentTheme } =
-    useTheme();
-  const { sessionSignIn } = useSession();
-  const dispatch = useDispatch();
-
+  const { currentTheme } = useTheme();
   const {
-    control,
-    handleSubmit,
-    formState: { isValid },
-  } = useForm({defaultValues: {
-    name: '',
-    email: '',
-    username: '',
-    password: '',
-  }});
-
-  const router = useRouter();
-
-  const user = useSelector((state) => state.auth.user);
-
-  if (user?.user?._id) {
-    InformUser({
-      title: user?.message,
-      duration: 5000,
-      placement: 'top-right',
-      style: { backgroundColor: currentTheme.colors.primary },
-    });
-    router.push('/');
-  }
-
-  const [request, response, promptAsync] = Google.useIdTokenAuthRequest({
-    clientId: WEB_CLIENT_ID,
-  });
-
-  /**
-   * Register a user with the given data.
-   *
-   * @param {object} data - The data object containing the user's name, username, email, and password.
-   */
-  const registerUser = (data) => {
-    const { name, username, email, password } = data;
-    try {
-      const alphanumeric = /^[a-zA-Z0-9]+$/;
-      if (!alphanumeric.test(username)) {
-        alert('Username should be alphanumeric');
-        return;
-      }
-      dispatch(signUp({ name, username, email, password })).then(
-        ({ payload }) => {
-          if (!payload) return;
-          if (payload.token) {
-            sessionSignIn(payload.token);
-          }
-        },
-      );
-    } catch (e) {
-      console.log('Error', e);
-    }
-  };
-
-  const auth = useSelector((state) => state.auth);
-
-  useEffect(() => {
-    if (auth.isLoggedIn) {
-      // router.push("/");
-    }
-  }, [auth, router]);
-
-  useEffect(() => {
-    if (response?.type === 'success') {
-      const { id_token } = response.params;
-      dispatch(signInWithGoogle({ idToken: id_token })).then(({ payload }) => {
-        if (!payload) return;
-        if (payload.token) {
-          sessionSignIn(payload.token);
-        }
-      });
-    }
-  }, [response]);
+    registerUser,
+    form: {
+      control,
+      handleSubmit,
+      formState: { isValid },
+    },
+  } = useRegisterUser();
+  const { promptAsync } = useGoogleAuth();
 
   return (
     <View style={{ width: '100%', alignItems: 'center' }}>
