312f74f16ebabc2fe28db39e6bca8675706627b8
yangTauTauAjah <93625104+yangTauTauAjah@users.noreply.github.com>
2024-01-07 10:31:01 +0700

    refactor progress component

--- client/components/progress/index.tsx
+++ client/components/progress/index.tsx
@@ -1,41 +1,14 @@
-import React, { useEffect, useState } from 'react';
-import { useSelector } from 'react-redux';
 import { Progress } from 'native-base';
-import useTheme from '~/hooks/useTheme';
+import useProgressBarComponent from '~/hooks/progress';
 
 const ProgressBarComponent = () => {
-  const { currentTheme } = useTheme();
-  const reduxTargetValue = useSelector((state) => state.progress.targetValue);
-  const [localCurrentValue, setLocalCurrentValue] = useState(0);
-  const [operationId, setOperationId] = useState(0);
 
-  useEffect(() => {
-    // Generate a new operation ID
-    const newOperationId = Math.random();
-    setOperationId(newOperationId);
+  const {
+    localCurrentValue,
+    currentTheme
+  } = useProgressBarComponent()
 
-    // Reset local current value to start new progress
-    setLocalCurrentValue(0);
-
-    function incrementProgress() {
-      if (
-        localCurrentValue < reduxTargetValue &&
-        operationId === newOperationId
-      ) {
-        setLocalCurrentValue((prevValue) => prevValue + 1);
-      }
-    }
-
-    // Start the increment
-    incrementProgress();
-  }, [reduxTargetValue]);
-
-  useEffect(() => {
-    if (localCurrentValue < reduxTargetValue && operationId !== 0) {
-      setLocalCurrentValue((prevValue) => prevValue + 1);
-    }
-  }, [localCurrentValue, reduxTargetValue, operationId]);
-
+  
   return (
     <Progress
       value={localCurrentValue}
---
+++ client/hooks/progress/index.ts
@@ -0,0 +1,45 @@
+import { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+import useTheme from '~/hooks/useTheme';
+
+export default function useProgressBarComponent() {
+  const { currentTheme } = useTheme();
+  const reduxTargetValue = useSelector<
+    { progress: { targetValue: any; [key: string]: any } },
+    number
+  >((state) => state.progress.targetValue);
+  const [localCurrentValue, setLocalCurrentValue] = useState(0);
+  const [operationId, setOperationId] = useState(0);
+
+  useEffect(() => {
+    // Generate a new operation ID
+    const newOperationId = Math.random();
+    setOperationId(newOperationId);
+
+    // Reset local current value to start new progress
+    setLocalCurrentValue(0);
+
+    function incrementProgress() {
+      if (
+        localCurrentValue < reduxTargetValue &&
+        operationId === newOperationId
+      ) {
+        setLocalCurrentValue((prevValue) => prevValue + 1);
+      }
+    }
+
+    // Start the increment
+    incrementProgress();
+  }, [reduxTargetValue]);
+
+  useEffect(() => {
+    if (localCurrentValue < reduxTargetValue && operationId !== 0) {
+      setLocalCurrentValue((prevValue) => prevValue + 1);
+    }
+  }, [localCurrentValue, reduxTargetValue, operationId]);
+
+  return {
+    localCurrentValue,
+    currentTheme,
+  };
+}
