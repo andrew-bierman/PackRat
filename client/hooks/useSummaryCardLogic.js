5e90cadf3da5b06c4c0bd016e8e10da64014bd28
riteshsinghh-coder <riteshkumar26563@gmail.com>
2024-01-07 22:51:47 +0530

    useSummaryCardLogic fix:#529

---
+++ client/hooks/useSummaryCardLogic.js
@@ -0,0 +1,14 @@
+
+import { useState } from 'react';
+
+const useSummaryCardLogic = () => {
+  const [dummyData, setDummyData] = useState(['First-aid kit', 'Water bottles', 'Tent', 'Sleeping bags (2x)']);
+
+  const handleDelete = () => {} ;
+
+  const handleEdit = () => {};
+
+  return { dummyData, handleDelete, handleEdit };
+};
+
+export default useSummaryCardLogic;
