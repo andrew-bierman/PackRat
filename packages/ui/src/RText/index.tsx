488d312e8767e0d6db9dcfefac8e3140c5e788fa
Andrew Bierman <94939237+andrew-bierman@users.noreply.github.com>
2024-01-09 21:05:01 -0500

    native base text works better than tamagui for us ?

--- packages/ui/src/RText/index.tsx
+++ packages/ui/src/RText/index.tsx
@@ -6,6 +6,6 @@ const RText = styled(Paragraph, {
   fontFamily: '$body',
 });
 
-// This is very temporary fix for issues we have with RText. Speeds up the app just using Text from react-native or native-base
+// This is very temporary fix for issues we have with RText. Speeds up the app just using Text from react-native or native-base. If we use native-base, we can get nearly identical styling to RText, for the time being.
 export default Text;
 // export default RText;
