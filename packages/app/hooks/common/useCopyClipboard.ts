0378329d1ee83db39b91b60844fd61c9e968be8e
Taron <taron.aleksanyan.20@gmail.com>
2024-01-13 03:36:18 -0800

    refactor: card component refactor

---
+++ packages/app/hooks/common/useCopyClipboard.ts
@@ -0,0 +1,39 @@
+import { useState } from 'react';
+import Clipboard from '@react-native-community/clipboard';
+import { Platform } from 'react-native';
+
+export const useCopyClipboard = (link) => {
+  const [isCopied, setIsCopied] = useState(false);
+
+  const handleCopyLink = (overideLink = '') => {
+    const copyLink = link || overideLink;
+
+    if (Platform.OS === 'web') {
+      navigator.clipboard
+        .writeText(copyLink)
+        .then(() => {
+          console.log('Text copied to clipboard');
+        })
+        .catch((err) => {
+          console.error('Failed to copy text to clipboard', err);
+        });
+    } else {
+      Clipboard.setString(copyLink);
+    }
+
+    setIsCopied(true);
+
+    setTimeout(() => {
+      setIsCopied(false);
+    }, 2000);
+    // Style in the future
+    // toast.show('Link copied to clipboard');
+    // InformUser({
+    //   title: 'Link copied to clipboard',
+    //   placement: 'bottom',
+    //   duration: 2000,
+    // });
+  };
+
+  return { handleCopyLink, isCopied };
+};
