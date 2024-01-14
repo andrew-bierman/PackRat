3d5343402caaf5817c12a7e6c5e335ac401a93e1
Andrew Bierman <94939237+andrew-bierman@users.noreply.github.com>
2024-01-13 19:30:14 -0500

    adding tamagui next plugin, refactoring logic to allow for more plugins in future

--- apps/next/next.config.js
+++ apps/next/next.config.js
@@ -1,7 +1,18 @@
 const { withExpo } = require('@expo/next-adapter');
+const { withTamagui } = require('@tamagui/next-plugin');
+const { join } = require('path');
+
+const boolVals = {
+  true: true,
+  false: false,
+};
+
+const disableExtraction =
+  boolVals[process.env.DISABLE_EXTRACTION] ??
+  process.env.NODE_ENV === 'development';
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+let nextConfig = {
   // reanimated (and thus, Moti) doesn't work with strict mode currently...
   // https://github.com/nandorojo/moti/issues/224
   // https://github.com/necolas/react-native-web/pull/2330
@@ -19,7 +30,57 @@ const nextConfig = {
     'react-native-reanimated',
     '@expo/html-elements',
     'react-native-gesture-handler',
+    'expo-linking',
+    'expo-constants',
+    'expo-modules-core',
+    'expo-asset',
+    'expo-font',
   ],
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  modularizeImports: {
+    '@tamagui/lucide-icons': {
+      transform: `@tamagui/lucide-icons/dist/esm/icons/{{kebabCase member}}`,
+      skipDefaultConversion: true,
+    },
+  },
+  experimental: {
+    scrollRestoration: true,
+  },
 };
 
-module.exports = withExpo(nextConfig);
+// Define plugins
+const plugins = [
+  withExpo,
+  withTamagui({
+    config: '../../packages/ui/src/tamagui.config.ts',
+    components: ['tamagui', '@packrat/ui'],
+    importsWhitelist: ['constants.js', 'colors.js'],
+    outputCSS:
+      process.env.NODE_ENV === 'production' ? './public/tamagui.css' : null,
+    logTimings: true,
+    disableExtraction,
+    shouldExtract: (path) => {
+      if (path.includes(join('packages', 'app'))) {
+        return true;
+      }
+    },
+    excludeReactNativeWebExports: [
+      'Switch',
+      'ProgressBar',
+      'Picker',
+      'CheckBox',
+      'Touchable',
+    ],
+  }),
+];
+
+module.exports = function () {
+  // Apply each plugin to the configuration
+  plugins.forEach((plugin) => {
+    nextConfig = plugin(nextConfig);
+  });
+
+  return nextConfig;
+};
