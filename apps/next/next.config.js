654228ee6fcf58639cca0a79b0c05cc7fef3f4f6
Andrew Bierman <94939237+andrew-bierman@users.noreply.github.com>
2024-01-14 14:20:15 -0500

    adding logic from @ raghavyuva's PR to help with expo icons. Removing .ttf from public folder

--- apps/next/next.config.js
+++ apps/next/next.config.js
@@ -1,87 +1,19 @@
 const { withExpo } = require('@expo/next-adapter');
 const { withTamagui } = require('@tamagui/next-plugin');
-const { withFonts } = require('next-fonts');
 const { join } = require('path');
 
 const boolVals = {
   true: true,
   false: false,
 };
-
 const disableExtraction =
   boolVals[process.env.DISABLE_EXTRACTION] ??
   process.env.NODE_ENV === 'development';
 
-/** @type {import('next').NextConfig} */
-let nextConfig = {
-  // reanimated (and thus, Moti) doesn't work with strict mode currently...
-  // https://github.com/nandorojo/moti/issues/224
-  // https://github.com/necolas/react-native-web/pull/2330
-  // https://github.com/nandorojo/moti/issues/224
-  // once that gets fixed, set this back to true
-  reactStrictMode: false,
-  transpilePackages: [
-    'react-native',
-    'react-native-web',
-    'solito',
-    'dripsy',
-    '@dripsy/core',
-    'moti',
-    'app',
-    'react-native-reanimated',
-    '@expo/html-elements',
-    'react-native-gesture-handler',
-    'expo-linking',
-    'expo-constants',
-    'expo-modules-core',
-    'expo-asset',
-    'expo-font',
-    '@expo/vector-icons',
-    'react-native-vector-icons',
-    'expo-linear-gradient',
-    '@tamagui/core',
-    'native-base',
-    'react-native-svg',
-    'react-native-paper',
-  ],
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  modularizeImports: {
-    '@tamagui/lucide-icons': {
-      transform: `@tamagui/lucide-icons/dist/esm/icons/{{kebabCase member}}`,
-      skipDefaultConversion: true,
-    },
-  },
-  experimental: {
-    scrollRestoration: true,
-  },
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      // Add a rule for loading .ttf files, to handle @expo/vector-icons
-      config.module.rules.push({
-        test: /\.ttf$/,
-        use: {
-          loader: 'file-loader',
-          options: {
-            publicPath: '/_next/static/fonts/',
-            outputPath: 'static/fonts/',
-            name: '[name].[ext]',
-          },
-        },
-      });
-    }
-    return config;
-  },
-};
-
-// Define plugins
 const plugins = [
-  // withFonts,
-  withExpo,
   withTamagui({
     config: '../../packages/ui/src/tamagui.config.ts',
-    components: ['tamagui', '@packrat/ui'],
+    components: ['tamagui'],
     importsWhitelist: ['constants.js', 'colors.js'],
     outputCSS:
       process.env.NODE_ENV === 'production' ? './public/tamagui.css' : null,
@@ -100,13 +32,89 @@ const plugins = [
       'Touchable',
     ],
   }),
+  withExpo,
 ];
 
-module.exports = function () {
-  // Apply each plugin to the configuration
-  plugins.forEach((plugin) => {
-    nextConfig = plugin(nextConfig);
-  });
+const nextConfig = function () {
+  let config = {
+    typescript: {
+      ignoreBuildErrors: true,
+    },
+    modularizeImports: {
+      '@tamagui/lucide-icons': {
+        transform: `@tamagui/lucide-icons/dist/esm/icons/{{kebabCase member}}`,
+        skipDefaultConversion: true,
+      },
+    },
+    transpilePackages: [
+      'solito',
+      'react-native-web',
+      'expo-linking',
+      'expo-constants',
+      'expo-modules-core',
+      'react-native',
+      'dripsy',
+      '@dripsy/core',
+      'moti',
+      'app',
+      'react-native-reanimated',
+      '@expo/html-elements',
+      'react-native-gesture-handler',
+      '@expo/vector-icons',
+      'expo-font',
+      'expo-asset',
+      'expo-constants',
+      'expo-file-system',
+      'expo-linking',
+      'expo-permissions',
+      'expo-splash-screen',
+      'expo-status-bar',
+      'native-base',
+      'react-native-svg',
+      'react-native-paper',
+      'react-native-vector-icons',
+      'expo-linear-gradient',
+      '@tamagui/core',
+      'native-base',
+      'react-native-svg',
+      'react-native-paper',
+    ],
+    experimental: {
+      scrollRestoration: true,
+    },
+    webpack: (config, options) => {
+      config.module.rules.push({
+        test: /\.(woff|woff2|eot|ttf|svg|otf)$/,
+        use: {
+          loader: 'url-loader',
+          options: {
+            limit: 100000,
+            name: '[name].[ext]',
+            publicPath: '/_next/static/fonts/',
+            outputPath: 'static/fonts/',
+          },
+        },
+      });
+      config.module.rules.push({
+        test: /\.js$/,
+        include: [
+          /node_modules\/@react-native\/assets-registry/,
+          /node_modules\/react-native/,
+        ],
+        use: options.defaultLoaders.babel,
+      });
+      return config;
+    },
+  };
 
-  return nextConfig;
+  for (const plugin of plugins) {
+    config = {
+      ...config,
+      ...plugin(config),
+    };
+  }
+
+  return config;
 };
+
+module.exports = nextConfig;
