101773b2949598a868cea2ccaf2f5504f27c092e
uros-simeunovic <dixypixy007@gmail.com>
2024-01-14 23:00:06 +0100

    added new types

---
+++ packages/app/types/PackType.ts
@@ -0,0 +1,23 @@
+import { Types } from 'mongoose';
+
+export type PackType = {
+  _id: Types.ObjectId;
+  name: string;
+  items: Types.ObjectId[];
+  owner_id: Types.ObjectId;
+  is_public: boolean;
+  favorited_by: Types.ObjectId[];
+  createdAt: NativeDate;
+  owners: Types.ObjectId[] | [];
+  grades: {
+    weight: string;
+    essentialItems: string;
+    redundancyAndVersatility: string;
+  };
+  scores: {
+    weightScore: number;
+    essentialItemsScore: number;
+    redundancyAndVersatilityScore: number;
+  };
+  type: string | 'pack';
+};
