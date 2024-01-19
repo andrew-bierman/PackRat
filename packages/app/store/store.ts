67045c7dd11a98c126ef512b3cbf547bea58f911
Andrew Bierman <94939237+andrew-bierman@users.noreply.github.com>
2024-01-18 21:04:37 -0500

    conditional on store persist for storage

--- packages/app/store/store.ts
+++ packages/app/store/store.ts
@@ -1,10 +1,11 @@
-"use client"
+'use client';
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import {
   persistReducer,
   persistStore,
   type PersistConfig,
 } from 'redux-persist';
+import { Platform } from 'react-native';
 
 // middleware
 import apiMessageMiddleware from './middleware/apiMessageMiddleware';
@@ -33,6 +34,7 @@ import offlineQueue from './offlineQueue';
 import progressReducer from './progressStore';
 import { type Reducer } from 'react';
 import storage from './customStorage';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 // combine reducers
 const rootReducer = combineReducers({
@@ -60,11 +62,11 @@ const rootReducer = combineReducers({
 });
 export type RootState = ReturnType<typeof rootReducer>;
 
-
 // configure persist store and whitelist reducers
 const persistConfig: PersistConfig<RootState> = {
   key: 'root',
-  storage: storage,
+  // storage: storage,
+  storage: Platform.OS === 'web' ? storage : AsyncStorage,
   whitelist: ['auth', 'globalItems', 'offlineQueue'], // add reducers to persist here
 };
 
@@ -86,7 +88,4 @@ export type AppDispatch = typeof store.dispatch;
 
 const persistor = persistStore(store);
 
-export {
-  store,
-  persistor
-};
+export { store, persistor };
