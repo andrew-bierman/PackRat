6e186afd6d94b18bb59a0c648b19c7ffa152a945
Andrew Bierman <94939237+andrew-bierman@users.noreply.github.com>
2024-01-18 21:03:39 -0500

    fix redux errors on native, adding conditional for storage option. TODO - fix SSR on next. Currently everything is still happening on client side

--- packages/app/provider/CombinedProvider.tsx
+++ packages/app/provider/CombinedProvider.tsx
@@ -4,10 +4,11 @@ import { SessionProvider } from '../context/Auth/SessionProvider';
 import { ThemeProvider } from '../context/theme';
 import { TrpcTanstackProvider } from './TrpcTanstackProvider';
 import dynamic from 'next/dynamic';
+import ReduxProvider from './ReduxProvider';
 
-const ReduxProvider = dynamic(() => import('./ReduxProvider'), {
-  ssr: false,
-});
+// const ReduxProvider = dynamic(() => import('./ReduxProvider'), {
+//   ssr: false,
+// });
 
 export function CombinedProvider({ children }: { children: React.ReactNode }) {
   return (
