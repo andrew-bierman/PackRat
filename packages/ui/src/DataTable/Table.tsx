87fb42ca177120dac920389eee88e874d04355b1
shahzaib <shahzi113awan@gmail.com>
2024-01-21 07:35:19 +0500

    :fix DataTable with tamagui -resizing

--- packages/ui/src/DataTable/Table.tsx
+++ packages/ui/src/DataTable/Table.tsx
@@ -1,79 +1,117 @@
+import { flexRender } from '@tanstack/react-table';
 import {
-      H3,
-      H4,
-      ListItem,
-      Paragraph,
-      ScrollView,
-      Separator,
-      XStack,
-      YStack,
-    } from 'tamagui'
-    
-    
-    export function DataTable({
-      title = '',
-      rows = [['test'], ['test']	],
-      'aria-label': ariaLabel,
-      'aria-labelledby': ariaLabelledBy,
-    }: {
-      title?: string
-      rows?: string[][]
-      'aria-label'?: string
-      'aria-labelledby'?: string
-    }) {
-      const hasAriaLabel = !!(ariaLabel || ariaLabelledBy)
-      return (
-        <ScrollView horizontal>
-          <YStack
-            borderWidth={1}
-            borderColor="$borderColor"
-            f={1}
-            aria-label={hasAriaLabel ? ariaLabel : 'Component Props'}
-            aria-labelledby={ariaLabelledBy}
-            my="$4"
-            br="$4"
-            ov="hidden"
-            mx="$-4"
-            $sm={{
-              mx: 0,
-            }}
-          >
-            {!!title && (
-              <XStack ai="center" py="$2" px="$4" backgroundColor="$borderColor">
-                <H3 size="$3">{title}</H3>
-              </XStack>
-            )}
-    
-            {rows.map((items, i) => (
-              <ListItem key={i} p={0}>
-                <XStack
-                  ai="center"
-                  pos="relative"
-                  py="$3"
-                  px="$4"
-                  $sm={{ flexDirection: 'column' }}
+  H3,
+  H4,
+  ListItem,
+  Paragraph,
+  ScrollView,
+  Separator,
+  View,
+  XStack,
+  YStack,
+} from 'tamagui';
+
+export function DataTable({
+  title,
+  table,
+  'aria-label': ariaLabel,
+  'aria-labelledby': ariaLabelledBy,
+}: {
+  title?: string;
+  table?: any;
+  'aria-label'?: string;
+  'aria-labelledby'?: string;
+}) {
+  const hasAriaLabel = !!(ariaLabel || ariaLabelledBy);
+  return (
+    <ScrollView horizontal>
+      <YStack
+        borderWidth={1}
+        borderColor="$borderColor"
+        f={1}
+        aria-label={hasAriaLabel ? ariaLabel : 'Component Props'}
+        aria-labelledby={ariaLabelledBy}
+        my="$4"
+        br="$4"
+        ov="hidden"
+        mx="$-4"
+        $sm={{
+          mx: 0,
+        }}
+      >
+        {!!title && (
+          <XStack ai="center" py="$2" px="$4" backgroundColor="$borderColor">
+            <H3 size="$3">{title}</H3>
+          </XStack>
+        )}
+
+        {table.getHeaderGroups().map((headerGroup) => (
+          <ListItem key={headerGroup.id} p={0}>
+            <XStack
+              ai="center"
+              pos="relative"
+              py="$3"
+              px="$4"
+              $sm={{ flexDirection: 'column' }}
+            >
+              {headerGroup.headers.map((header) => (
+                <View
+                  onPressIn={header.getResizeHandler()}
+                  onPressOut={header.getResizeHandler()}
                 >
-                  {items.map((item) => (
-                    <H4
-                      color="$color"
-                      fow="700"
-                      key={item}
-                      maw={100}
-                      fontFamily="$mono"
-                      textTransform="none"
-                      ai="center"
-                      jc="center"
-                      textAlign="center"
-                      size="$4"
-                      width={200}
-                    >
-                      {item}
-                    </H4>
-                  ))}
-                </XStack>
-              </ListItem>
-            ))}
-          </YStack>
-        </ScrollView>
-      )
-    }
+                  <H4
+                    color="$color"
+                    fow="700"
+                    key={header.id}
+                    borderWidth={1}
+                    maw={100}
+                    textTransform="none"
+                    ai="center"
+                    jc="center"
+                    textAlign="center"
+                    size="$4"
+                    width={header.getSize()}
+                  >
+                    {flexRender(
+                      header.column.columnDef.header,
+                      header.getContext(),
+                    )}
+                  </H4>
+                </View>
+              ))}
+            </XStack>
+          </ListItem>
+        ))}
+        {table.getRowModel().rows.map((row, i) => (
+          <ListItem key={row.id} p={0}>
+            <XStack
+              ai="center"
+              pos="relative"
+              borderWidth={1}
+              py="$3"
+              px="$4"
+              $sm={{ flexDirection: 'column' }}
+            >
+              {row.getVisibleCells().map((cell) => (
+                <H4
+                  color="$color"
+                  fow="700"
+                  key={cell.id}
+                  maw={100}
+                  textTransform="none"
+                  ai="center"
+                  jc="center"
+                  textAlign="center"
+                  size="$4"
+                  width={200}
+                >
+                  {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                </H4>
+              ))}
+            </XStack>
+          </ListItem>
+        ))}
+      </YStack>
+    </ScrollView>
+  );
+}
--- packages/ui/src/DataTable/index.tsx
+++ packages/ui/src/DataTable/index.tsx
@@ -1,68 +1,31 @@
-import { Box  } from 'tamagui';
+import { Box } from 'tamagui';
 
 import {
-  createColumnHelper,
-  flexRender,
   getCoreRowModel,
   useReactTable,
 } from '@tanstack/react-table';
-import "./table.css"
 import { createColumns } from './columnsParser';
 import { DataTable } from './Table';
+ 
 
-function App({data=[{}]}) {
+function App({ data = [{}], resize = false }) {
 
   const columns = createColumns(data);
   const table = useReactTable({
     data,
     columns,
     getCoreRowModel: getCoreRowModel(),
-    columnResizeMode: "onChange"
+    enableColumnResizing:true,
+    columnResizeMode:  'onChange'
   });
 
   return (
     <div className="p-2">
-     <table className="styled-table" style={{ width: table.getTotalSize() }}>
-        <thead>
-          {table.getHeaderGroups().map((headerGroup) => (
-            <tr key={headerGroup.id}>
-              {headerGroup.headers.map((header) => (
-                <th className="styled-th" style={{ width: `${header.getSize()}` }} key={header.id}>
-                  {header.isPlaceholder
-                    ? null
-                    : flexRender(
-                        header.column.columnDef.header,
-                        header.getContext(),
-                      )}
-                  <div
-                    className="styled-resizer"
-                    onMouseDown={header.getResizeHandler()}
-                    onTouchStart={header.getResizeHandler()}
-                  ></div>
-                </th>
-              ))}
-            </tr>
-          ))}
-        </thead>
-        <tbody>
-          {table.getRowModel().rows.map((row, i) => (
-            <tr key={row.id} className={i % 2 === 0 ? 'styled-tr' : 'styled-tr-alt'}>
-              {row.getVisibleCells().map((cell) => (
-                <td className="styled-td" key={cell.id}>
-                  {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                </td>
-              ))}
-            </tr>
-          ))}
-        </tbody>
-      </table>
-      <DataTable rows={table.getRowModel().rows.map((_)=>{
-        return _.getVisibleCells().map((cell) => (
-           cell.getValue()
-        ))
-      })}  />
+      <DataTable
+      table = {table}
+      />
     </div>
   );
 }
-
+  
 export default App;
