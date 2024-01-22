3c3516cf118003aff3d61c94d6b5a212b684b8b9
Andrew Bierman <94939237+andrew-bierman@users.noreply.github.com>
2024-01-21 19:43:42 -0500

    adding transparent background to trigger component of modal

--- packages/ui/src/modal/BaseModal.tsx
+++ packages/ui/src/modal/BaseModal.tsx
@@ -31,6 +31,7 @@ export const BaseModal = ({
       <RButton
         onPress={() => setIsModalOpen(true)}
         style={{ backgroundColor: 'transparent' }}
+        backgroundColor={'transparent'}
       >
         {React.cloneElement(triggerComponent, { setIsModalOpen })}
       </RButton>
@@ -78,62 +79,61 @@ export const BaseModal = ({
     >
       <Dialog.Trigger asChild>{triggerElement}</Dialog.Trigger>
       <Dialog.Portal>
-       
-          <Dialog.Overlay
-            key="overlay"
-            animation="quick"
-            opacity={0.5}
-            enterStyle={{ opacity: 0 }}
-            exitStyle={{ opacity: 0 }}
-            minWidth={400}
-          />
+        <Dialog.Overlay
+          key="overlay"
+          animation="quick"
+          opacity={0.5}
+          enterStyle={{ opacity: 0 }}
+          exitStyle={{ opacity: 0 }}
+          minWidth={400}
+        />
 
-          <Dialog.Content
-            bordered
-            elevate
-            key="content"
-            animateOnly={['transform', 'opacity']}
-            animation={[
-              'quick',
-              {
-                opacity: {
-                  overshootClamping: true,
-                },
+        <Dialog.Content
+          bordered
+          elevate
+          key="content"
+          animateOnly={['transform', 'opacity']}
+          animation={[
+            'quick',
+            {
+              opacity: {
+                overshootClamping: true,
               },
-            ]}
-            enterStyle={{ x: 0, y: -20, opacity: 0, scale: 0.9 }}
-            exitStyle={{ x: 0, y: 10, opacity: 0, scale: 0.95 }}
+            },
+          ]}
+          enterStyle={{ x: 0, y: -20, opacity: 0, scale: 0.9 }}
+          exitStyle={{ x: 0, y: 10, opacity: 0, scale: 0.95 }}
+          gap="$4"
+        >
+          <Dialog.Title>{title}</Dialog.Title>
+          <Dialog.Description>
+            <ModalProvider
+              isModalOpen={isModalOpen}
+              setIsModalOpen={setIsModalOpen}
+            >
+              {children}
+            </ModalProvider>
+          </Dialog.Description>
+
+          <RStack
+            style={{ alignSelf: 'flex-end', flexDirection: 'row' }}
             gap="$4"
           >
-            <Dialog.Title>{title}</Dialog.Title>
-            <Dialog.Description>
-            <ModalProvider
-          isModalOpen={isModalOpen}
-          setIsModalOpen={setIsModalOpen}
-        >
-              {children}
-              </ModalProvider>
-              </Dialog.Description>
-
-            <RStack
-              style={{ alignSelf: 'flex-end', flexDirection: 'row' }}
-              gap="$4"
-            >
-              {memoFooterButtons}
-            </RStack>
-            {footerElement}
-            <Dialog.Close asChild>
-              <Button
-                position="absolute"
-                backgroundColor="transparent"
-                top="$3"
-                right="$3"
-                size="$2"
-                circular
-                icon={X}
-              />
-            </Dialog.Close>
-          </Dialog.Content>
+            {memoFooterButtons}
+          </RStack>
+          {footerElement}
+          <Dialog.Close asChild>
+            <Button
+              position="absolute"
+              backgroundColor="transparent"
+              top="$3"
+              right="$3"
+              size="$2"
+              circular
+              icon={X}
+            />
+          </Dialog.Close>
+        </Dialog.Content>
       </Dialog.Portal>
     </Dialog>
   );
