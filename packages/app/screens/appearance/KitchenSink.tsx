4ab60994884f65d458cc79546a5f8880927b6241
pinocchio-life-like <icbr19fl@gmail.com>
2024-01-27 18:28:25 +0300

    refactor appearance component inside screen

---
+++ packages/app/screens/appearance/KitchenSink.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Box, Text } from 'native-base';
+import { DialogDemo } from '../../components/DialogDemo';
+
+const KitchenSink = () => (
+  <Box>
+    <Text>Kitchen Sink</Text>
+    <DialogDemo />
+  </Box>
+);
+
+export default KitchenSink;
