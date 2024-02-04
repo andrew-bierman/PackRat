6cbefbdc2ec8a3941e71fb73e3a41d8080cad4ac
yasTheDreamer <yassine19265@gmail.com>
2024-02-04 16:18:20 +0100

    fix useRef declared outside of component

--- packages/app/context/ListRef.tsx
+++ packages/app/context/ListRef.tsx
@@ -1,6 +1,6 @@
 import { createContext, useContext, useEffect, useRef, useState } from 'react';
 
-export const ListRefContext = createContext(useRef(null));
+export const ListRefContext = createContext(null);
 
 export const ListRefProvider = (props) => {
   const ref = useRef(null);
