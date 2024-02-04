53f44fc83b4cd408319afbe42a2a15566f809312
Eliyas Asefa <103815611+pinocchio-life-like@users.noreply.github.com>
2024-02-04 20:45:04 +0300

    Create useWeather.tsx

---
+++ packages/app/screens/trip/useWeather.tsx
@@ -0,0 +1,10 @@
+import { useSelector } from 'react-redux';
+
+const useWeather = () => {
+  const weatherObject = useSelector((state) => state.weather.weatherObject);
+  const weatherWeek = useSelector((state) => state.weather.weatherWeek);
+
+  return { weatherObject, weatherWeek };
+};
+
+export default useWeather;
