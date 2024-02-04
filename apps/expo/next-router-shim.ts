5551ce77a6a4792b1dcd94a22eaaf16bec4f358f
yasTheDreamer <yassine19265@gmail.com>
2024-02-04 17:49:45 +0100

    remove unnecessary file

--- apps/expo/next-router-shim.ts
+++
@@ -1,9 +0,0 @@
-export default {
-    push() {},
-    replace() {},
-    router: null,
-  }
-  /**
-   * @deprecated You imported from the wrong file!
-   */
-  export const useRouter = () => undefined
