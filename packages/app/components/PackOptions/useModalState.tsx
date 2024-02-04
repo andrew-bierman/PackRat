de50e20ffe4b2087e2aa9fe954ee78ca1203694d
Eliyas Asefa <103815611+pinocchio-life-like@users.noreply.github.com>
2024-02-04 22:10:02 +0300

    Create useModalState.tsx

---
+++ packages/app/components/PackOptions/useModalState.tsx
@@ -0,0 +1,10 @@
+import { useState } from 'react';
+
+export const useModalState = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openModal = () => setIsModalOpen(true);
+  const closeModal = () => setIsModalOpen(false);
+
+  return { isModalOpen, openModal, closeModal };
+};
