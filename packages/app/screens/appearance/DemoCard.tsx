082550dbf2c0d2deac30dc8b30790eae92f4143a
Eliyas Asefa <103815611+pinocchio-life-like@users.noreply.github.com>
2024-02-04 20:52:37 +0300

    Create DemoCard.tsx

---
+++ packages/app/screens/appearance/DemoCard.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Card, H2, Paragraph, XStack, Button } from 'tamagui';
+
+const DemoCard = () => (
+  <Card
+    elevate
+    size="$4"
+    bordered
+    width={250}
+    height={300}
+    scale={0.9}
+    hoverStyle={{ scale: 0.925 }}
+    pressStyle={{ scale: 0.875 }}
+  >
+    <Card.Header padded>
+      <H2>Sony A7IV</H2>
+      <Paragraph theme="alt2">Now available</Paragraph>
+    </Card.Header>
+    <Card.Footer padded>
+      <XStack flex={1} />
+      <Button borderRadius="$10">Purchase</Button>
+    </Card.Footer>
+  </Card>
+);
+
+export default DemoCard;
