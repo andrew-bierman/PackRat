207ef135973e8bbc32cf1f96f1dad305a59a8d8b
Andrew Bierman <94939237+andrew-bierman@users.noreply.github.com>
2024-02-04 22:30:08 -0500

    update is mobile hook to hardcode screen width, will switch back to media queries

--- packages/app/hooks/common/useIsMobileView.ts
+++ packages/app/hooks/common/useIsMobileView.ts
@@ -1,7 +1,10 @@
 import { useMedia } from 'tamagui';
+import { useScreenWidth } from './useScreenWidth';
 
 export const useIsMobileView = () => {
   const media = useMedia();
 
-  return media.md;
+  const { screenWidth } = useScreenWidth();
+
+  return screenWidth < 1900; // hard coded for now, will be replaced with media queries
 };
