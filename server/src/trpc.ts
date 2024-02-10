7844107c555a346dbf8e9715f9d65f78b857babb
Andrew Bierman <94939237+andrew-bierman@users.noreply.github.com>
2024-02-09 21:20:14 -0500

    setting up protected trpc procedure and enhancing context to include user

--- server/src/trpc.ts
+++ server/src/trpc.ts
@@ -1,6 +1,48 @@
 import { TRPCError, initTRPC } from '@trpc/server';
+import * as trpcExpress from '@trpc/server/adapters/express';
+import { getUserByTokenService } from './services/user/getUserByToken';
 
 const t = initTRPC.create();
+
+/**
+ * Create a context object that will be passed to all resolvers
+ */
+export const createContext = async ({
+  req,
+  res,
+}: trpcExpress.CreateExpressContextOptions) => {
+  // Create context based on the request object
+  // Will be available as `ctx` in all your resolvers
+
+  // Extract the token from the request headers, verify it, and retrieve the user. Add the user to the context object.
+  const getUserFromHeader = async () => {
+    let user = null;
+
+    // Extract the token from the request headers
+    const authHeader = req.headers.authorization || '';
+
+    // If the token is present, verify it
+    if (authHeader.startsWith('Bearer ')) {
+      const token = authHeader.split(' ')[1];
+
+      // Try to verify the token and retrieve the user
+      user = await getUserByTokenService(token);
+    }
+
+    return user;
+  };
+
+  const user = await getUserFromHeader();
+  console.log('user', user);
+
+  return {
+    req,
+    res,
+    user,
+  };
+};
+
+export type Context = inferAsyncReturnType<typeof createContext>;
 
 /**
  * Export reusable router and procedure helpers
@@ -9,3 +51,21 @@ const t = initTRPC.create();
 export const router = t.router;
 export const middleware = t.middleware;
 export const publicProcedure = t.procedure;
+
+/**
+ * Authentication middleware
+ */
+const isAuthenticated = t.middleware((opts) => {
+  const { ctx, next } = opts;
+
+  if (!ctx.user) {
+    // If the user is invalid, throw an error
+    throw new TRPCError({
+      code: 'UNAUTHORIZED',
+      message: 'Invalid token',
+    });
+  }
+  return next(ctx);
+});
+
+export const protectedProcedure = t.procedure.use(isAuthenticated);
