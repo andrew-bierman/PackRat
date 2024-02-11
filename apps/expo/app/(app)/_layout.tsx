c51f46f2bd0824d09f87975d0871963d34544f4e
Andrew Bierman <94939237+andrew-bierman@users.noreply.github.com>
2024-02-11 18:28:54 -0500

    exploring possible modal implementation

--- apps/expo/app/(app)/_layout.tsx
+++ apps/expo/app/(app)/_layout.tsx
@@ -1,12 +1,25 @@
-import { Slot } from 'expo-router';
+import { Link, Slot } from 'expo-router';
 import { AuthWrapper } from 'app/auth/AuthWrapper';
 import { Stack } from 'expo-router';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 export default function AppLayout() {
   return (
     <AuthWrapper>
       <Stack>
         <Stack.Screen name="(drawer)" options={{ headerShown: false }} />
+        <Stack.Screen
+          name="modal"
+          options={{
+            headerShown: true,
+            headerRight: () => (
+              <Link href="../">
+                <MaterialCommunityIcons name="close" size={24} color="black" />
+              </Link>
+            ),
+            presentation: 'modal',
+          }}
+        />
       </Stack>
     </AuthWrapper>
   );
