e82fbafe3dd84801ea09e1aae79dce36ac421932
Andrew Bierman <abbierman101@gmail.com>
2024-02-14 00:45:33 -0500

    fixing vite build errors

--- apps/vite/vite.config.ts
+++ apps/vite/vite.config.ts
@@ -1,7 +1,8 @@
 import { tamaguiExtractPlugin, tamaguiPlugin } from '@tamagui/vite-plugin'
 import react from '@vitejs/plugin-react-swc'
 import { defineConfig } from 'vite'
-import { resolve } from 'path' // Import resolve from the path module
+import { resolve } from 'path' 
+import esbuildFlowPlugin from 'esbuild-plugin-flow'
 
 const shouldExtract = process.env.EXTRACT === '1'
 
@@ -10,6 +11,23 @@ const tamaguiConfig = {
   config: 'src/tamagui.config.ts',
 }
 
+// https://tamagui.dev/docs/intro/installation
+const extensions = [
+  ".web.tsx",
+  ".tsx",
+  ".web.ts",
+  ".ts",
+  ".web.jsx",
+  ".jsx",
+  ".web.js",
+  ".js",
+  ".css",
+  ".json",
+  ".mjs",
+];
+
+const development = process.env.NODE_ENV === "development";
+
 export default defineConfig({
   clearScreen: true,
   plugins: [
@@ -17,9 +35,36 @@ export default defineConfig({
     tamaguiPlugin(tamaguiConfig),
     shouldExtract ? tamaguiExtractPlugin(tamaguiConfig) : null,
   ].filter(Boolean),
+  define: {
+    // https://github.com/bevacqua/dragula/issues/602#issuecomment-1296313369
+    global: "window",
+    __DEV__: JSON.stringify(development),
+    // https://tamagui.dev/docs/intro/installation
+    DEV: JSON.stringify(development),
+    "process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV),
+  },
   resolve: { // Add the resolve configuration
     alias: {
+      extensions,
       '@env': resolve(__dirname, 'envResolver'), 
+      'react-native': 'react-native-web',
+      'react-native/Libraries/Image/AssetRegistry': resolve(__dirname, '../../node_modules/react-native-web/dist/modules/AssetRegistry'),
+      '@react-native/assets-registry/regisery': resolve(__dirname, '../../node_modules/react-native-web/dist/modules/AssetRegistry'),
     },
   },
+  optimizeDeps: {
+    esbuildOptions: {
+      resolveExtensions: extensions,
+      // https://github.com/vitejs/vite-plugin-react/issues/192#issuecomment-1627384670
+      jsx: "automatic",
+      // need either this or the plugin below
+      loader: { ".js": "jsx" },
+      plugins: [
+        esbuildFlowPlugin(/\.(flow|jsx?)$/, (path) =>
+          /\.jsx$/.test(path) ? "jsx" : "jsx"
+        ),
+      ],
+    },
+  },
+
 })
