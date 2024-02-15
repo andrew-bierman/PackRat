b2b0445ae766d7a8992060611d137db9b96eb586
pinocchio-life-like <icbr19fl@gmail.com>
2024-02-15 15:40:23 +0300

    type add and refactor

--- packages/app/components/inventory/Inventory.tsx
+++ packages/app/components/inventory/Inventory.tsx
@@ -1,4 +1,4 @@
-import { View, Text, TouchableOpacity } from 'react-native';
+import { View, Text } from 'react-native';
 // import { TableContainer } from '../Table';
 
 export const Inventory = () => {
