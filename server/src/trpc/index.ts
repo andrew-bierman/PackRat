aa1031220edbbed1e7db4a1d8753de03b68afee8
Andrew Bierman <94939237+andrew-bierman@users.noreply.github.com>
2024-02-26 21:50:28 -0500

    adding error formatter for zod

--- server/src/trpc/index.ts
+++ server/src/trpc/index.ts
@@ -1,13 +1,15 @@
 import { TRPCError, initTRPC } from '@trpc/server';
 import { auth } from './middlewares';
 import { type ExtendedContext } from './context';
-
-const t = initTRPC.context<ExtendedContext>().create();
+import { errorFormatter } from './errorFormatter';
 
 /**
  * Export reusable router and procedure helpers
  * that can be used throughout the router
  */
+const t = initTRPC.context<ExtendedContext>().create({
+  errorFormatter,
+});
 export const router = t.router;
 export const middleware = t.middleware;
 export const publicProcedure = t.procedure;
