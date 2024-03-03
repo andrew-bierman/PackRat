8dfe57c79bd4fb90ba32368469bc70d373b20cc8
Andrew Bierman <94939237+andrew-bierman@users.noreply.github.com>
2024-03-03 12:47:40 -0500

    attempting to add miniflare to tests

--- server/src/tests/utils/testHelpers.ts
+++ server/src/tests/utils/testHelpers.ts
@@ -1,11 +1,16 @@
 // testHelpers.ts
-// import mongoose from 'mongoose';
+import { Miniflare } from 'miniflare';
 import { createCaller } from '../../routes/trpcRouter';
 import { createContextInner } from '../../trpc/context';
 
 export async function setupTest() {
   process.env.NODE_ENV = 'test';
   // await mongoose.connect(process.env.MONGODB_URI ?? '');
+
+  // const mf = new Miniflare({
+  //   d1Databases: ["DB"],
+  // });
+
   const ctx = await createContextInner({});
   const caller = createCaller(ctx);
   return { caller };
