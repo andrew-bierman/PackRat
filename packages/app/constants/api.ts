de548e3c40d9e6406f6ba1a2f7f596a5024a871d
Andrew Bierman <94939237+andrew-bierman@users.noreply.github.com>
2024-04-30 11:01:50 -0400

    Update api.ts

--- packages/app/constants/api.ts
+++ packages/app/constants/api.ts
@@ -2,13 +2,13 @@
 
 import { NODE_ENV, API_URL, NEXT_PUBLIC_API_URL } from '@env';
 
-let api = 'http://localhost:8080/api';
+let api = 'http://localhost:8787/api';
 
 if (NODE_ENV === 'production') {
   // api = API_URL;
   api = NEXT_PUBLIC_API_URL;
 } else {
-  api = 'http://localhost:8080/api';
+  api = 'http://localhost:8787/api';
   // api = "https://packrat.onrender.com";
 }
 
