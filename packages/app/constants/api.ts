051ffc07d1adf8fe0d2665332028c71525773147
Tadjaur <tadjaur@tas-groups.com>
2024-04-30 20:03:07 +0100

    Fix mobile application not able to parse backend responses

--- packages/app/constants/api.ts
+++ packages/app/constants/api.ts
@@ -1,15 +1,9 @@
-// export const api = "http://add your ip address";
-
 import { NODE_ENV, API_URL, NEXT_PUBLIC_API_URL } from '@env';
 
-let api = 'http://localhost:8787/api';
+let api = API_URL ?? NEXT_PUBLIC_API_URL;
 
-if (NODE_ENV === 'production') {
-  // api = API_URL;
-  api = NEXT_PUBLIC_API_URL;
-} else {
+if(!api && NODE_ENV !== 'production') {
   api = 'http://localhost:8787/api';
-  // api = "https://packrat.onrender.com";
 }
 
 export { api };
