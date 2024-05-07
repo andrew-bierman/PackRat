e290250f61e5a70cad7ce8dda6c8de8a12118fb3
Andrew Bierman <94939237+andrew-bierman@users.noreply.github.com>
2024-05-06 21:55:08 -0400

    🐛 fix vite key for google

--- packages/app/auth/hooks/useGoogleAuth.ts
+++ packages/app/auth/hooks/useGoogleAuth.ts
@@ -3,10 +3,11 @@ import * as Google from 'expo-auth-session/providers/google';
 
 import { queryTrpc } from 'app/trpc';
 import { useSessionSignIn } from './useSessionSignIn';
+import { GOOGLE_ID, IOS_CLIENT_ID, ANDROID_CLIENT_ID } from '@packrat/config';
 
-const webClientId = String(process.env.NEXT_PUBLIC_GOOGLE_ID);
-const iosClientId = String(process.env.IOS_CLIENT_ID);
-const androidClientId = String(process.env.ANDROID_CLIENT_ID);
+const webClientId = String(GOOGLE_ID);
+const iosClientId = String(IOS_CLIENT_ID);
+const androidClientId = String(ANDROID_CLIENT_ID);
 
 export const useGoogleAuth = () => {
   const [token, setToken] = useState('');
