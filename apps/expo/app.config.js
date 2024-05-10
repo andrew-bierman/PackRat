f60abb6ca1c84e39ceac2eed76a8454e73e22c7e
Andrew Bierman <94939237+andrew-bierman@users.noreply.github.com>
2024-05-09 21:17:11 -0400

    🎨 adding EXPO_PUBLIC prefix

--- apps/expo/app.config.js
+++ apps/expo/app.config.js
@@ -8,6 +8,11 @@ module.exports = ({ config }) => {
   config.extra.MAPBOX_ACCESS_TOKEN = process.env.MAPBOX_ACCESS_TOKEN;
   config.extra.API_URL = process.env.API_URL;
 
+  config.extra.EXPO_PUBLIC_NODE_ENV = process.env.EXPO_PUBLIC_NODE_ENV;
+  config.extra.EXPO_PUBLIC_CLIENT_URL = process.env.EXPO_PUBLIC_CLIENT_URL;
+  config.extra.EXPO_PUBLIC_MAPBOX_ACCESS_TOKEN = process.env.EXPO_PUBLIC_MAPBOX_ACCESS_TOKEN;
+  config.extra.EXPO_PUBLIC_API_URL = process.env.EXPO_PUBLIC_API_URL;
+
   return {
     ...config,
     plugins: config.plugins.map(i => {
