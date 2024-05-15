f8399addc886976f9f7068a6b737bb2289454f9b
Tadjaur <tadjaur@tas-groups.com>
2024-05-15 06:56:18 +0100

    Fix config for android local build

--- apps/expo/app.config.js
+++ apps/expo/app.config.js
@@ -1,17 +1,4 @@
-import 'dotenv/config';
-
 module.exports = ({ config }) => {
-  config.extra.NODE_ENV = process.env.NODE_ENV;
-  config.extra.CLIENT_URL = process.env.CLIENT_URL;
-  config.extra.MAPBOX_ACCESS_TOKEN = process.env.MAPBOX_ACCESS_TOKEN;
-  config.extra.API_URL = process.env.API_URL;
-
-  config.extra.EXPO_PUBLIC_NODE_ENV = process.env.EXPO_PUBLIC_NODE_ENV;
-  config.extra.EXPO_PUBLIC_CLIENT_URL = process.env.EXPO_PUBLIC_CLIENT_URL;
-  config.extra.EXPO_PUBLIC_MAPBOX_ACCESS_TOKEN =
-    process.env.EXPO_PUBLIC_MAPBOX_ACCESS_TOKEN;
-  config.extra.EXPO_PUBLIC_API_URL = process.env.EXPO_PUBLIC_API_URL;
-
   return {
     ...config,
     plugins: config.plugins.map((i) => {
