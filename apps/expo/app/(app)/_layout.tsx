65b5be7966c06d73ac56a6c9261afe7dab43ab75
nandwere <mnandwere@gmail.com>
2024-06-25 10:50:51 +0300

    feed styling
    merge feedstyle

--- apps/expo/app/(app)/_layout.tsx
+++ apps/expo/app/(app)/_layout.tsx
@@ -3,25 +3,28 @@ import { Link, Slot } from 'expo-router';
 import { AuthWrapper } from 'app/auth/AuthWrapper';
 import { Stack } from 'expo-router';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { SearchProvider } from 'app/components/feed/SearchProvider';
 
 export default function AppLayout() {
   return (
     <AuthWrapper>
-      <Stack>
-        <Stack.Screen name="(drawer)" options={{ headerShown: false }} />
-        <Stack.Screen
-          name="modal"
-          options={{
-            headerShown: true,
-            headerRight: () => (
-              <Link href="../">
-                <MaterialCommunityIcons name="close" size={24} color="black" />
-              </Link>
-            ),
-            presentation: 'modal',
-          }}
-        />
-      </Stack>
+      <SearchProvider>
+        <Stack>
+          <Stack.Screen name="(drawer)" options={{ headerShown: false }} />
+          <Stack.Screen
+            name="modal"
+            options={{
+              headerShown: true,
+              headerRight: () => (
+                <Link href="../">
+                  <MaterialCommunityIcons name="close" size={24} color="black" />
+                </Link>
+              ),
+              presentation: 'modal',
+            }}
+          />
+        </Stack>
+      </SearchProvider>
     </AuthWrapper>
   );
 }
