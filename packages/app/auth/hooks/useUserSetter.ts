b8c98ad72442b5a5f2d19baeff693a1783894e33
Taron <taron.aleksanyan.20@gmail.com>
2024-06-29 10:36:45 +0400

    fix user setter hook

--- packages/app/auth/hooks/useUserSetter.ts
+++ packages/app/auth/hooks/useUserSetter.ts
@@ -1,16 +1,15 @@
 import { useCallback } from 'react';
 import { getQueryKey } from '@trpc/react-query';
 import { queryTrpc } from 'app/trpc';
-import { useQueryClient } from '@tanstack/react-query';
-import { User } from '../ts';
+import { type User } from '../ts';
 
 export const useUserSetter = () => {
-  const queryClient = useQueryClient();
+  const utils = queryTrpc.useUtils();
   const userKey = getQueryKey(queryTrpc.getMe, undefined, 'query');
 
   const setUser = useCallback(
-    (data: User | null) => queryClient.setQueryData(userKey, data),
-    [queryClient, userKey],
+    (data: User | null) => utils.getMe.setData(null, data),
+    [utils, userKey],
   );
 
   return setUser;
