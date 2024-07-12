f0dba72ceba492311920e3dff9ceab258ba47614
Ibrahim Isa Jajere <ibrahimisajajere274@gmail.com>
2024-07-12 12:53:07 +0100

    📝 remove wrong JSDoc from getSimilarPacksRoute function

--- server/src/controllers/pack/getSimilarPacks.ts
+++ server/src/controllers/pack/getSimilarPacks.ts
@@ -2,12 +2,6 @@ import { publicProcedure } from '../../trpc';
 import { getSimilarPacksService } from '../../services/pack/pack.service';
 import { z } from 'zod';
 
-/**
- * Retrieves packs that are similar to provided pack.
- * @param {Object} req - the request object
- * @param {Object} res - the response object
- * @return {Promise} - a promise that resolves with an array of similar packs
- */
 export function getSimilarPacksRoute() {
   return publicProcedure
     .input(z.object({ id: z.string(), limit: z.number() }))
