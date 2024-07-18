e9ab1a102a9bafe48110ecd9c395a1ae8912823b
Taron <taron.aleksanyan.20@gmail.com>
2024-07-18 09:11:09 +0400

    fix profile screen crash

--- apps/vite/src/App.tsx
+++ apps/vite/src/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider, createRouter } from '@tanstack/react-router';
