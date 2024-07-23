04cc41c3809634c39060f1ee0f5f1f63e6b6de1d
mikeCyb <michaelgashaw972@gmail.com>
2024-07-23 23:12:21 +0300

    items loading box fix

--- packages/app/components/itemtable/itemTable.tsx
+++ packages/app/components/itemtable/itemTable.tsx
@@ -4,7 +4,7 @@ import useResponsive from 'app/hooks/useResponsive';
 import Loader from '../Loader';
 import useTheme from '../../hooks/useTheme';
 import Layout from 'app/components/layout/Layout';
-import { BasicTable } from '@packrat/ui/src/Bento/elements/tables';
+import { PaginatedSortedTable } from '@packrat/ui/src/Bento/elements/tables';
 import { PaginationLimit } from '../paginationChooseLimit';
 
 interface Category {
@@ -92,35 +92,43 @@ export const ItemsTable = ({
   const groupedData = groupByType(filteredData);
 
   return (
-    <Layout>
-      <ScrollView>
-        <View
-          style={{
-            paddingVertical: 16,
-            flex: 1,
-            padding: 30,
-            backgroundColor: isDark ? '#1A1A1D' : 'white',
-            width: '100%',
-            borderRadius: 12,
-            
-          }}
-        >
-          {isLoading ? (
-            <Loader />
-          ) : (
-            <BasicTable
-              groupedData={groupedData}
-              onDelete={onDelete}
-              handleCheckboxChange={handleCheckboxChange}
-              currentPack={currentPack}
-              hasPermissions={hasPermissions}
-              refetch={refetch ?? false}
-              setRefetch={setRefetch}
-            ></BasicTable>
-          )}
-          <PaginationLimit limit={limit} setLimit={setLimit} />
-        </View>
-      </ScrollView>
-    </Layout>
+    <View
+      style={{
+        padding: 30,
+        backgroundColor: isDark ? '#1A1A1D' : 'white',
+        borderRadius: 12,
+        margin: 20,
+      }}
+    >
+      <Layout>
+        <ScrollView>
+          <View
+            style={{
+              flexGrow: 1,
+              backgroundColor: isDark ? '#1A1A1D' : 'white',
+              width: '100%',
+            }}
+          >
+            {isLoading ? (
+              <Loader />
+            ) : (
+              <PaginatedSortedTable
+                groupedData={groupedData}
+                handleCheckboxChange={handleCheckboxChange}
+                onDelete={onDelete}
+                hasPermissions={hasPermissions}
+                currentPack={currentPack}
+                refetch={refetch}
+                setRefetch={setRefetch}
+                totalPages={totalPages}
+                page={page}
+                setPage={setPage}
+              />
+            )}
+            <PaginationLimit limit={limit} setLimit={setLimit} />
+          </View>
+        </ScrollView>
+      </Layout>
+    </View>
   );
 };
