0c2b86ce0a7540a586559864c5d16afeeeff8e7c
JewelTee <jewelteeee@gmail.com>
2024-08-01 17:18:20 +0800

    updating playwright tests

--- packages/playwright/tests/pack-add-item.spec.js
+++ packages/playwright/tests/pack-add-item.spec.js
@@ -1,6 +1,7 @@
 import { test, expect } from '@playwright/test';
 
 test('test', async ({ page }) => {
+  test.setTimeout(60000);
   await page.goto('https://packrat.world/');
   await page.getByRole('link', { name: 'Get Started' }).click();
   await page.getByLabel('Email').click();
@@ -9,7 +10,7 @@ test('test', async ({ page }) => {
   await page.getByLabel('Password').fill('12345678');
   await page.getByRole('button', { name: 'Sign In' }).click();
   await page.getByRole('button', { name: 'Menu' }).click();
-  await page.getByRole('button', { name: 'Packs' }).click();
+  await page.getByRole('button', { name: ' Packs' }).click();
   await page.goto('https://packrat.world/packs');
   await page.getByRole('link', { name: 'Quantity field' }).click();
   await page.getByRole('button', { name: 'Add Item' }).click();
@@ -20,5 +21,4 @@ test('test', async ({ page }) => {
   await page.getByPlaceholder('Quantity').fill('1');
   await page.getByLabel('Essentials').click();
   await page.getByRole('button', { name: 'Add Item' }).click();
-  await page.goto('https://packrat.world/pack/jfxuhkd3zflt4ddavnyhh3so');
 });
