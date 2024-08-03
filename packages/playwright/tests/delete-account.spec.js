126b415c5bbe1cd45fb8c61f5dccbbc4d63d93f5
JewelTee <jewelteeee@gmail.com>
2024-08-03 22:10:53 +0800

    Fixing failing tests

--- packages/playwright/tests/delete-account.spec.js
+++ packages/playwright/tests/delete-account.spec.js
@@ -9,10 +9,12 @@ test('test', async ({ page }) => {
   await page.getByLabel('Password').fill('12345678');
   await page.getByRole('button', { name: 'Sign In' }).click();
   await page.getByRole('button', { name: 'Menu' }).hover();
-  await page.getByRole('button', { name: ' Packs' }).click();
-  await page.goto('https://packrat.world/packs');
-  await page.waitForLoadState('load');
-  await page.getByRole('link', { name: 'Quantity field' }).click();
-  await page.goto('https://packrat.pages.dev/pack/jfxuhkd3zflt4ddavnyhh3so');
-  await page.waitForLoadState('load');
+  await page.getByRole('button', { name: ' Profile' }).click();
+  await page.goto('https://packrat.world/profile');
+  await page.getByRole('button', { name: '󰢻' }).click();
+  await page.getByLabel('To confirm this, type "delete"').click();
+  await page.getByLabel('To confirm this, type "delete"').fill('delete');
+  await page.getByRole('button', { name: 'Delete account' }).click();
+  await page.goto('https://packrat.world/sign-in');
+
 });
--- packages/playwright/tests/privacy-policy.spec.js
+++ packages/playwright/tests/privacy-policy.spec.js
@@ -2,7 +2,7 @@ import { test, expect } from '@playwright/test';
 
 test('test', async ({ page }) => {
   await page.goto('https://packrat.pages.dev/');
-  await page.getByRole('button', { name: 'Menu' }).click();
+  await page.getByRole('button', { name: 'Menu' }).hover();
   await page.getByRole('button', { name: ' About' }).click();
   await page.goto('https://packrat.world/about');
   await page.getByText('Privacy Policy').click();
