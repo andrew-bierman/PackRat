1275fe5542d157e9193b2f3a0b78c89f9a96b4ea
Anmol Verma <anmolverma2810@gmail.com>
2024-09-25 14:55:37 +0530

    remove redundent files

--- packages/app/modules/user/components/UserPackCard.tsx
+++ packages/app/modules/user/components/UserPackCard.tsx
@@ -1,16 +1,87 @@
-import React, { type FC } from 'react';
-import { type FeedCardProps } from 'app/modules/feed';
+import {
+  Card,
+  Details,
+  RButton,
+  RLink,
+  RStack,
+  RSwitch,
+  RText,
+} from '@packrat/ui';
+import React, { useState, type FC } from 'react';
+import { PackImage } from 'app/modules/pack/components/PackCard/PackImage';
+import {
+  FavoriteButton,
+  CreatedAtLabel,
+  type FeedCardProps,
+} from 'app/modules/feed';
 import { type PackDetails } from 'app/modules/pack/model';
-import { UserPrimaryPackCard } from './UserPrimaryPackCard';
+import { DuplicateIcon } from 'app/assets/icons';
+import { useItemWeightUnit } from 'app/modules/item';
+import { convertWeight } from 'app/utils/convertWeight';
+import { roundNumber } from 'app/utils';
+import { useEditPack } from 'app/modules/pack/hooks';
+import { TouchableWithoutFeedback } from 'react-native-gesture-handler';
+import useTheme from 'app/hooks/useTheme';
 
-interface UserPackCardProps extends FeedCardProps<PackDetails> {}
+interface PackCardProps extends FeedCardProps<PackDetails> {}
 
-const UserPackCards = {
-  primary: UserPrimaryPackCard,
-};
+export const UserPackCard: FC<PackCardProps> = (props) => {
+  const { editPack, isLoading, isError } = useEditPack();
+  const [isPublic, setIsPublic] = useState(props.is_public);
+  const updateIsPublic = (value) => {
+    setIsPublic(value);
+    editPack({ id: props.id, name: props.title, is_public: value });
+  };
+  const { currentTheme } = useTheme();
 
-export const UserPackCard: FC<UserPackCardProps> = (props) => {
-  const UserCardComponent = UserPackCards[props.cardType];
-
-  return <UserCardComponent {...props} />;
+  return (
+    <Card
+      title={props.title}
+      link={`/pack/${props.id}`}
+      image={<PackImage />}
+      subtitle={
+        <Details
+          items={[
+            {
+              key: 'score',
+              label: 'Score',
+              value: props.details.score,
+            },
+          ]}
+        />
+      }
+      actions={
+        <RStack style={{ flexDirection: 'row', gap: 12 }}>
+          <FavoriteButton
+            count={props.favoriteCount}
+            isAuthUserFavorite={props.isUserFavorite}
+            onClick={(e) => {
+              e.stopPropagation();
+              e.preventDefault();
+              props.toggleFavorite();
+            }}
+          />
+          <RButton
+            style={{
+              backgroundColor: currentTheme.colors.background,
+              borderRadius: 20,
+              height: 20,
+              width: 30,
+            }}
+            onClick={(e) => {
+              e.stopPropagation();
+              e.preventDefault();
+            }}
+          >
+            <RSwitch
+              checked={isPublic}
+              onCheckedChange={updateIsPublic}
+              size="$1.5"
+            />
+          </RButton>
+        </RStack>
+      }
+      type={props.cardType}
+    />
+  );
 };
