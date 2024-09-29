1cf0ebefe071232337eb0d895dfb822255912bbb
Muhammad Hassan <engineermirzahassan@gmail.com>
2024-09-29 14:43:06 +0500

    removed logs

--- packages/app/modules/auth/hooks/useUser.ts
+++ packages/app/modules/auth/hooks/useUser.ts
@@ -1,56 +1,9 @@
-// import { useStorage } from 'app/hooks/storage/useStorage';
-// import { queryTrpc } from 'app/trpc';
-// import { useMemo } from 'react';
-
-// export const useAuthUserToken = () => {
-//   const [[isLoading, token]] = useStorage<string>('token');
-
-//   const memorizedToken = useMemo(() => token, [token]);
-//   const memorizedLoading = useMemo(() => isLoading, [isLoading]);
-
-//   return { token: memorizedToken, isLoading: memorizedLoading };
-// };
-
-// export const useUserQuery = () => {
-//   const { token, isLoading: isTokenLoading } = useAuthUserToken();
-//   // const isRequestEnabled = !!token && !isTokenLoading;
-//   const isRequestEnabled = useMemo(() => !!token && !isTokenLoading, [token, isTokenLoading]);
-//   console.log('isRequestEnabled', isRequestEnabled)
-//   const {
-//     refetch,
-//     data,
-//     isLoading: isRequestLoading,
-//   } = queryTrpc.getMe.useQuery(undefined, {
-//     enabled: isRequestEnabled,
-//   });
-//   // Sometimes the isLoading state don't work as expected so we have this solution here
-//   // isLoading stays true when request is not enabled
-//   // TODO fix loading state
-//   const isLoading = useMemo(() => {
-//     return (isRequestEnabled && isRequestLoading) || isTokenLoading === undefined || isTokenLoading;
-//   }, [isRequestEnabled, isRequestLoading, isTokenLoading]);
-
-//   return { user: data, isLoading, refetch };
-// };
-
-// export const useAuthUser = () => {
-//   const { user, isLoading, refetch } = useUserQuery();
-
-//   return useMemo(() => user, [user]);
-// };
-
-
 import { useEffect, useState, useMemo } from 'react';
 import { useStorage } from 'app/hooks/storage/useStorage';
 import { queryTrpc } from 'app/trpc';
 
 export const useAuthUserToken = () => {
   const [[isLoading, token]] = useStorage<string>('token');
-  
-  // Log for debugging purposes
-  useEffect(() => {
-    console.log('Token loading:', isLoading, 'Token value:', token);
-  }, [isLoading, token]);
 
   return { token, isLoading };
 };
@@ -77,12 +30,6 @@ export const useUserQuery = () => {
   const isLoading = useMemo(() => {
     return isTokenLoading || (isRequestEnabled && isRequestLoading);
   }, [isTokenLoading, isRequestEnabled, isRequestLoading]);
-
-  // Debugging to check the status
-  useEffect(() => {
-    console.log('isRequestLoading:', isRequestLoading);
-    console.log('user data:', data);
-  }, [isRequestLoading, data]);
 
   return { user: data, isLoading, refetch };
 };
