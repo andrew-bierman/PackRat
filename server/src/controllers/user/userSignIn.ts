b23c4475e20aaea6aae77eaaeeb7ca42535d068e
Taron <taron.aleksanyan.20@gmail.com>
2024-10-04 18:07:45 +0400

    fix getme query

--- server/src/controllers/user/userSignIn.ts
+++ server/src/controllers/user/userSignIn.ts
@@ -33,6 +33,8 @@ export function userSignInRoute() {
       user.id,
     );
 
-    return { accessToken, refreshToken };
+    const { password, ...userResponse } = user;
+
+    return { accessToken, refreshToken, user: userResponse };
   });
 }
