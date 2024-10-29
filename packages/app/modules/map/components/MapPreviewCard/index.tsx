a9a7e8a3e6addcfa37d8d8268d93d1580bce329c
Taron <taron.aleksanyan.20@gmail.com>
2024-10-29 21:41:55 +0400

    offline map support

--- packages/app/modules/map/components/MapPreviewCard/index.tsx
+++ packages/app/modules/map/components/MapPreviewCard/index.tsx
@@ -1,30 +1,72 @@
-import React, { type FC } from 'react';
+import React, { useEffect, useState, type FC } from 'react';
 import { StatusLabel } from './StatusLabel';
-import { View, RText, Card, YStack, Details } from '@packrat/ui';
-import useTheme from 'app/hooks/useTheme';
+import { View, RText, Card } from '@packrat/ui';
 import { TouchableOpacity } from 'react-native';
 import { MapImage } from './MapImage';
+import { Entypo } from '@expo/vector-icons';
+import { useDownloadMapProgress } from 'app/modules/map/hooks/useDownloadMapProgress';
+import { type OfflineMap } from 'app/modules/map/screens/OfflineMapsScreen';
 
 interface MapPreviewCardProps {
   id: string;
-  title: string;
-  isDownloaded: boolean;
+  item: OfflineMap;
   onShowMapClick: (id: string) => void;
 }
 
 export const MapPreviewCard: FC<MapPreviewCardProps> = ({
   id,
   onShowMapClick,
-  title,
-  isDownloaded,
+  item,
 }) => {
-  const { enableDarkMode, enableLightMode, isDark, isLight, currentTheme } =
-    useTheme();
+  const [isDownloaded, setIsDownloaded] = useState(item.downloaded);
+  const { downloadMap, isDownloading, progress } = useDownloadMapProgress(() =>
+    setIsDownloaded(true),
+  );
+
+  const handleDownloadMap = () => {
+    downloadMap({
+      name: item.name,
+      bounds: item.bounds,
+      styleURL: item.styleURL,
+      minZoom: item.minZoom,
+      maxZoom: item.maxZoom,
+      metadata: {
+        id: item.id,
+        userId: item.userId,
+      },
+    });
+  };
+
+  useEffect(() => {
+    setIsDownloaded(item.downloaded);
+  }, [item.downloaded]);
 
   return (
     <Card
-      title={title}
-      subtitle={isDownloaded ? <StatusLabel /> : null}
+      title={item.name}
+      subtitle={
+        isDownloaded ? (
+          <StatusLabel />
+        ) : (
+          <TouchableOpacity onPress={handleDownloadMap}>
+            {isDownloading ? (
+              <RText style={{ color: '#000' }}>{`${progress}%`}</RText>
+            ) : (
+              <View
+                style={{
+                  flexDirection: 'row',
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                  gap: 8,
+                }}
+              >
+                <Entypo name={'download'} size={16} color="grey" />
+                <RText>Download</RText>
+              </View>
+            )}
+          </TouchableOpacity>
+        )
+      }
       link=""
       image={<MapImage />}
       isFullWidth
