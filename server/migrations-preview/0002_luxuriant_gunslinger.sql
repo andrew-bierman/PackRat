c311ca265d5e35d3e1d7e15ef0554bca5cdc164a
Tadjaur <tadjaur@tas-groups.com>
2024-10-29 20:48:22 +0100

    Update the 002 sql generated data

--- server/migrations-preview/0002_luxuriant_gunslinger.sql
+++ server/migrations-preview/0002_luxuriant_gunslinger.sql
@@ -5,4 +5,4 @@ ALTER TABLE item ADD `product_details` text;--> statement-breakpoint
 ALTER TABLE item ADD `seller` text;--> statement-breakpoint
 ALTER TABLE item_pack_templates ADD `quantity` integer DEFAULT 1 NOT NULL;--> statement-breakpoint
 ALTER TABLE item_packs ADD `quantity` integer DEFAULT 1 NOT NULL;--> statement-breakpoint
-ALTER TABLE `item` DROP COLUMN `quantity`;
+ALTER TABLE item DROP COLUMN `quantity`;
