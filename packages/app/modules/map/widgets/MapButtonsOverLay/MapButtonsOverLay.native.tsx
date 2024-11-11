a49037a04c405896637e767e6e984298269f9125
Tadjaur <tadjaur@tas-groups.com>
2024-11-11 20:32:17 +0100

    Ensure we display download feautre only for natie application

--- packages/app/modules/map/widgets/MapButtonsOverLay/MapButtonsOverLay.native.tsx
+++ packages/app/modules/map/widgets/MapButtonsOverLay/MapButtonsOverLay.native.tsx
@@ -1,6 +1,7 @@
 import React, { type FC } from 'react';
 import { type MapButtonsOverlayProps } from './model';
 import { DownloadMapBtn } from '../../components/DownloadMapBtn';
+import { Platform } from 'react-native';
 
 export const MapButtonsOverlay: FC<MapButtonsOverlayProps> = ({
   children,
@@ -8,10 +9,11 @@ export const MapButtonsOverlay: FC<MapButtonsOverlayProps> = ({
   shape,
   shouldEnableDownload,
 }) => {
+  const isNative = Platform.OS !== 'web';
   return (
     <>
       {/* DISABLE OFFLINE MAPS */}
-      {shouldEnableDownload ? (
+      {shouldEnableDownload && isNative ? (
         <DownloadMapBtn
           currentBounds={currentBounds}
           shape={shape}
