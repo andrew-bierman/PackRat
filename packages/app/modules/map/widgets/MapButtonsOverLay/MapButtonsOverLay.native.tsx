412c912e4727fd47030c2db34339193d4c5367e6
Anmol Verma <anmolverma2810@gmail.com>
2024-11-12 02:36:01 +0530

    Revert "Fix offline manager issue"

--- packages/app/modules/map/widgets/MapButtonsOverLay/MapButtonsOverLay.native.tsx
+++ packages/app/modules/map/widgets/MapButtonsOverLay/MapButtonsOverLay.native.tsx
@@ -1,7 +1,6 @@
 import React, { type FC } from 'react';
 import { type MapButtonsOverlayProps } from './model';
 import { DownloadMapBtn } from '../../components/DownloadMapBtn';
-import { Platform } from 'react-native';
 
 export const MapButtonsOverlay: FC<MapButtonsOverlayProps> = ({
   children,
@@ -9,11 +8,10 @@ export const MapButtonsOverlay: FC<MapButtonsOverlayProps> = ({
   shape,
   shouldEnableDownload,
 }) => {
-  const isNative = Platform.OS !== 'web';
   return (
     <>
       {/* DISABLE OFFLINE MAPS */}
-      {shouldEnableDownload && isNative ? (
+      {shouldEnableDownload ? (
         <DownloadMapBtn
           currentBounds={currentBounds}
           shape={shape}
