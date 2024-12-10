1f9012f13e13b71889bdca8ff5ac1f15834eee39
Tadjaur <tadjaur@tas-groups.com>
2024-12-10 01:44:46 +0100

    revert the minifar installation

---
+++ packages/app/modules/map/components/DownloadMapBtn/DownloadMapBtn.web.tsx
@@ -0,0 +1,7 @@
+import React, { type FC } from 'react';
+import { View } from 'react-native';
+
+export const DownloadMapBtn: FC = () => {
+  // Returns empty component on the on the web implementation.
+  return <View />;
+};
