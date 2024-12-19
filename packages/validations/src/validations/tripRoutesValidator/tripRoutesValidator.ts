92934561bffa788403bb707b08e01597406e108a
Tadjaur <tadjaur@tas-groups.com>
2024-12-19 02:49:54 +0100

    Fix trip schema definition and migration

--- packages/validations/src/validations/tripRoutesValidator/tripRoutesValidator.ts
+++ packages/validations/src/validations/tripRoutesValidator/tripRoutesValidator.ts
@@ -4,7 +4,7 @@ import { TripActivity } from './enums';
 const tripActivityValues = Object.values(TripActivity) as [string, ...string[]];
 
 export const tripForm = z.object({
-  name: z.string().min(2).max(40),
+  name: z.string().min(3).max(40),
   description: z.string().min(50).max(200).optional().nullable(),
   activity: z.enum(tripActivityValues).optional(),
   is_public: z.boolean().optional(),
