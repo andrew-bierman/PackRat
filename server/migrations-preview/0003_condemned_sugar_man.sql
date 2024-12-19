937528ad69c5b8fc53f82a3545a3211bb4730634
Tadjaur <tadjaur@tas-groups.com>
2024-12-19 03:24:01 +0100

    generate a new migration in preview environment

--- server/migrations-preview/0003_condemned_sugar_man.sql
+++ server/migrations-preview/0003_condemned_sugar_man.sql
@@ -6,31 +6,3 @@
 
  Due to that we don't generate migration automatically and it has to be done manually
 */
-/* MANUAL MIGRATION */
-CREATE TABLE `trip_new` (
-	`id` text PRIMARY KEY NOT NULL,
-	`name` text NOT NULL,
-	`description` text,
-	`parks` text,
-	`start_date` text NOT NULL,
-	`end_date` text NOT NULL,
-	`destination` text NOT NULL,
-	`owner_id` text REFERENCES `user`(`id`) ON UPDATE no action ON DELETE cascade,
-	`packs_id` text REFERENCES `pack`(`id`) ON UPDATE no action ON DELETE set null,
-	`is_public` integer,
-	`activity` text DEFAULT 'trip',
-	`bounds` text,
-	`type` text DEFAULT 'trip',
-	`scores` text DEFAULT '{"totalScore":0}',
-	`created_at` text DEFAULT CURRENT_TIMESTAMP,
-	`updated_at` text DEFAULT CURRENT_TIMESTAMP
-);
-
-INSERT INTO trip_new
-SELECT * FROM trip;
-
-DROP TABLE trip;
-
-ALTER TABLE trip_new RENAME TO trip;
-
-ALTER TABLE trip ADD `trails` text;
