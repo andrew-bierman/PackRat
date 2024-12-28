74a5dd700024b862f16304486a995a4a2e47d2c7
pinocchio-life-like <icbr19fl@gmail.com>
2024-12-28 15:13:47 +0300

    modules  and some others

--- server/src/modules/map/controllers/getOfflineMaps.ts
+++ server/src/modules/map/controllers/getOfflineMaps.ts
@@ -1,25 +1,30 @@
-import { getPaginationResponse } from 'src/helpers/pagination';
+import { getPaginationResponse } from '../../../helpers/pagination';
 import { protectedProcedure } from '../../../trpc';
 import { getOfflineMapsService } from '../services';
 import { z } from 'zod';
+import { OfflineMap } from '../../../db/schema';
 
 export function getOfflineMapsRoute() {
   return protectedProcedure
     .input(
       z.object({
         ownerId: z.string(),
-        pagination: z.object({ limit: z.number(), offset: z.number() }),
+        pagination: z
+          .object({ limit: z.number(), offset: z.number() })
+          .strict(),
       }),
     )
-    .query(async (opts) => {
-      const { pagination, ownerId } = opts.input;
-      const { offlineMaps, totalCount } = await getOfflineMapsService(
-        ownerId,
-        pagination,
-      );
-      return {
-        data: offlineMaps,
-        ...getPaginationResponse(pagination, totalCount),
-      };
-    });
+    .query(
+      async (opts): Promise<{ data: OfflineMap[]; totalCount: number }> => {
+        const { pagination, ownerId } = opts.input;
+        const { offlineMaps, totalCount } = await getOfflineMapsService(
+          ownerId,
+          pagination as { limit: number; offset: number },
+        );
+        return {
+          data: offlineMaps,
+          ...getPaginationResponse(pagination, Number(totalCount)),
+        };
+      },
+    );
 }
