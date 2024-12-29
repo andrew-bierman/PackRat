117e3f41aa41e1f5305df426b2aa8970f81dd767
pinocchio-life-like <icbr19fl@gmail.com>
2024-12-29 19:55:03 +0300

    remove ignore type

--- server/src/tests/routes/weather.spec.ts
+++ server/src/tests/routes/weather.spec.ts
@@ -1,5 +1,3 @@
-// @ts-nocheck
-
 import { describe, it, expect, beforeAll } from 'vitest';
 import { setupTest } from '../utils/testHelpers';
 import type { trpcCaller } from '../utils/testHelpers';
@@ -29,7 +27,7 @@ describe('Get weather information', () => {
       lat: 20.5937,
       lon: 78.9629,
     };
-    const weather = await caller.getWeatherWeek(coordinates);
+    const weather = await caller.getWeather(coordinates);
     expect(weather).toBeDefined();
     expect(weather?.coord?.lon).toEqual(coordinates?.lon);
     expect(weather?.coord?.lat).toEqual(coordinates?.lat);
