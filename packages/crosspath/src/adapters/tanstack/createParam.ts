5c731f8585603e7cb767d194828ea846799f7f09
Taron <taron.aleksanyan.20@gmail.com>
2025-01-06 14:29:30 +0400

    fix create param router

--- packages/crosspath/src/adapters/tanstack/createParam.ts
+++ packages/crosspath/src/adapters/tanstack/createParam.ts
@@ -3,45 +3,54 @@ import {
   useSearch,
 } from '@tanstack/react-router';
 import { useRouter } from './useRouter';
-import { CreateParam, CreateParamOptions } from '../../model/types';
+import type { CreateParam, CreateParamOptions } from '../../model';
 
-export const createParam: CreateParam = () => ({
-  useParam: (key, options) => {
-    const { initial, parse, stringify } = options || {};
-    const { push } = useRouter();
-    const screenParams = useTanstackParams({ strict: false }) as any;
-    const searchParams = useSearch({ strict: false }) as any;
-
-    const rawParam = screenParams[key] || searchParams[key] || initial;
-    const paramAsString = typeof rawParam === 'string' ? rawParam : '';
-    const parsedParam = parse ? parse(paramAsString) : rawParam;
-
-    const setParam = (value: any) => {
-      if (screenParams[key]) {
-        return console.error("You can't update screen params");
-      }
-      const stringifiedValue = stringify ? stringify(value) : value;
-      console.log('aaaa', { ...searchParams, [key]: stringifiedValue });
-
-      push({ query: { ...searchParams, [key]: stringifiedValue } });
-    };
-
-    return [parsedParam, setParam];
-  },
-  useParams: (key) => {
-    const { push } = useRouter();
-    const screenParams = useTanstackParams({ strict: false }) as any;
-    const searchParams = useSearch({ strict: false }) as any;
-    const params = { ...screenParams, ...searchParams };
-
-    const setParams = (value: any) => {
-      if (screenParams[key]) {
-        return console.error("You can't update screen params");
-      }
-
-      push({ query: { ...searchParams, [key]: value } });
-    };
-
-    return { params, setParams };
-  },
+export const createParam: CreateParam = <T>() => ({
+  useParam: useParam<T>,
+  useParams: useParams<T>,
 });
+
+const useParam = <T>(
+  key: keyof T,
+  options: CreateParamOptions,
+): [value: any, setValue: (value: any) => void] => {
+  const { initial, parse, stringify } = options || {};
+  const { push } = useRouter();
+  const screenParams = useTanstackParams({ strict: false }) || {};
+  const searchParams = useSearch({ strict: false }) || {};
+
+  const param = screenParams[key] || searchParams[key] || initial;
+  const parsedParam = parse ? parse(param) : param;
+
+  const setParam = (value: any) => {
+    if (screenParams[key]) {
+      return console.error("You can't update screen params");
+    }
+    const stringifiedValue = stringify ? stringify(value) : value;
+    console.log('aaaa', { ...searchParams, [key]: stringifiedValue });
+
+    push({ query: { ...searchParams, [key]: stringifiedValue } });
+  };
+
+  return [parsedParam, setParam];
+};
+
+const useParams = <T>(): {
+  params: T;
+  setParams: (value: Partial<T>) => void;
+} => {
+  const { push } = useRouter();
+  const screenParams = (useTanstackParams({ strict: false }) || {}) as T;
+  const searchParams = (useSearch({ strict: false }) || {}) as T;
+  const params = { ...screenParams, ...searchParams };
+
+  const setParams = (value: Partial<T>) => {
+    if (Object.keys(value).some((key) => !!screenParams[key])) {
+      return console.error("You can't update screen params");
+    }
+
+    push({ query: { ...searchParams, ...value } });
+  };
+
+  return { params, setParams };
+};
