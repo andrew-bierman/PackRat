7003be800aad2356344140f61820c72982a8bb49
Taron <taron.aleksanyan.20@gmail.com>
2025-01-14 18:54:54 +0400

    fix api url

--- packages/app/constants/api.ts
+++ packages/app/constants/api.ts
@@ -1,5 +1,5 @@
 import { API_URL } from '@packrat/config';
-import { Platform } from 'react-native';
+// import { Platform } from 'react-native';
 // const API_URL = import.meta.env.VITE_API_URL;
 
 /**
@@ -9,8 +9,6 @@ import { Platform } from 'react-native';
  */
 
 // use this for android emulator
-export const api =
-  Platform.OS === 'web'
-    ? API_URL
-    : 'https://6916-5-77-200-104.ngrok-free.app/api';
-// export const api = API_URL;
+// export const api = Platform.OS === 'web' ? API_URL : 'http://10.0.2.2:8787/api';
+
+export const api = API_URL;
